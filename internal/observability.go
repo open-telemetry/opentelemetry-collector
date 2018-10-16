093f0b2169f2baee3bcbe92f344c3198ec2ca82b
Emmanuel T Odeke <emmanuel@orijtech.com>
2018-10-15 19:47:22 -0700

    exporter: add metrics recording helper for Exporters
    Updates #64

--- internal/observability.go
+++ internal/observability.go
@@ -32,22 +32,38 @@ import (
 	tracepb "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
 )
 
-var tagKeyInterceptorName, _ = tag.NewKey("opencensus_interceptor")
+var (
+	tagKeyInterceptorName, _ = tag.NewKey("opencensus_interceptor")
+	tagKeyExporterName, _    = tag.NewKey("opencensus_exporter")
+)
+
 var mReceivedSpans = stats.Int64("oc.io/interceptor/received_spans", "Counts the number of spans received by the interceptor", "1")
+
+var itemsDistribution = view.Distribution(
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90,
+	100, 150, 200, 250, 300, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000,
+)
 
 var ViewReceivedSpansInterceptor = &view.View{
 	Name:        "oc.io/interceptor/received_spans",
 	Description: "The number of spans received by the interceptor",
 	Measure:     mReceivedSpans,
-	Aggregation: view.Distribution(
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90,
-		100, 150, 200, 250, 300, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000,
-	),
-	TagKeys: []tag.Key{tagKeyInterceptorName},
+	Aggregation: itemsDistribution,
+	TagKeys:     []tag.Key{tagKeyInterceptorName},
+}
+
+var mExportedSpans = stats.Int64("oc.io/interceptor/exported_spans", "Counts the number of exported spans", "1")
+var ViewExportedSpans = &view.View{
+	Name:        "oc.io/interceptor/exported_spans",
+	Description: "Tracks the number of exported spans",
+	Measure:     mExportedSpans,
+	Aggregation: itemsDistribution,
+	TagKeys:     []tag.Key{tagKeyExporterName},
 }
 
 var AllViews = []*view.View{
 	ViewReceivedSpansInterceptor,
+	ViewExportedSpans,
 }
 
 // ContextWithInterceptorName adds the tag "opencensus_interceptor" and the name of the
@@ -70,6 +86,16 @@ func NewReceivedSpansRecorderStreaming(lifetimeCtx context.Context, interceptorN
 	return func(ni *commonpb.Node, spans []*tracepb.Span) {
 		// TODO: (@odeke-em) perhaps also record information from the node?
 		stats.Record(ctx, mReceivedSpans.M(int64(len(spans))))
+	}
+}
+
+// NewExportedSpansRecorder creates a helper function that'll add the name of the
+// creating exporter as a tag value in the context that will be used to count the
+// the number of spans exported.
+func NewExportedSpansRecorder(exporterName string) func(context.Context, *commonpb.Node, []*tracepb.Span) {
+	return func(ctx context.Context, ni *commonpb.Node, spans []*tracepb.Span) {
+		ctx, _ = tag.New(ctx, tag.Upsert(tagKeyExporterName, exporterName))
+		stats.Record(ctx, mExportedSpans.M(int64(len(spans))))
 	}
 }
 
