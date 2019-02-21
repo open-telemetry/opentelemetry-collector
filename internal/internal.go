57457be9c576b26d0c63958b8a4940fa9760cc0f
Paulo Janotti <paulo@omnition.io>
2019-02-21 11:31:12 -0800

    Remove unused code in internal package (#425)
    Just removing unused code from internal package.

--- internal/internal.go
+++ internal/internal.go
@@ -15,77 +15,12 @@
 package internal
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/user"
-	"path/filepath"
-	"runtime"
 	"strings"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
 )
-
-// Service contains metadata about the exporter service.
-type Service struct {
-	Endpoint string `json:"endpoint"`
-}
-
-// WriteToEndpointFile writes service metadata to
-// canonical endpoint file.
-func (s *Service) WriteToEndpointFile() (path string, err error) {
-	data, err := json.Marshal(s)
-	if err != nil {
-		return "", err
-	}
-	path = defaultEndpointFile()
-	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
-		return "", err
-	}
-	if err := ioutil.WriteFile(path, data, 0755); err != nil {
-		return "", err
-	}
-	return path, nil
-}
-
-// ParseEndpointFile reads and parses the canonical endpoint
-// file for metadata.
-func ParseEndpointFile() (*Service, error) {
-	file := defaultEndpointFile()
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-	var s Service
-	if err := json.Unmarshal(data, &s); err != nil {
-		return nil, err
-	}
-	return &s, nil
-}
-
-// defaultEndpointFile is the location where the
-// endpoint file is at on the current platform.
-func defaultEndpointFile() string {
-	const f = "opencensus.endpoint"
-	if runtime.GOOS == "windows" {
-		return filepath.Join(os.Getenv("APPDATA"), "opencensus", f)
-	}
-	return filepath.Join(guessUnixHomeDir(), ".config", f)
-}
-
-func guessUnixHomeDir() string {
-	// Prefer $HOME over user.Current due to glibc bug: golang.org/issue/13470
-	if v := os.Getenv("HOME"); v != "" {
-		return v
-	}
-	// Else, fall back to user.Current:
-	if u, err := user.Current(); err == nil {
-		return u.HomeDir
-	}
-	return ""
-}
 
 // TimeToTimestamp converts a time.Time to a timestamp.Timestamp pointer.
 func TimeToTimestamp(t time.Time) *timestamp.Timestamp {
