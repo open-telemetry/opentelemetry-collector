55297dfee615aa4c675ab0e463a90ee21d014603
Tigran Najaryan <tigran@omnition.io>
2019-06-21 09:47:51 -0400

    Add factory and new-style config for Prometheus receiver
    This is part of remaining migration to new configuration format.
    
    Github issue: https://github.com/open-telemetry/opentelemetry-service/issues/33
    
    Testing done: make

---
+++ receiver/prometheusreceiver/config_test.go
@@ -0,0 +1,54 @@
+// Copyright 2019, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheusreceiver
+
+import (
+	"path"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-service/internal/configmodels"
+	"github.com/open-telemetry/opentelemetry-service/internal/configv2"
+	"github.com/open-telemetry/opentelemetry-service/internal/factories"
+)
+
+var _ = configv2.RegisterTestFactories()
+
+func TestLoadConfig(t *testing.T) {
+	factory := factories.GetReceiverFactory(typeStr)
+
+	config, err := configv2.LoadConfigFile(t, path.Join(".", "testdata", "config.yaml"))
+
+	require.NoError(t, err)
+	require.NotNil(t, config)
+
+	assert.Equal(t, len(config.Receivers), 2)
+
+	r0 := config.Receivers["prometheus"]
+	assert.Equal(t, r0, factory.CreateDefaultConfig())
+
+	r1 := config.Receivers["prometheus/customname"].(*ConfigV2)
+	assert.Equal(t, r1.ReceiverSettings,
+		configmodels.ReceiverSettings{
+			TypeVal:  typeStr,
+			NameVal:  "prometheus/customname",
+			Endpoint: "1.2.3.4:456",
+		})
+	assert.Equal(t, r1.PrometheusConfig.ScrapeConfigs[0].JobName, "demo")
+	assert.Equal(t, time.Duration(r1.PrometheusConfig.ScrapeConfigs[0].ScrapeInterval), 5*time.Second)
+}
---
+++ receiver/prometheusreceiver/factory_test.go
@@ -0,0 +1,45 @@
+// Copyright 2019, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheusreceiver
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/open-telemetry/opentelemetry-service/internal/factories"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := factories.GetReceiverFactory(typeStr)
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+}
+
+func TestCreateReceiver(t *testing.T) {
+	factory := factories.GetReceiverFactory(typeStr)
+	cfg := factory.CreateDefaultConfig()
+
+	tReceiver, err := factory.CreateTraceReceiver(context.Background(), cfg, nil)
+	assert.Equal(t, err, factories.ErrDataTypeIsNotSupported)
+	assert.Nil(t, tReceiver)
+
+	// The default config does not provide scrape_config so we expect that metrics receiver
+	// creation must also fail.
+	mReceiver, err := factory.CreateMetricsReceiver(cfg, nil)
+	assert.Equal(t, err, errNilScrapeConfig)
+	assert.Nil(t, mReceiver)
+}
