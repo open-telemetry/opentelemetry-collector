f8f514460aac3491c22ee2e202f20c4752daf4e0
Tigran Najaryan <tigran@omnition.io>
2019-07-02 15:37:19 -0400

    Implement Prometheus factory and config V2
    Github issue: https://github.com/open-telemetry/opentelemetry-service/issues/38
    
    Testing done:
    
    - make && make otelsvc
    - Run otelsvc with the following config and make sure Prometheus can scrape :
    
    receivers:
      opencensus:
        port: 55678
    
      prometheus:
        config:
          scrape_configs:
            - job_name: 'demo'
              scrape_interval: 5s
    
    zpages:
      port: 55679
    
    exporters:
      prometheus:
        endpoint: "127.0.0.1:8889"
    
    pipelines:
      metrics:
        receivers: [prometheus]
        exporters: [prometheus]

---
+++ exporter/prometheusexporter/factory_test.go
@@ -0,0 +1,79 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheusexporter
+
+import (
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-service/models"
+
+	"go.uber.org/zap"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-service/factories"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := factories.GetExporterFactory(typeStr)
+	require.NotNil(t, factory)
+
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+}
+
+func TestCreateTraceExporter(t *testing.T) {
+	factory := factories.GetExporterFactory(typeStr)
+	cfg := factory.CreateDefaultConfig()
+
+	_, _, err := factory.CreateTraceExporter(zap.NewNop(), cfg)
+	assert.Error(t, err, models.ErrDataTypeIsNotSupported)
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	const defaultTestEndPoint = "127.0.0.1:55678"
+	tests := []struct {
+		name     string
+		config   ConfigV2
+		mustFail bool
+	}{
+		{
+			name: "NoEndpoint",
+			config: ConfigV2{
+				Endpoint: "",
+			},
+			mustFail: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			factory := factories.GetExporterFactory(typeStr)
+			consumer, stopFunc, err := factory.CreateMetricsExporter(zap.NewNop(), &tt.config)
+
+			if tt.mustFail {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.NotNil(t, consumer)
+				assert.NotNil(t, stopFunc)
+
+				err = stopFunc()
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
