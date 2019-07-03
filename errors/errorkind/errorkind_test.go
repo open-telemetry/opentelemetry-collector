31d6389b572360cfc045c2a80b47492f366c6fb1
Paulo Janotti <paulo@omnition.io>
2019-07-03 08:40:41 -0700

    No queued retry on bad data (#91)
    * No queued retry on bad data
    
    Allows the queued-retry to identify permanent errors from the consumer attached to it. This way it can safely drop batches of data data without burdening the system with retries.
    
    * Move errorkind package and use require package in tests

---
+++ errors/errorkind/errorkind_test.go
@@ -0,0 +1,41 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package errorkind provides wrappers to easily classify errors, allowing
+// appropriate action by error handlers without need to know each individual
+// error type/instance.
+package errorkind
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPermanent(t *testing.T) {
+	err := errors.New("testError")
+	if IsPermanent(err) {
+		t.Fatalf("IsPermanent() = true, want false")
+	}
+	err = Permanent(err)
+	if !IsPermanent(err) {
+		t.Fatalf("IsPermanent() = false, want true")
+	}
+}
+
+func TestIsPermanent_NilError(t *testing.T) {
+	var err error
+	if IsPermanent(err) {
+		t.Fatalf("IsPermanent() = true, want false")
+	}
+}
