11a0a2ab4e8bb28c8dcf11b33e7d02fd99c61a19
Mohamed Elqdusy <mohamedelqdusy@gmail.com>
2019-07-26 19:56:43 +0200

    Implement factory and config for sampling processor (head-based) (#186)
    * Implement factory and config for sampling processor (head-based)
    
    * Change Config name

---
+++ processor/tracesamplerprocessor/config_test.go
@@ -0,0 +1,73 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracesamplerprocessor
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-service/config"
+	"github.com/open-telemetry/opentelemetry-service/config/configmodels"
+	"github.com/open-telemetry/opentelemetry-service/processor"
+)
+
+func TestLoadConfig(t *testing.T) {
+	receivers, processors, exporters, err := config.ExampleComponents()
+	assert.Nil(t, err)
+
+	factory := &Factory{}
+	processors[typeStr] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "config.yaml"), receivers, processors, exporters,
+	)
+
+	require.Nil(t, err)
+	require.NotNil(t, cfg)
+
+	p0 := cfg.Processors["trace-sampler"]
+	assert.Equal(t, p0,
+		&Config{
+			ProcessorSettings: configmodels.ProcessorSettings{
+				TypeVal: "trace-sampler",
+				NameVal: "trace-sampler",
+			},
+			SamplingPercentage: 15.3,
+			HashSeed:           22,
+		})
+
+}
+
+func TestLoadConfigEmpty(t *testing.T) {
+	receivers, _, exporters, err := config.ExampleComponents()
+	processors, err := processor.Build(&Factory{})
+	require.NotNil(t, processors)
+	require.NoError(t, err)
+
+	config, err := config.LoadConfigFile(
+		t,
+		path.Join(".", "testdata", "empty.yaml"),
+		receivers,
+		processors,
+		exporters)
+
+	require.Nil(t, err)
+	require.NotNil(t, config)
+	p0 := config.Processors["trace-sampler"]
+	factory := &Factory{}
+	assert.Equal(t, p0, factory.CreateDefaultConfig())
+}
