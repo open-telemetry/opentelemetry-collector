ed70d6c53a8ed1be6584061fa42657246459dcb0
Paulo Janotti <paulo@omnition.io>
2019-08-01 08:48:40 -0700

    Add Jaeger gRPC exporter
    Adds a Jaeger gRPC exporter.

--- exporter/jaeger/empty_test.go
+++ exporter/jaeger/empty_test.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package jaegerexporter
+package jaeger
 
 // TODO: Delete me when tests are added.
---
+++ exporter/jaeger/jaegergrpcexporter/factory_test.go
@@ -0,0 +1,65 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jaegergrpcexporter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-service/config/configerror"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+
+	_, _, err := factory.CreateMetricsExporter(zap.NewNop(), cfg)
+	assert.Error(t, err, configerror.ErrDataTypeIsNotSupported)
+}
+
+func TestCreateInstanceViaFactory(t *testing.T) {
+	factory := Factory{}
+
+	cfg := factory.CreateDefaultConfig()
+
+	// Default config doesn't have default endpoint so creating from it should
+	// fail.
+	exp, expStopFn, err := factory.CreateTraceExporter(
+		zap.NewNop(),
+		cfg)
+	assert.Error(t, err)
+	assert.Nil(t, exp)
+	assert.Nil(t, expStopFn)
+
+	// Endpoint doesn't have a default value so set it directly.
+	expCfg := cfg.(*Config)
+	expCfg.Endpoint = "some.target.org:12345"
+	exp, expStopFn, err = factory.CreateTraceExporter(
+		zap.NewNop(),
+		cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, exp)
+
+	// Currently this exporter doesn't have a stop function.
+	assert.Nil(t, expStopFn)
+}
