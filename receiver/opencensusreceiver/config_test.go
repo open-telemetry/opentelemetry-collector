b47d77d322afcf93827325c01234283c00048b5b
Paulo Janotti <paulo@omnition.io>
2019-08-05 15:30:22 -0700

    Recover test that covered more than back presssure

--- receiver/opencensusreceiver/config_test.go
+++ receiver/opencensusreceiver/config_test.go
@@ -41,7 +41,7 @@ func TestLoadConfig(t *testing.T) {
 
 	// Currently disabled receivers are removed from the total list of receivers so 'opencensus/disabled' doesn't
 	// contribute to the count.
-	assert.Equal(t, len(cfg.Receivers), 4)
+	assert.Equal(t, len(cfg.Receivers), 5)
 
 	r0 := cfg.Receivers["opencensus"]
 	assert.Equal(t, r0, factory.CreateDefaultConfig())
@@ -77,10 +77,27 @@ func TestLoadConfig(t *testing.T) {
 			},
 		})
 
+	r3 := cfg.Receivers["opencensus/msg-size-conc-connect-max-idle"].(*Config)
+	assert.Equal(t, r3,
+		&Config{
+			ReceiverSettings: configmodels.ReceiverSettings{
+				TypeVal:             typeStr,
+				NameVal:             "opencensus/msg-size-conc-connect-max-idle",
+				Endpoint:            "127.0.0.1:55678",
+			},
+			MaxRecvMsgSizeMiB:    32,
+			MaxConcurrentStreams: 16,
+			Keepalive: &serverParametersAndEnforcementPolicy{
+				ServerParameters: &keepaliveServerParameters{
+					MaxConnectionIdle: 10 * time.Second,
+				},
+			},
+		})
+
 	// TODO(ccaraman): Once the config loader checks for the files existence, this test may fail and require
 	// 	use of fake cert/key for test purposes.
-	r3 := cfg.Receivers["opencensus/tlscredentials"].(*Config)
-	assert.Equal(t, r3,
+	r4 := cfg.Receivers["opencensus/tlscredentials"].(*Config)
+	assert.Equal(t, r4,
 		&Config{
 			ReceiverSettings: configmodels.ReceiverSettings{
 				TypeVal:  typeStr,
