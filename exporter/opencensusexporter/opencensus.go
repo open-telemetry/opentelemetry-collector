aeaee36320c4a1c556365151ab87be31e4c0b1d7
Yang Song <songy23@users.noreply.github.com>
2019-08-07 07:08:16 -0700

    Support metrics exporter with oc exporter config (#232)
    * Support metrics exporter with oc exporter config
    
    Also remove outdated v1 configs and fix tests.
    
    Updates #231.
    
    * Fix static check
    
    * Add documentation
    
    * Send proto metrics with ExportMetricsServiceRequest()
    
    Fixes #231.
    
    * Remove config TOC
    
    * Add document to config.go

--- exporter/opencensusexporter/opencensus.go
+++ exporter/opencensusexporter/opencensus.go
@@ -16,23 +16,15 @@ package opencensusexporter
 
 import (
 	"context"
-	"crypto/x509"
 	"fmt"
 	"sync"
 	"time"
 
 	"contrib.go.opencensus.io/exporter/ocagent"
+	agentmetricspb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/metrics/v1"
 	agenttracepb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/trace/v1"
-	"github.com/spf13/viper"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/keepalive"
 
-	"github.com/open-telemetry/opentelemetry-service/compression"
-	compressiongrpc "github.com/open-telemetry/opentelemetry-service/compression/grpc"
-	"github.com/open-telemetry/opentelemetry-service/consumer"
 	"github.com/open-telemetry/opentelemetry-service/consumer/consumerdata"
-	"github.com/open-telemetry/opentelemetry-service/exporter/exporterhelper"
 	"github.com/open-telemetry/opentelemetry-service/oterr"
 )
 
@@ -44,38 +36,26 @@ type KeepaliveConfig struct {
 	PermitWithoutStream bool          `mapstructure:"permit-without-stream,omitempty"`
 }
 
-type opencensusConfig struct {
-	Endpoint            string            `mapstructure:"endpoint,omitempty"`
-	Compression         string            `mapstructure:"compression,omitempty"`
-	Headers             map[string]string `mapstructure:"headers,omitempty"`
-	NumWorkers          int               `mapstructure:"num-workers,omitempty"`
-	CertPemFile         string            `mapstructure:"cert-pem-file,omitempty"`
-	UseSecure           bool              `mapstructure:"secure,omitempty"`
-	ReconnectionDelay   time.Duration     `mapstructure:"reconnection-delay,omitempty"`
-	KeepaliveParameters *KeepaliveConfig  `mapstructure:"keepalive,omitempty"`
-	// TODO: service name options.
-}
-
 type ocagentExporter struct {
 	exporters chan *ocagent.Exporter
 }
 
-type ocTraceExporterErrorCode int
-type ocTraceExporterError struct {
-	code ocTraceExporterErrorCode
+type ocExporterErrorCode int
+type ocExporterError struct {
+	code ocExporterErrorCode
 	msg  string
 }
 
-var _ error = (*ocTraceExporterError)(nil)
+var _ error = (*ocExporterError)(nil)
 
-func (e *ocTraceExporterError) Error() string {
+func (e *ocExporterError) Error() string {
 	return e.msg
 }
 
 const (
 	defaultNumWorkers int = 2
 
-	_ ocTraceExporterErrorCode = iota // skip 0
+	_ ocExporterErrorCode = iota // skip 0
 	// errEndpointRequired indicates that this exporter was not provided with an endpoint in its config.
 	errEndpointRequired
 	// errUnsupportedCompressionType indicates that this exporter was provided with a compression protocol it does not support.
@@ -85,108 +65,6 @@ const (
 	// errAlreadyStopped indicates that the exporter was already stopped.
 	errAlreadyStopped
 )
-
-// OpenCensusTraceExportersFromViper unmarshals the viper and returns an consumer.TraceConsumer targeting
-// OpenCensus Agent/Collector according to the configuration settings.
-func OpenCensusTraceExportersFromViper(v *viper.Viper) (tps []consumer.TraceConsumer, mps []consumer.MetricsConsumer, doneFns []func() error, err error) {
-	var cfg struct {
-		OpenCensus *opencensusConfig `mapstructure:"opencensus"`
-	}
-	if err := v.Unmarshal(&cfg); err != nil {
-		return nil, nil, nil, err
-	}
-	ocac := cfg.OpenCensus
-	if ocac == nil {
-		return nil, nil, nil, nil
-	}
-
-	if ocac.Endpoint == "" {
-		return nil, nil, nil, &ocTraceExporterError{
-			code: errEndpointRequired,
-			msg:  "OpenCensus exporter config requires an Endpoint",
-		}
-	}
-
-	opts := []ocagent.ExporterOption{ocagent.WithAddress(ocac.Endpoint)}
-	if ocac.Compression != "" {
-		if compressionKey := compressiongrpc.GetGRPCCompressionKey(ocac.Compression); compressionKey != compression.Unsupported {
-			opts = append(opts, ocagent.UseCompressor(compressionKey))
-		} else {
-			return nil, nil, nil, &ocTraceExporterError{
-				code: errUnsupportedCompressionType,
-				msg:  fmt.Sprintf("OpenCensus exporter unsupported compression type %q", ocac.Compression),
-			}
-		}
-	}
-	if ocac.CertPemFile != "" {
-		creds, err := credentials.NewClientTLSFromFile(ocac.CertPemFile, "")
-		if err != nil {
-			return nil, nil, nil, &ocTraceExporterError{
-				code: errUnableToGetTLSCreds,
-				msg:  fmt.Sprintf("OpenCensus exporter unable to read TLS credentials from pem file %q: %v", ocac.CertPemFile, err),
-			}
-		}
-		opts = append(opts, ocagent.WithTLSCredentials(creds))
-	} else if ocac.UseSecure {
-		certPool, err := x509.SystemCertPool()
-		if err != nil {
-			return nil, nil, nil, &ocTraceExporterError{
-				code: errUnableToGetTLSCreds,
-				msg: fmt.Sprintf(
-					"OpenCensus exporter unable to read certificates from system pool: %v", err),
-			}
-		}
-		creds := credentials.NewClientTLSFromCert(certPool, "")
-		opts = append(opts, ocagent.WithTLSCredentials(creds))
-	} else {
-		opts = append(opts, ocagent.WithInsecure())
-	}
-	if len(ocac.Headers) > 0 {
-		opts = append(opts, ocagent.WithHeaders(ocac.Headers))
-	}
-	if ocac.ReconnectionDelay > 0 {
-		opts = append(opts, ocagent.WithReconnectionPeriod(ocac.ReconnectionDelay))
-	}
-	if ocac.KeepaliveParameters != nil {
-		opts = append(opts, ocagent.WithGRPCDialOption(grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                ocac.KeepaliveParameters.Time,
-			Timeout:             ocac.KeepaliveParameters.Timeout,
-			PermitWithoutStream: ocac.KeepaliveParameters.PermitWithoutStream,
-		})))
-	}
-
-	numWorkers := defaultNumWorkers
-	if ocac.NumWorkers > 0 {
-		numWorkers = ocac.NumWorkers
-	}
-
-	exportersChan := make(chan *ocagent.Exporter, numWorkers)
-	for exporterIndex := 0; exporterIndex < numWorkers; exporterIndex++ {
-		exporter, serr := ocagent.NewExporter(opts...)
-		if serr != nil {
-			return nil, nil, nil, fmt.Errorf("cannot configure OpenCensus Trace exporter: %v", serr)
-		}
-		exportersChan <- exporter
-	}
-
-	oce := &ocagentExporter{exporters: exportersChan}
-	oexp, err := exporterhelper.NewTraceExporter(
-		"oc_trace",
-		oce.PushTraceData,
-		exporterhelper.WithSpanName("ocservice.exporter.OpenCensus.ConsumeTraceData"),
-		exporterhelper.WithRecordMetrics(true))
-
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	tps = append(tps, oexp)
-	doneFns = append(doneFns, oce.stop)
-
-	// TODO: (@odeke-em, @songya23) implement ExportMetrics for OpenCensus.
-	// mps = append(mps, oexp)
-	return
-}
 
 func (oce *ocagentExporter) stop() error {
 	wg := &sync.WaitGroup{}
@@ -221,7 +99,7 @@ func (oce *ocagentExporter) PushTraceData(ctx context.Context, td consumerdata.T
 	// Get first available exporter.
 	exporter, ok := <-oce.exporters
 	if !ok {
-		err := &ocTraceExporterError{
+		err := &ocExporterError{
 			code: errAlreadyStopped,
 			msg:  fmt.Sprintf("OpenCensus exporter was already stopped."),
 		}
@@ -238,6 +116,30 @@ func (oce *ocagentExporter) PushTraceData(ctx context.Context, td consumerdata.T
 	oce.exporters <- exporter
 	if err != nil {
 		return len(td.Spans), err
+	}
+	return 0, nil
+}
+
+func (oce *ocagentExporter) PushMetricsData(ctx context.Context, md consumerdata.MetricsData) (int, error) {
+	// Get first available exporter.
+	exporter, ok := <-oce.exporters
+	if !ok {
+		err := &ocExporterError{
+			code: errAlreadyStopped,
+			msg:  fmt.Sprintf("OpenCensus exporter was already stopped."),
+		}
+		return len(md.Metrics), err
+	}
+
+	req := &agentmetricspb.ExportMetricsServiceRequest{
+		Metrics:  md.Metrics,
+		Resource: md.Resource,
+		Node:     md.Node,
+	}
+	err := exporter.ExportMetricsServiceRequest(req)
+	oce.exporters <- exporter
+	if err != nil {
+		return len(md.Metrics), err
 	}
 	return 0, nil
 }
