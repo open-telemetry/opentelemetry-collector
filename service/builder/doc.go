5bd6084307b28a1c71b8559c325f6ec27d734702
Constance Caramanolis <c.caramanolis@gmail.com>
2019-08-08 11:37:27 -0700

    Clean up unused code in service/builder from port of OC Svc.

--- service/builder/doc.go
+++ service/builder/doc.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package builder handles the options to build the OpenCensus collector
+// Package builder handles the options to build the OpenTelemetry collector
 // pipeline.
 package builder
