6427a6b6d184caa1625862022fb8409671875a86
songy23 <songya@google.com>
2019-08-09 15:51:32 -0700

    Move zpages to package zpages instead of internal

--- zpages/zpagesserver.go
+++ zpages/zpagesserver.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package zpagesserver
+// Package zpages contains the config on setting up ZPages for diagnostics.
+package zpages
 
 import (
 	"flag"
