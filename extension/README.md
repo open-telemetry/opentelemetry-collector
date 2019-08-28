0b8eba05dda7ce62ef831eda31c41759fb4639ed
Paulo Janotti <paulo@omnition.io>
2019-08-28 08:50:15 -0700

    Refactor extensions to new config format

---
+++ extension/README.md
@@ -0,0 +1,23 @@
+# Extensions
+*Note* This documentation is still in progress. For any questions, please reach
+out in the [OpenTelemetry Gitter](https://gitter.im/open-telemetry/opentelemetry-service)
+or refer to the [issues page](https://github.com/open-telemetry/opentelemetry-service/issues).
+
+Supported service extensions (sorted alphabetically):
+- [Health Check](#health-check)
+- [Performance Profiler](#pprof)
+- [zPages](#zpages)
+
+## Ordering Extensions
+The order extensions are specified for the service is important as this is the
+order in which each extension will be started and the reverse order in which they
+will be shutdown.
+
+## <a name="health-check"></a>Health Check
+TODO
+
+## <a name="pprof"></a>Performance Profiler
+TODO
+
+## <a name="zpages"></a>zPages
+TODO
--- extension/pprofextension/pprofextension.go
+++ extension/pprofextension/pprofextension.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package pprofserver
+package pprofextension
 
 import (
 	"flag"
@@ -25,28 +25,8 @@ import (
 	"go.uber.org/zap"
 )
 
-const (
-	httpPprofPortCfg          = "http-pprof-port"
-	pprofBlockProfileFraction = "pprof-block-profile-fraction"
-	pprofMutexProfileFraction = "pprof-mutex-profile-fraction"
-)
 
-// AddFlags add the command-line flags used to control the Performance Profiler
-// (pprof) HTTP server to the given flag set.
-func AddFlags(flags *flag.FlagSet) {
-	flags.Uint(
-		httpPprofPortCfg,
-		0,
-		"Port to be used by golang net/http/pprof (Performance Profiler), the profiler is disabled if no port or 0 is specified.")
-	flags.Int(
-		pprofBlockProfileFraction,
-		-1,
-		"Fraction of blocking events that are profiled. A value <= 0 disables profiling. See runtime.SetBlockProfileRate for details.")
-	flags.Int(
-		pprofMutexProfileFraction,
-		-1,
-		"Fraction of mutex contention events that are profiled. A value <= 0 disables profiling. See runtime.SetMutexProfileFraction for details.")
-}
+
 
 // SetupFromViper sets up the Performance Profiler (pprof) as an HTTP endpoint
 // according to the configuration in the given viper.
