d4c2408a665df6256ba6fb8f9acc94bd226bf606
Dino Oliva <dino@docdino.com>
2019-11-14 12:50:10 -0800

    Remove redundant package description from service/builder

--- service/builder/builder.go
+++ service/builder/builder.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package builder contains the logic on parsing configs and building receivers,
-// exporters, pipeplines and tail sampling.
 package builder
 
 import (
