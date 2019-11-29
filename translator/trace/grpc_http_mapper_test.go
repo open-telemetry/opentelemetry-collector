bffa6d49da692d616cbb140ce941400429785644
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-11-29 13:10:38 -0600

    fix formatting

--- translator/trace/grpc_http_mapper_test.go
+++ translator/trace/grpc_http_mapper_test.go
@@ -15,8 +15,9 @@
 package tracetranslator
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHTTPStatusFromOTStatus(t *testing.T) {
