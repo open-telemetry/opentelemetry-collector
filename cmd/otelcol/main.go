2465c53e376fdba3ff78e6c0d46e20f6667a533b
Nail Islamov <nislamov@google.com>
2020-03-13 16:17:47 +1100

    Improve messages for fatal errors in the main function

--- cmd/otelcol/main.go
+++ cmd/otelcol/main.go
@@ -25,14 +25,14 @@ import (
 )
 
 func main() {
-	handleErr := func(err error) {
+	handleErr := func(message string, err error) {
 		if err != nil {
-			log.Fatalf("Failed to run the service: %v", err)
+			log.Fatalf("%s: %v", message, err)
 		}
 	}
 
 	factories, err := defaults.Components()
-	handleErr(err)
+	handleErr("Failed to build default components", err)
 
 	info := service.ApplicationStartInfo{
 		ExeName:  "otelcol",
@@ -42,8 +42,8 @@ func main() {
 	}
 
 	svc, err := service.New(factories, info)
-	handleErr(err)
+	handleErr("Failed to construct the application", err)
 
 	err = svc.Start()
-	handleErr(err)
+	handleErr("Application run finished with error", err)
 }
