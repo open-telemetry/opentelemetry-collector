e6fa7e1d145c8f78c70787ec5e2408773fc92813
Liz Fong-Jones <lizf@honeycomb.io>
2020-03-26 11:10:41 -0400

    address review feedback

--- internal/tools.go
+++ internal/tools.go
@@ -29,5 +29,6 @@ import (
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/pavius/impi/cmd/impi"
 	_ "github.com/securego/gosec/cmd/gosec"
+	_ "github.com/wadey/gocovmerge"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
