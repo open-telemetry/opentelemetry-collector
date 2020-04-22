69bc931ed6696647b02d98303288ab4e871519f8
Annie Fu <anniefu@google.com>
2020-04-21 19:35:35 -0700

    Rename package to filtermetric

--- internal/processor/filtermetric/config.go
+++ internal/processor/filtermetric/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package metric
+package filtermetric
 
 import (
 	"github.com/open-telemetry/opentelemetry-collector/internal/processor/filterset/factory"
--- internal/processor/filtermetric/doc.go
+++ internal/processor/filtermetric/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package metric is a helper package for processing metrics.
-package metric
+// Package filtermetric is a helper package for processing metrics.
+package filtermetric
