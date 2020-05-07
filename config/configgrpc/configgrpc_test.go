10380502d2eb51c671600f492f6bf59f71553b96
Pavol Loffay <ploffay@redhat.com>
2020-05-07 14:16:42 +0200

    Fix lint
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- config/configgrpc/configgrpc_test.go
+++ config/configgrpc/configgrpc_test.go
@@ -19,9 +19,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBasicGrpcSettings(t *testing.T) {
