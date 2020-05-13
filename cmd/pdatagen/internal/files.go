51a47302b46695173e66a9a35509ddb1403cf133
Bruno Garcia <bruno@brunogarcia.com>
2020-05-13 15:18:26 -0300

    remove year from copyright

--- cmd/pdatagen/internal/files.go
+++ cmd/pdatagen/internal/files.go
@@ -1,4 +1,4 @@
-// Copyright 2020 OpenTelemetry Authors
+// Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ package internal
 
 import "strings"
 
-const header = `// Copyright 2020 OpenTelemetry Authors
+const header = `// Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
