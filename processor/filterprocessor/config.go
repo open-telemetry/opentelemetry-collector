733a8139dd481b11beb0651df3e102f67b89c242
Annie Fu <anniefu@google.com>
2020-05-17 21:43:45 -0700

    filterprocessor for filtering (dropping) incoming metrics by various properties
    traceprocessor for spans to come in a separate PR.

---
+++ processor/filterprocessor/config.go
@@ -0,0 +1,39 @@
+// Copyright 2020 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filterprocessor
+
+import (
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/internal/processor/filtermetric"
+)
+
+// Config defines configuration for Resource processor.
+type Config struct {
+	configmodels.ProcessorSettings `mapstructure:",squash"`
+	Metrics                        MetricFilters `mapstructure:"metrics"`
+}
+
+// MetricFilter filters by Metric properties.
+type MetricFilters struct {
+	// Include match properties describe metrics that should be included in the Collector Service pipeline,
+	// all other metrics should be dropped from further processing.
+	// If both Include and Exclude are specified, Include filtering occurs first.
+	Include *filtermetric.MatchProperties `mapstructure:"include"`
+
+	// Exclude match properties describe metrics that should be excluded from the Collector Service pipeline,
+	// all other metrics should be included.
+	// If both Include and Exclude are specified, Include filtering occurs first.
+	Exclude *filtermetric.MatchProperties `mapstructure:"exclude"`
+}
---
+++ processor/filterprocessor/filter_processor_test.go
@@ -0,0 +1,195 @@
+// Copyright 2020 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filterprocessor
+
+import (
+	"context"
+	"testing"
+
+	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/consumer/consumerdata"
+	"go.opentelemetry.io/collector/internal/processor/filtermetric"
+	"go.opentelemetry.io/collector/internal/processor/filterset"
+	ptest "go.opentelemetry.io/collector/processor/processortest"
+)
+
+var (
+	validFilters = []string{
+		"prefix/.*",
+		"prefix_.*",
+		".*/suffix",
+		".*_suffix",
+		".*/contains/.*",
+		".*_contains_.*",
+		"full/name/match",
+		"full_name_match",
+	}
+)
+
+func createFilterProcessorConfig(matchType filterset.MatchType, inc []string, exc []string) *Config {
+	return &Config{}
+
+}
+
+func TestFilterMetricProcessor(t *testing.T) {
+	inMetricNames := []string{
+		"full_name_match",
+		"not_exact_string_match",
+		"prefix/test/match",
+		"prefix_test_match",
+		"prefixprefix/test/match",
+		"test/match/suffix",
+		"test_match_suffix",
+		"test/match/suffixsuffix",
+		"test/contains/match",
+		"test_contains_match",
+		"random",
+		"full/name/match",
+		"full_name_match", // repeats
+		"not_exact_string_match",
+	}
+
+	regexpMetricsFilterProperties := &filtermetric.MatchProperties{
+		Config: filterset.Config{
+			MatchType: filterset.Regexp,
+		},
+		MetricNames: validFilters,
+	}
+
+	tests := []struct {
+		name  string
+		cfg   *Config
+		inc   *filtermetric.MatchProperties
+		exc   *filtermetric.MatchProperties
+		inMN  []string // input Metric names
+		outMN []string // output Metric names
+	}{
+		{
+			name: "includeFilter",
+			inc:  regexpMetricsFilterProperties,
+			inMN: inMetricNames,
+			outMN: []string{
+				"full_name_match",
+				"prefix/test/match",
+				"prefix_test_match",
+				"prefixprefix/test/match",
+				"test/match/suffix",
+				"test_match_suffix",
+				"test/match/suffixsuffix",
+				"test/contains/match",
+				"test_contains_match",
+				"full/name/match",
+				"full_name_match",
+			},
+		}, {
+			name: "excludeFilter",
+			exc:  regexpMetricsFilterProperties,
+			inMN: inMetricNames,
+			outMN: []string{
+				"not_exact_string_match",
+				"random",
+				"not_exact_string_match",
+			},
+		}, {
+			name: "includeAndExclude",
+			inc:  regexpMetricsFilterProperties,
+			exc: &filtermetric.MatchProperties{
+				Config: filterset.Config{
+					MatchType: filterset.Strict,
+				},
+				MetricNames: []string{
+					"prefix_test_match",
+					"test_contains_match",
+				},
+			},
+			inMN: inMetricNames,
+			outMN: []string{
+				"full_name_match",
+				"prefix/test/match",
+				// "prefix_test_match", excluded by exclude filter
+				"prefixprefix/test/match",
+				"test/match/suffix",
+				"test_match_suffix",
+				"test/match/suffixsuffix",
+				"test/contains/match",
+				// "test_contains_match", excluded by exclude filter
+				"full/name/match",
+				"full_name_match",
+			},
+		}, {
+			name: "emptyFilterInclude",
+			inc: &filtermetric.MatchProperties{
+				Config: filterset.Config{
+					MatchType: filterset.Strict,
+				},
+			},
+			inMN:  inMetricNames,
+			outMN: []string{},
+		}, {
+			name: "emptyFilterExclude",
+			exc: &filtermetric.MatchProperties{
+				Config: filterset.Config{
+					MatchType: filterset.Strict,
+				},
+			},
+			inMN:  inMetricNames,
+			outMN: inMetricNames,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// next stores the results of the filter metric processor
+			next := &ptest.CachingMetricsConsumer{}
+			cfg := &Config{
+				ProcessorSettings: configmodels.ProcessorSettings{
+					TypeVal: typeStr,
+					NameVal: typeStr,
+				},
+				Metrics: MetricFilters{
+					Include: test.inc,
+					Exclude: test.exc,
+				},
+			}
+			fmp, err := newFilterMetricProcessor(next, cfg)
+			assert.NotNil(t, fmp)
+			assert.Nil(t, err)
+
+			md := consumerdata.MetricsData{
+				Metrics: make([]*metricspb.Metric, len(test.inMN)),
+			}
+
+			for idx, in := range test.inMN {
+				md.Metrics[idx] = &metricspb.Metric{
+					MetricDescriptor: &metricspb.MetricDescriptor{
+						Name: in,
+					},
+				}
+			}
+
+			cErr := fmp.ConsumeMetricsData(context.Background(), md)
+			assert.Nil(t, cErr)
+
+			require.Equal(t, len(test.outMN), len(next.Data.Metrics))
+			for idx, out := range next.Data.Metrics {
+				assert.Equal(t, test.outMN[idx], out.MetricDescriptor.Name)
+			}
+		})
+	}
+}
