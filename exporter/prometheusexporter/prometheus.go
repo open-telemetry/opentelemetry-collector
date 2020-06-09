e37f37086515da2afb189804871a7f8b652f20f1
Liam White <liam@tetrate.io>
2020-06-09 08:30:28 -0700

    Handle overlapping metrics from different jobs in prom exporter
    Signed-off-by: Liam White <liam@tetrate.io>

--- exporter/prometheusexporter/prometheus.go
+++ exporter/prometheusexporter/prometheus.go
@@ -15,13 +15,16 @@
 package prometheusexporter
 
 import (
+	"bytes"
 	"context"
 	"errors"
 
 	// TODO: once this repository has been transferred to the
 	// official census-ecosystem location, update this import path.
+
 	"github.com/orijtech/prometheus-go-metrics-exporter"
 
+	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumerdata"
@@ -42,10 +45,38 @@ func (pe *prometheusExporter) Start(_ context.Context, _ component.Host) error {
 }
 
 func (pe *prometheusExporter) ConsumeMetricsData(ctx context.Context, md consumerdata.MetricsData) error {
+	merged := make(map[string]*metricspb.Metric)
 	for _, metric := range md.Metrics {
+		key := metricSignature(metric)
+		merged[key] = merge(merged[key], metric)
+	}
+	for _, metric := range merged {
 		_ = pe.exporter.ExportMetric(ctx, md.Node, md.Resource, metric)
 	}
 	return nil
+}
+
+func merge(a, b *metricspb.Metric) *metricspb.Metric {
+	if a == nil {
+		return b
+	}
+	if b == nil {
+		return a
+	}
+	a.Timeseries = append(a.Timeseries, b.Timeseries...)
+	return a
+}
+
+// Unique identifier of a given promtheus metric
+// Assumes label keys are always in the same order
+func metricSignature(metric *metricspb.Metric) string {
+	var buf bytes.Buffer
+	buf.WriteString(metric.GetMetricDescriptor().GetName())
+	labelKeys := metric.GetMetricDescriptor().GetLabelKeys()
+	for _, labelKey := range labelKeys {
+		buf.WriteString("-" + labelKey.Key)
+	}
+	return buf.String()
 }
 
 // Shutdown stops the exporter and is invoked during shutdown.
