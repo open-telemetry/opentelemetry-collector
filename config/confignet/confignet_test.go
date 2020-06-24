86c87908354795faff54895602450e2446a44ba0
Bogdan Drutu <bogdandrutu@gmail.com>
2020-06-24 16:01:39 -0700

    Add a NetAddr that can be used for HTTP or other protocols
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

---
+++ config/confignet/confignet_test.go
@@ -0,0 +1,57 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package confignet
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNetAddr(t *testing.T) {
+	nas := &NetAddr{
+		Endpoint:  "localhost:0",
+		Transport: "tcp",
+	}
+	ln, err := nas.Listen()
+	assert.NoError(t, err)
+	done := make(chan bool, 1)
+
+	go func() {
+		conn, errGo := ln.Accept()
+		assert.NoError(t, errGo)
+		buf := make([]byte, 10)
+		var numChr int
+		numChr, errGo = conn.Read(buf)
+		assert.NoError(t, errGo)
+		assert.Equal(t, "test", string(buf[:numChr]))
+		assert.NoError(t, conn.Close())
+		done <- true
+	}()
+
+	nac := &NetAddr{
+		Endpoint:  ln.Addr().String(),
+		Transport: "tcp",
+	}
+	var conn net.Conn
+	conn, err = nac.Dial()
+	assert.NoError(t, err)
+	_, err = conn.Write([]byte("test"))
+	assert.NoError(t, err)
+	assert.NoError(t, conn.Close())
+	<-done
+	assert.NoError(t, ln.Close())
+}
