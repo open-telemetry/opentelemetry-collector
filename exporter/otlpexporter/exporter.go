bc5fe23452280f3355faf2529cee534439ef23e5
Bogdan Drutu <bogdandrutu@gmail.com>
2020-06-26 12:19:40 -0700

    Refactor otlp export to allow adding a new httpSender (#1205)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/otlpexporter/exporter.go
+++ exporter/otlpexporter/exporter.go
@@ -36,17 +36,15 @@ type exporterImp struct {
 	// Input configuration.
 	config *Config
 
-	// Prepared dial options.
-	dialOpts []grpc.DialOption
-
 	stopOnce sync.Once
+	w        sender
+}
 
-	// gRPC clients and connection.
-	traceExporter  otlptracecol.TraceServiceClient
-	metricExporter otlpmetriccol.MetricsServiceClient
-	logExporter    otlplogcol.LogServiceClient
-	grpcClientConn *grpc.ClientConn
-	metadata       metadata.MD
+type sender interface {
+	exportTrace(ctx context.Context, request *otlptracecol.ExportTraceServiceRequest) error
+	exportMetrics(ctx context.Context, request *otlpmetriccol.ExportMetricsServiceRequest) error
+	exportLogs(ctx context.Context, request *otlplogcol.ExportLogServiceRequest) error
+	stop() error
 }
 
 var (
@@ -59,20 +57,11 @@ var (
 func newExporter(config *Config) (*exporterImp, error) {
 	e := &exporterImp{}
 	e.config = config
-
-	var err error
-	e.dialOpts, err = e.config.GRPCClientSettings.ToDialOptions()
+	w, err := newGrpcSender(config)
 	if err != nil {
 		return nil, err
 	}
-
-	if e.grpcClientConn, err = grpc.Dial(e.config.GRPCClientSettings.Endpoint, e.dialOpts...); err != nil {
-		return nil, err
-	}
-	e.traceExporter = otlptracecol.NewTraceServiceClient(e.grpcClientConn)
-	e.metricExporter = otlpmetriccol.NewMetricsServiceClient(e.grpcClientConn)
-	e.logExporter = otlplogcol.NewLogServiceClient(e.grpcClientConn)
-	e.metadata = metadata.New(config.GRPCClientSettings.Headers)
+	e.w = w
 	return e, nil
 }
 
@@ -80,25 +69,92 @@ func (e *exporterImp) stop() error {
 	var err error
 	e.stopOnce.Do(func() {
 		// Close the connection.
-		err = e.grpcClientConn.Close()
+		err = e.w.stop()
 	})
 	return err
+}
+
+func (e *exporterImp) exportTrace(ctx context.Context, request *otlptracecol.ExportTraceServiceRequest) error {
+	return e.w.exportTrace(ctx, request)
+}
+
+func (e *exporterImp) exportMetrics(ctx context.Context, request *otlpmetriccol.ExportMetricsServiceRequest) error {
+	return e.w.exportMetrics(ctx, request)
+}
+
+func (e *exporterImp) exportLogs(ctx context.Context, request *otlplogcol.ExportLogServiceRequest) error {
+	return e.w.exportLogs(ctx, request)
+}
+
+type grpcSender struct {
+	// gRPC clients and connection.
+	traceExporter  otlptracecol.TraceServiceClient
+	metricExporter otlpmetriccol.MetricsServiceClient
+	logExporter    otlplogcol.LogServiceClient
+	grpcClientConn *grpc.ClientConn
+	metadata       metadata.MD
+	waitForReady   bool
+}
+
+func newGrpcSender(config *Config) (sender, error) {
+	gs := &grpcSender{}
+	dialOpts, err := config.GRPCClientSettings.ToDialOptions()
+	if err != nil {
+		return nil, err
+	}
+
+	if gs.grpcClientConn, err = grpc.Dial(config.GRPCClientSettings.Endpoint, dialOpts...); err != nil {
+		return nil, err
+	}
+	gs.traceExporter = otlptracecol.NewTraceServiceClient(gs.grpcClientConn)
+	gs.metricExporter = otlpmetriccol.NewMetricsServiceClient(gs.grpcClientConn)
+	gs.logExporter = otlplogcol.NewLogServiceClient(gs.grpcClientConn)
+	gs.metadata = metadata.New(config.GRPCClientSettings.Headers)
+	return gs, nil
+}
+
+func (gs *grpcSender) stop() error {
+	return gs.grpcClientConn.Close()
+}
+
+func (gs *grpcSender) exportTrace(ctx context.Context, request *otlptracecol.ExportTraceServiceRequest) error {
+	return exportRequest(gs.enhanceContext(ctx), func(ctx context.Context) error {
+		_, err := gs.traceExporter.Export(ctx, request, grpc.WaitForReady(gs.waitForReady))
+		return err
+	})
+}
+
+func (gs *grpcSender) exportMetrics(ctx context.Context, request *otlpmetriccol.ExportMetricsServiceRequest) error {
+	return exportRequest(gs.enhanceContext(ctx), func(ctx context.Context) error {
+		_, err := gs.metricExporter.Export(ctx, request, grpc.WaitForReady(gs.waitForReady))
+		return err
+	})
+}
+
+func (gs *grpcSender) exportLogs(ctx context.Context, request *otlplogcol.ExportLogServiceRequest) error {
+	return exportRequest(gs.enhanceContext(ctx), func(ctx context.Context) error {
+		_, err := gs.logExporter.Export(ctx, request, grpc.WaitForReady(gs.waitForReady))
+		return err
+	})
+}
+
+func (gs *grpcSender) enhanceContext(ctx context.Context) context.Context {
+	if gs.metadata.Len() > 0 {
+		return metadata.NewOutgoingContext(ctx, gs.metadata)
+	}
+	return ctx
 }
 
 // Send a trace or metrics request to the server. "perform" function is expected to make
 // the actual gRPC unary call that sends the request. This function implements the
 // common OTLP logic around request handling such as retries and throttling.
-func (e *exporterImp) exportRequest(ctx context.Context, perform func(ctx context.Context) error) error {
+func exportRequest(ctx context.Context, perform func(ctx context.Context) error) error {
 
 	expBackoff := backoff.NewExponentialBackOff()
 
 	// Spend max 15 mins on this operation. This is just a reasonable number that
 	// gives plenty of time for typical quick transient errors to resolve.
 	expBackoff.MaxElapsedTime = time.Minute * 15
-
-	if e.metadata.Len() > 0 {
-		ctx = metadata.NewOutgoingContext(ctx, e.metadata)
-	}
 
 	for {
 		// Send to server.
@@ -149,27 +205,6 @@ func (e *exporterImp) exportRequest(ctx context.Context, perform func(ctx contex
 			// Time to try again.
 		}
 	}
-}
-
-func (e *exporterImp) exportTrace(ctx context.Context, request *otlptracecol.ExportTraceServiceRequest) error {
-	return e.exportRequest(ctx, func(ctx context.Context) error {
-		_, err := e.traceExporter.Export(ctx, request, grpc.WaitForReady(e.config.WaitForReady))
-		return err
-	})
-}
-
-func (e *exporterImp) exportMetrics(ctx context.Context, request *otlpmetriccol.ExportMetricsServiceRequest) error {
-	return e.exportRequest(ctx, func(ctx context.Context) error {
-		_, err := e.metricExporter.Export(ctx, request, grpc.WaitForReady(e.config.WaitForReady))
-		return err
-	})
-}
-
-func (e *exporterImp) exportLogs(ctx context.Context, request *otlplogcol.ExportLogServiceRequest) error {
-	return e.exportRequest(ctx, func(ctx context.Context) error {
-		_, err := e.logExporter.Export(ctx, request, grpc.WaitForReady(e.config.WaitForReady))
-		return err
-	})
 }
 
 func shouldRetry(code codes.Code) bool {
