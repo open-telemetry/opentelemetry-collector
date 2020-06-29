e29ed74617ac2089eca68ffa0310e1cf2c183a6a
Bogdan Drutu <bogdandrutu@gmail.com>
2020-06-29 15:20:17 -0700

    Add tests to componenttest to make codecov happy (#1231)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- component/componenttest/nop_host_test.go
+++ component/componenttest/nop_host_test.go
@@ -18,12 +18,19 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/component"
 )
 
 func TestNewNopHost(t *testing.T) {
-	got := NewNopHost()
-	require.IsType(t, &NopHost{}, got)
+	nh := NewNopHost()
+	require.NotNil(t, nh)
+	require.IsType(t, &NopHost{}, nh)
 
-	got.ReportFatalError(errors.New("TestError"))
+	nh.ReportFatalError(errors.New("TestError"))
+	assert.Nil(t, nh.GetExporters())
+	assert.Nil(t, nh.GetExtensions())
+	assert.Nil(t, nh.GetFactory(component.KindReceiver, "test"))
 }
