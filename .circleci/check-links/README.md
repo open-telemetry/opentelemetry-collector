b97ce638ee43b3afc37bfeea02589672427205d0
Jeff Cheng <jcheng@signalfx.com>
2020-06-29 14:16:10 -0400

    Update check-links to ignore 429 errors

--- .circleci/check-links/README.md
+++ .circleci/check-links/README.md
@@ -1,7 +1,9 @@
 # Markdown Link Checker
 
-Check links in markdown docs with
+Check links in markdown docs based on
 [`markdown-link-check`](https://github.com/tcort/markdown-link-check).
+The app in this repo has been updated to ignore 429 responses (too many
+requests).
 
 Update [config.json](./config.json) to exclude specific links from being
 checked (e.g. examples or links requiring authentication).  See
--- .circleci/check-links/check-links.sh
+++ .circleci/check-links/check-links.sh
@@ -7,12 +7,18 @@ REPO_DIR="$( cd "${SCRIPT_DIR}/../../" && pwd )"
 CIRCLE_BRANCH=${CIRCLE_BRANCH:-}
 CIRCLE_TAG=${CIRCLE_TAG:-}
 
+diff_files="$( git -c "$REPO_DIR" diff HEAD origin/master --name-only )"
+check_all_files=1
+if [[ "$CIRCLE_BRANCH" = "master" ]] || [[ -n "$CIRCLE_TAG" ]] || [[ -n "$( echo "$diff_files" | grep ".circleci/check-links" )" ]]; then
+    check_all_files=0
+fi
+
 nfailed=0
 
 # check all docs in master/tags or new/modified docs in PR
 for md in $(find "$REPO_DIR" -name "*.md" | sort); do
-    if [[ "$CIRCLE_BRANCH" = "master" ]] || [[ -n "$CIRCLE_TAG" ]] || [[ -n "$( git -c "$REPO_DIR" diff HEAD origin/master --name-only | grep "^${md/#$REPO_DIR\//}" )" ]]; then
-        markdown-link-check -c ${SCRIPT_DIR}/config.json -qv "$md" || (( nfailed += $? ))
+    if [[ $check_all_files ]] || [[ -n "$( echo "$diff_files" | grep "^${md/#$REPO_DIR\//}" )" ]]; then
+        node $SCRIPT_DIR/markdown-link-check -c ${SCRIPT_DIR}/config.json -v "$md" || (( nfailed += $? ))
         # wait to scan files so that we don't overload github with requests which may result in 429 responses
         sleep 2
     fi
