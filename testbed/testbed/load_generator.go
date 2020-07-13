3d77741171f20eef4912386b77a983f1d80745d3
Jay Camp <jay.r.camp@gmail.com>
2020-07-13 16:29:51 -0400

    Add parallelism to load generator (#1336)
    * Add parallelism to load generator
    
    This adds the ability for the load generator to send in parallel to increase
    output rate.
    
    * remove options args

--- testbed/testbed/load_generator.go
+++ testbed/testbed/load_generator.go
@@ -63,6 +63,9 @@ type LoadOptions struct {
 
 	// Attributes to add to each generated data item. Can be empty.
 	Attributes map[string]string
+
+	// Parallel specifies how many goroutines to send from.
+	Parallel int
 }
 
 // NewLoadGenerator creates a load generator that sends data using specified sender.
@@ -147,29 +150,45 @@ func (lg *LoadGenerator) generate() {
 		return
 	}
 
-	t := time.NewTicker(time.Second / time.Duration(lg.options.DataItemsPerSecond/lg.options.ItemsPerBatch))
-	defer t.Stop()
-	done := false
-	for !done {
-		select {
-		case <-t.C:
-			switch lg.sender.(type) {
-			case TraceDataSender:
-				lg.generateTrace()
-			case TraceDataSenderOld:
-				lg.generateTraceOld()
-			case MetricDataSender:
-				lg.generateMetrics()
-			case MetricDataSenderOld:
-				lg.generateMetricsOld()
-			default:
-				log.Printf("Invalid type of LoadGenerator sender")
-			}
+	numWorkers := 1
 
-		case <-lg.stopSignal:
-			done = true
-		}
+	if lg.options.Parallel > 0 {
+		numWorkers = lg.options.Parallel
 	}
+
+	var workers sync.WaitGroup
+
+	for i := 0; i < numWorkers; i++ {
+		workers.Add(1)
+
+		go func() {
+			defer workers.Done()
+			t := time.NewTicker(time.Second / time.Duration(lg.options.DataItemsPerSecond/lg.options.ItemsPerBatch/numWorkers))
+			defer t.Stop()
+			for {
+				select {
+				case <-t.C:
+					switch lg.sender.(type) {
+					case TraceDataSender:
+						lg.generateTrace()
+					case TraceDataSenderOld:
+						lg.generateTraceOld()
+					case MetricDataSender:
+						lg.generateMetrics()
+					case MetricDataSenderOld:
+						lg.generateMetricsOld()
+					default:
+						log.Printf("Invalid type of LoadGenerator sender")
+					}
+				case <-lg.stopSignal:
+					return
+				}
+			}
+		}()
+	}
+
+	workers.Wait()
+
 	// Send all pending generated data.
 	lg.sender.Flush()
 }
--- testbed/tests/scenarios.go
+++ testbed/tests/scenarios.go
@@ -129,6 +129,7 @@ func Scenario10kItemsPerSecond(
 	options := testbed.LoadOptions{
 		DataItemsPerSecond: 10_000,
 		ItemsPerBatch:      100,
+		Parallel:           4,
 	}
 	agentProc := &testbed.ChildProcess{}
 	configStr := createConfigYaml(t, sender, receiver, resultDir, processors)
