7eee24f95d256522c3dfed35c2be69c6db521be9
James Bebbington <jbebbington@google.com>
2020-07-16 07:40:28 +1000

    Switch to using perf counters to get disk io metrics on Windows (#1340)

---
+++ receiver/hostmetricsreceiver/internal/scraper/diskscraper/disk_scraper_others_test.go
@@ -0,0 +1,95 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows
+
+package diskscraper
+
+import (
+	"context"
+	"errors"
+	"runtime"
+	"testing"
+
+	"github.com/shirou/gopsutil/disk"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/consumer/pdata"
+)
+
+func TestScrapeMetrics_Others(t *testing.T) {
+	type testCase struct {
+		name              string
+		bootTimeFunc      func() (uint64, error)
+		ioCountersFunc    func(names ...string) (map[string]disk.IOCountersStat, error)
+		expectedStartTime pdata.TimestampUnixNano
+		initializationErr string
+		expectedErr       string
+	}
+
+	testCases := []testCase{
+		{
+			name:              "Validate Start Time",
+			bootTimeFunc:      func() (uint64, error) { return 100, nil },
+			expectedStartTime: 100 * 1e9,
+		},
+		{
+			name:              "Boot Time Error",
+			bootTimeFunc:      func() (uint64, error) { return 0, errors.New("err1") },
+			initializationErr: "err1",
+		},
+		{
+			name:           "Error",
+			ioCountersFunc: func(names ...string) (map[string]disk.IOCountersStat, error) { return nil, errors.New("err2") },
+			expectedErr:    "err2",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			scraper := newDiskScraper(context.Background(), &Config{})
+			if test.bootTimeFunc != nil {
+				scraper.bootTime = test.bootTimeFunc
+			}
+			if test.ioCountersFunc != nil {
+				scraper.ioCounters = test.ioCountersFunc
+			}
+
+			err := scraper.Initialize(context.Background())
+			if test.initializationErr != "" {
+				assert.EqualError(t, err, test.initializationErr)
+				return
+			}
+			require.NoError(t, err, "Failed to initialize disk scraper: %v", err)
+			defer func() { assert.NoError(t, scraper.Close(context.Background())) }()
+
+			metrics, err := scraper.ScrapeMetrics(context.Background())
+			if test.expectedErr != "" {
+				assert.EqualError(t, err, test.expectedErr)
+				return
+			}
+
+			require.NoError(t, err, "Failed to scrape metrics: %v", err)
+
+			assertDiskMetricValid(t, metrics.At(0), diskIODescriptor, test.expectedStartTime)
+			assertDiskMetricValid(t, metrics.At(1), diskOpsDescriptor, test.expectedStartTime)
+			assertDiskMetricValid(t, metrics.At(2), diskTimeDescriptor, test.expectedStartTime)
+
+			if runtime.GOOS == "linux" {
+				assertDiskMetricValid(t, metrics.At(3), diskMergedDescriptor, test.expectedStartTime)
+			}
+		})
+	}
+}
---
+++ receiver/hostmetricsreceiver/internal/scraper/diskscraper/disk_scraper_windows_test.go
@@ -0,0 +1,80 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build windows
+
+package diskscraper
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/receiver/hostmetricsreceiver/internal/windows/pdh"
+)
+
+func TestScrapeMetrics_Error(t *testing.T) {
+	type testCase struct {
+		name                                   string
+		diskReadBytesPerSecCounterReturnValue  interface{}
+		diskWriteBytesPerSecCounterReturnValue interface{}
+		diskReadsPerSecCounterReturnValue      interface{}
+		diskWritesPerSecCounterReturnValue     interface{}
+		expectedErr                            string
+	}
+
+	testCases := []testCase{
+		{
+			name:                                  "readBytesPerSecCounterError",
+			diskReadBytesPerSecCounterReturnValue: errors.New("err1"),
+			expectedErr:                           "err1",
+		},
+		{
+			name:                                   "writeBytesPerSecCounterError",
+			diskWriteBytesPerSecCounterReturnValue: errors.New("err1"),
+			expectedErr:                            "err1",
+		},
+		{
+			name:                              "readsPerSecCounterError",
+			diskReadsPerSecCounterReturnValue: errors.New("err1"),
+			expectedErr:                       "err1",
+		},
+		{
+			name:                               "writesPerSecCounterError",
+			diskWritesPerSecCounterReturnValue: errors.New("err1"),
+			expectedErr:                        "err1",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			scraper := newDiskScraper(context.Background(), &Config{})
+
+			err := scraper.Initialize(context.Background())
+			require.NoError(t, err, "Failed to initialize disk scraper: %v", err)
+			defer func() { assert.NoError(t, scraper.Close(context.Background())) }()
+
+			scraper.diskReadBytesPerSecCounter = pdh.NewMockPerfCounter(test.diskReadBytesPerSecCounterReturnValue)
+			scraper.diskWriteBytesPerSecCounter = pdh.NewMockPerfCounter(test.diskWriteBytesPerSecCounterReturnValue)
+			scraper.diskReadsPerSecCounter = pdh.NewMockPerfCounter(test.diskReadsPerSecCounterReturnValue)
+			scraper.diskWritesPerSecCounter = pdh.NewMockPerfCounter(test.diskWritesPerSecCounterReturnValue)
+
+			_, err = scraper.ScrapeMetrics(context.Background())
+			assert.EqualError(t, err, test.expectedErr)
+		})
+	}
+}
