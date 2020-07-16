181599118b59aeeeec108702c96128a35b38cf9f
James Bebbington <jbebbington@google.com>
2020-07-16 16:53:54 +1000

    Add network device dimension and option to filter by it

--- receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_scraper_test.go
+++ receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_scraper_test.go
@@ -24,28 +24,49 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/internal/processor/filterset"
 	"go.opentelemetry.io/collector/receiver/hostmetricsreceiver/internal"
 )
 
 func TestScrapeMetrics(t *testing.T) {
 	type testCase struct {
-		name              string
-		bootTimeFunc      func() (uint64, error)
-		ioCountersFunc    func(bool) ([]net.IOCountersStat, error)
-		connectionsFunc   func(string) ([]net.ConnectionStat, error)
-		expectedStartTime pdata.TimestampUnixNano
-		initializationErr string
-		expectedErr       string
+		name                 string
+		config               Config
+		bootTimeFunc         func() (uint64, error)
+		ioCountersFunc       func(bool) ([]net.IOCountersStat, error)
+		connectionsFunc      func(string) ([]net.ConnectionStat, error)
+		expectNetworkMetrics bool
+		expectedStartTime    pdata.TimestampUnixNano
+		newErrRegex          string
+		initializationErr    string
+		expectedErr          string
 	}
 
 	testCases := []testCase{
 		{
-			name: "Standard",
+			name:                 "Standard",
+			expectNetworkMetrics: true,
 		},
 		{
-			name:              "Validate Start Time",
-			bootTimeFunc:      func() (uint64, error) { return 100, nil },
-			expectedStartTime: 100 * 1e9,
+			name:                 "Validate Start Time",
+			bootTimeFunc:         func() (uint64, error) { return 100, nil },
+			expectNetworkMetrics: true,
+			expectedStartTime:    100 * 1e9,
+		},
+		{
+			name:                 "Include Filter that matches nothing",
+			config:               Config{Include: MatchConfig{filterset.Config{MatchType: "strict"}, []string{"@*^#&*$^#)"}}},
+			expectNetworkMetrics: false,
+		},
+		{
+			name:        "Invalid Include Filter",
+			config:      Config{Include: MatchConfig{Interfaces: []string{"test"}}},
+			newErrRegex: "^error creating network interface include filters:",
+		},
+		{
+			name:        "Invalid Exclude Filter",
+			config:      Config{Exclude: MatchConfig{Interfaces: []string{"test"}}},
+			newErrRegex: "^error creating network interface exclude filters:",
 		},
 		{
 			name:              "Boot Time Error",
@@ -66,7 +87,14 @@ func TestScrapeMetrics(t *testing.T) {
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			scraper := newNetworkScraper(context.Background(), &Config{})
+			scraper, err := newNetworkScraper(context.Background(), &test.config)
+			if test.newErrRegex != "" {
+				require.Error(t, err)
+				require.Regexp(t, test.newErrRegex, err)
+				return
+			}
+			require.NoError(t, err, "Failed to create network scraper: %v", err)
+
 			if test.bootTimeFunc != nil {
 				scraper.bootTime = test.bootTimeFunc
 			}
@@ -77,7 +105,7 @@ func TestScrapeMetrics(t *testing.T) {
 				scraper.connections = test.connectionsFunc
 			}
 
-			err := scraper.Initialize(context.Background())
+			err = scraper.Initialize(context.Background())
 			if test.initializationErr != "" {
 				assert.EqualError(t, err, test.initializationErr)
 				return
@@ -92,14 +120,21 @@ func TestScrapeMetrics(t *testing.T) {
 			}
 			require.NoError(t, err, "Failed to scrape metrics: %v", err)
 
-			assert.Equal(t, 5, metrics.Len())
+			expectedMetricCount := 1
+			if test.expectNetworkMetrics {
+				expectedMetricCount += 4
+			}
+			assert.Equal(t, expectedMetricCount, metrics.Len())
 
-			assertNetworkIOMetricValid(t, metrics.At(0), networkPacketsDescriptor, test.expectedStartTime)
-			assertNetworkIOMetricValid(t, metrics.At(1), networkDroppedPacketsDescriptor, test.expectedStartTime)
-			assertNetworkIOMetricValid(t, metrics.At(2), networkErrorsDescriptor, test.expectedStartTime)
-			assertNetworkIOMetricValid(t, metrics.At(3), networkIODescriptor, test.expectedStartTime)
-
-			assertNetworkTCPConnectionsMetricValid(t, metrics.At(4))
+			idx := 0
+			if test.expectNetworkMetrics {
+				assertNetworkIOMetricValid(t, metrics.At(idx+0), networkPacketsDescriptor, test.expectedStartTime)
+				assertNetworkIOMetricValid(t, metrics.At(idx+1), networkDroppedPacketsDescriptor, test.expectedStartTime)
+				assertNetworkIOMetricValid(t, metrics.At(idx+2), networkErrorsDescriptor, test.expectedStartTime)
+				assertNetworkIOMetricValid(t, metrics.At(idx+3), networkIODescriptor, test.expectedStartTime)
+				idx += 4
+			}
+			assertNetworkTCPConnectionsMetricValid(t, metrics.At(idx+0))
 		})
 	}
 }
@@ -109,7 +144,8 @@ func assertNetworkIOMetricValid(t *testing.T, metric pdata.Metric, descriptor pd
 	if startTime != 0 {
 		internal.AssertInt64MetricStartTimeEquals(t, metric, startTime)
 	}
-	assert.Equal(t, 2, metric.Int64DataPoints().Len())
+	assert.GreaterOrEqual(t, metric.Int64DataPoints().Len(), 2)
+	internal.AssertInt64MetricLabelExists(t, metric, 0, interfaceLabelName)
 	internal.AssertInt64MetricLabelHasValue(t, metric, 0, directionLabelName, transmitDirectionLabelValue)
 	internal.AssertInt64MetricLabelHasValue(t, metric, 1, directionLabelName, receiveDirectionLabelValue)
 }
