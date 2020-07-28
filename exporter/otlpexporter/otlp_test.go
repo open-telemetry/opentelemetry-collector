1f9413915ac4e69bdb7963b3b0aafb8fe4256903
Bogdan Drutu <bogdandrutu@gmail.com>
2020-07-27 17:29:16 -0700

    Give more time for otlp gRPC server to start
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/otlpexporter/otlp_test.go
+++ exporter/otlpexporter/otlp_test.go
@@ -424,7 +424,7 @@ func startServerAndMakeRequest(t *testing.T, exp component.TraceExporter, td pda
 	assert.EqualValues(t, 0, atomic.LoadInt32(&rcv.requestCount))
 
 	// Resend the request, this should succeed.
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	assert.NoError(t, exp.ConsumeTraces(ctx, td))
 	cancel()
 
