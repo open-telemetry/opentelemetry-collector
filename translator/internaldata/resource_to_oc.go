f204d4b86c3edc7a5685549e78c0124705a1fd75
Nail Islamov <nislamov@google.com>
2020-07-30 15:58:03 +1000

    Infer OpenCensus resource type based on OpenTelemetry's semantic conventions

--- translator/internaldata/resource_to_oc.go
+++ translator/internaldata/resource_to_oc.go
@@ -23,10 +23,44 @@ import (
 	occommon "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
 	ocresource "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 	"github.com/golang/protobuf/ptypes"
+	"go.opencensus.io/resource/resourcekeys"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.opentelemetry.io/collector/translator/conventions"
 )
+
+type ocResourceTypeDetector struct {
+	// label presence to check against
+	labelKey string
+	// matching resource type
+	resourceType string
+}
+
+// mapping of label presence to inferred OC resource type
+// NOTE: defined in the priority order (first match wins)
+var attributeToResourceType = []ocResourceTypeDetector{
+	{
+		// See https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/resource/semantic_conventions/container.md
+		labelKey:     resourcekeys.ContainerKeyName,
+		resourceType: resourcekeys.ContainerType,
+	},
+	{
+		// See https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/resource/semantic_conventions/k8s.md#pod
+		labelKey: resourcekeys.K8SKeyPodName,
+		// NOTE: OpenCensus is using "k8s" rather than "k8s.pod" for Pod
+		resourceType: resourcekeys.K8SType,
+	},
+	{
+		// See https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/resource/semantic_conventions/host.md
+		labelKey:     resourcekeys.HostKeyName,
+		resourceType: resourcekeys.HostType,
+	},
+	{
+		// See https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/resource/semantic_conventions/cloud.md
+		labelKey:     resourcekeys.CloudKeyProvider,
+		resourceType: resourcekeys.CloudType,
+	},
+}
 
 func internalResourceToOC(resource pdata.Resource) (*occommon.Node, *ocresource.Resource) {
 	if resource.IsNil() {
@@ -104,6 +138,14 @@ func internalResourceToOC(resource pdata.Resource) (*occommon.Node, *ocresource.
 	})
 	ocResource.Labels = labels
 
+	// If resource type is missing, try to infer it
+	// based on the presence of resource labels (semantic conventions)
+	if ocResource.Type == "" {
+		if resType, ok := inferResourceType(ocResource.Labels); ok {
+			ocResource.Type = resType
+		}
+	}
+
 	return &ocNode, &ocResource
 }
 
@@ -151,4 +193,18 @@ func attributeValueToString(attr pdata.AttributeValue, jsonLike bool) string {
 	}
 
 	// TODO: Add support for ARRAY type.
+}
+
+func inferResourceType(labels map[string]string) (string, bool) {
+	if labels == nil {
+		return "", false
+	}
+
+	for _, detector := range attributeToResourceType {
+		if _, ok := labels[detector.labelKey]; ok {
+			return detector.resourceType, true
+		}
+	}
+
+	return "", false
 }
--- translator/internaldata/resource_to_oc_test.go
+++ translator/internaldata/resource_to_oc_test.go
@@ -21,6 +21,7 @@ import (
 	agenttracepb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/trace/v1"
 	ocresource "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 	"github.com/stretchr/testify/assert"
+	"go.opencensus.io/resource/resourcekeys"
 	"google.golang.org/protobuf/proto"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
@@ -88,6 +89,78 @@ func TestAttributeValueToString(t *testing.T) {
 	v.MapVal().Insert("d", pdata.NewAttributeValueNull())
 	v.MapVal().Insert("e", v)
 	assert.EqualValues(t, `{"a\"\\":"b\"\\","c":123,"d":null,"e":{"a\"\\":"b\"\\","c":123,"d":null}}`, attributeValueToString(v, false))
+}
+
+func TestInferResourceType(t *testing.T) {
+	tests := []struct {
+		name             string
+		labels           map[string]string
+		wantResourceType string
+		wantOk           bool
+	}{
+		{
+			name:   "empty labels",
+			labels: nil,
+			wantOk: false,
+		},
+		{
+			name: "container",
+			labels: map[string]string{
+				resourcekeys.K8SKeyClusterName:   "cluster1",
+				resourcekeys.K8SKeyPodName:       "pod1",
+				resourcekeys.K8SKeyNamespaceName: "namespace1",
+				resourcekeys.ContainerKeyName:    "container-name1",
+				resourcekeys.CloudKeyAccountID:   "proj1",
+				resourcekeys.CloudKeyZone:        "zone1",
+			},
+			wantResourceType: resourcekeys.ContainerType,
+			wantOk:           true,
+		},
+		{
+			name: "pod",
+			labels: map[string]string{
+				resourcekeys.K8SKeyClusterName:   "cluster1",
+				resourcekeys.K8SKeyPodName:       "pod1",
+				resourcekeys.K8SKeyNamespaceName: "namespace1",
+				resourcekeys.CloudKeyZone:        "zone1",
+			},
+			wantResourceType: resourcekeys.K8SType,
+			wantOk:           true,
+		},
+		{
+			name: "host",
+			labels: map[string]string{
+				resourcekeys.K8SKeyClusterName: "cluster1",
+				resourcekeys.CloudKeyZone:      "zone1",
+				resourcekeys.HostKeyName:       "node1",
+			},
+			wantResourceType: resourcekeys.HostType,
+			wantOk:           true,
+		},
+		{
+			name: "gce",
+			labels: map[string]string{
+				resourcekeys.CloudKeyProvider: resourcekeys.CloudProviderGCP,
+				resourcekeys.HostKeyID:        "inst1",
+				resourcekeys.CloudKeyZone:     "zone1",
+			},
+			wantResourceType: resourcekeys.CloudType,
+			wantOk:           true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			resourceType, ok := inferResourceType(tc.labels)
+			if tc.wantOk {
+				assert.True(t, ok)
+				assert.Equal(t, tc.wantResourceType, resourceType)
+			} else {
+				assert.False(t, ok)
+				assert.Equal(t, "", resourceType)
+			}
+		})
+	}
 }
 
 func BenchmarkInternalResourceToOC(b *testing.B) {
