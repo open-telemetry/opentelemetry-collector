91dddb93e824dde29d044df5773d1af51e9673f1
Daniel Bang <bangdb@amazon.com>
2020-07-30 13:43:13 -0500

    Testbed for the config and factory constructions:

--- exporter/cortexexporter/factory.go
+++ exporter/cortexexporter/factory.go
@@ -13,3 +13,41 @@
 // limitations under the License.
 
 package cortexexporter
+
+import (
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "jaeger"
+)
+
+func NewFactory() component.ExporterFactory {
+	return exporterhelper.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		exporterhelper.WithTraces(createTraceExporter))
+}
+
+func createDefaultConfig() configmodels.Exporter {
+	// TODO: Enable the queued settings.
+	qs := exporterhelper.CreateDefaultQueueSettings()
+	qs.Enabled = false
+	return &Config{
+		ExporterSettings: configmodels.ExporterSettings{
+			TypeVal: typeStr,
+			NameVal: typeStr,
+		},
+		TimeoutSettings: exporterhelper.CreateDefaultTimeoutSettings(),
+		RetrySettings:   exporterhelper.CreateDefaultRetrySettings(),
+		QueueSettings:   qs,
+		GRPCClientSettings: configgrpc.GRPCClientSettings{
+			// We almost read 0 bytes, so no need to tune ReadBufferSize.
+			WriteBufferSize: 512 * 1024,
+		},
+	}
+}
--- exporter/cortexexporter/factory_test.go
+++ exporter/cortexexporter/factory_test.go
@@ -13,3 +13,32 @@
 // limitations under the License.
 
 package cortexexporter
+
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"go.opentelemetry.io/collector/component"s
+	"go.opentelemetry.io/collector/config/configcheck"
+	"go.opentelemetry.io/collector/config/configerror"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	params := component.ExporterCreateParams{Logger: zap.NewNop()}
+	_, err := factory.CreateMetricsExporter(context.Background(), params, cfg)
+	assert.Error(t, err, configerror.ErrDataTypeIsNotSupported)
+}
