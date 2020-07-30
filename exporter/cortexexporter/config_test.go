f205e0f184ed1fa26a4b53087e2a142a47971149
Daniel Bang <bangdb@amazon.com>
2020-07-30 13:43:13 -0500

    Testbed for the config and factory constructions:

--- exporter/cortexexporter/config_test.go
+++ exporter/cortexexporter/config_test.go
@@ -1,1 +1,61 @@
 package cortexexporter
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/config/configtest"
+	"go.opentelemetry.io/collector/config/configtls"
+)
+
+func TestLoadConfig(t *testing.T) {
+	factories, err := componenttest.ExampleComponents()
+	assert.NoError(t, err)
+
+	factory := &Factory{}
+	factories.Exporters[typeStr] = factory
+	cfg, err := configtest.LoadConfigFile(t, path.Join(".", "testdata", "config.yaml"), factories)
+
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	e0 := cfg.Exporters["cortex"]
+	assert.Equal(t, e0, factory.CreateDefaultConfig())
+
+	e1 := cfg.Exporters["cortex/2"]
+	assert.Equal(t, e1,
+		&Config{
+			ExporterSettings: configmodels.ExporterSettings{
+				NameVal: "cortex/2",
+				TypeVal: "cortex",
+			},
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Headers: map[string]string{
+					//oof gotta reimplement these params
+				},
+				Endpoint:    "localhost:8888",
+				Compression: "on",
+				TLSSetting: configtls.TLSClientSetting{
+					TLSSetting: configtls.TLSSetting{
+						CAFile: "/var/lib/mycert.pem",
+					},
+					Insecure: false,
+				},
+				Keepalive: &configgrpc.KeepaliveClientConfig{
+					Time:                20,
+					PermitWithoutStream: true,
+					Timeout:             30,
+				},
+				WriteBufferSize: 512 * 1024,
+				BalancerName:    "round_robin",
+			},
+			NumWorkers:        123,
+			ReconnectionDelay: 15,
+		})
+}
--- exporter/cortexexporter/factory_test.go
+++ exporter/cortexexporter/factory_test.go
@@ -1,1 +1,30 @@
 package cortexexporter
+
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"go.opentelemetry.io/collector/component"s
+	"go.opentelemetry.io/collector/config/configcheck"
+	"go.opentelemetry.io/collector/config/configerror"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	params := component.ExporterCreateParams{Logger: zap.NewNop()}
+	_, err := factory.CreateMetricsExporter(context.Background(), params, cfg)
+	assert.Error(t, err, configerror.ErrDataTypeIsNotSupported)
+}
