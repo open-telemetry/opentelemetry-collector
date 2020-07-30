8a943064dd0d74bc59abd63ea1f4e2c16926e149
Daniel Bang <bangdb@amazon.com>
2020-07-30 13:43:13 -0500

    Testbed for the config and factory constructions:

--- exporter/cortexexporter/config.go
+++ exporter/cortexexporter/config.go
@@ -1,1 +1,21 @@
 package cortexexporter
+
+import (
+	prometheus_golang "github.com/prometheus/client_golang/prometheus"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configmodels"
+)
+
+// Config defines configuration for Remote Write exporter.
+type Config struct {
+	configmodels.ExporterSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+
+	// Namespace if set, exports metrics under the provided value.
+	Namespace string `mapstructure:"namespace"`
+
+	// ConstLabels are values that are applied for every exported metric.
+	ConstLabels prometheus_golang.Labels `mapstructure:"const_labels"`
+
+	confighttp.HTTPClientSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+
+}
