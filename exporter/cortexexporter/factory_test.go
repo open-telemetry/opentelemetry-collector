a822d75f60e6bf9577f18571a29450e6a9dab80b
Daniel Bang <bangdb@amazon.com>
2020-08-03 02:51:19 -0500

    some more config and factory stuff:

--- exporter/cortexexporter/factory_test.go
+++ exporter/cortexexporter/factory_test.go
@@ -3,6 +3,7 @@ package cortexexporter
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
@@ -10,6 +11,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configcheck"
 	"go.opentelemetry.io/collector/config/configerror"
+	"go.opentelemetry.io/collector/config/configtls"
 )
 
 func TestCreateDefaultConfig(t *testing.T) {
@@ -26,4 +28,110 @@ func TestCreateMetricsExporter(t *testing.T) {
 	params := component.ExporterCreateParams{Logger: zap.NewNop()}
 	_, err := factory.CreateMetricsExporter(context.Background(), params, cfg)
 	assert.Error(t, err, configerror.ErrDataTypeIsNotSupported)
+
+	tests := []struct {
+		name     string
+		config   Config
+		mustFail bool
+	}{
+		{
+			name: "NoEndpoint",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: "",
+				},
+			},
+			mustFail: true,
+		},
+		{
+			name: "UseSecure",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					TLSSetting: configtls.TLSClientSetting{
+						Insecure: false,
+					},
+				},
+			},
+		},
+		{
+			name: "Keepalive",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					Keepalive: &configgrpc.KeepaliveClientConfig{
+						Time:                30 * time.Second,
+						Timeout:             25 * time.Second,
+						PermitWithoutStream: true,
+					},
+				},
+			},
+		},
+		{
+			name: "Compression",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint:    endpoint,
+					Compression: configgrpc.CompressionGzip,
+				},
+			},
+		},
+		{
+			name: "Headers",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					Headers: map[string]string{
+						"hdr1": "val1",
+						"hdr2": "val2",
+					},
+				},
+			},
+		},
+		{
+			name: "NumConsumers",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+				},
+			},
+		},
+		{
+			name: "CompressionError",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint:    endpoint,
+					Compression: "unknown compression",
+				},
+			},
+			mustFail: true,
+		},
+		{
+			name: "CaCert",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					TLSSetting: configtls.TLSClientSetting{
+						TLSSetting: configtls.TLSSetting{
+							CAFile: "testdata/test_cert.pem",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "CertPemFileError",
+			config: Config{
+				GRPCClientSettings: configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					TLSSetting: configtls.TLSClientSetting{
+						TLSSetting: configtls.TLSSetting{
+							CAFile: "nosuchfile",
+						},
+					},
+				},
+			},
+			mustFail: true,
+		},
+	}
 }
