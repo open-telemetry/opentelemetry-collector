a88335706ed9ce652b496bed606c7830dd6dbeda
Daniel Bang <bangdb@amazon.com>
2020-08-03 03:01:51 -0500

    config and some factory, export skeleton

--- exporter/cortexexporter/factory_test.go
+++ exporter/cortexexporter/factory_test.go
@@ -3,7 +3,6 @@ package cortexexporter
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
@@ -11,7 +10,6 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configcheck"
 	"go.opentelemetry.io/collector/config/configerror"
-	"go.opentelemetry.io/collector/config/configtls"
 )
 
 func TestCreateDefaultConfig(t *testing.T) {
@@ -21,6 +19,7 @@ func TestCreateDefaultConfig(t *testing.T) {
 	assert.NoError(t, configcheck.ValidateConfig(cfg))
 }
 
+// These are absolutely subject to change
 func TestCreateMetricsExporter(t *testing.T) {
 	factory := NewFactory()
 	cfg := factory.CreateDefaultConfig()
@@ -29,109 +28,8 @@ func TestCreateMetricsExporter(t *testing.T) {
 	_, err := factory.CreateMetricsExporter(context.Background(), params, cfg)
 	assert.Error(t, err, configerror.ErrDataTypeIsNotSupported)
 
-	tests := []struct {
-		name     string
-		config   Config
-		mustFail bool
-	}{
-		{
-			name: "NoEndpoint",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: "",
-				},
-			},
-			mustFail: true,
-		},
-		{
-			name: "UseSecure",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-					TLSSetting: configtls.TLSClientSetting{
-						Insecure: false,
-					},
-				},
-			},
-		},
-		{
-			name: "Keepalive",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-					Keepalive: &configgrpc.KeepaliveClientConfig{
-						Time:                30 * time.Second,
-						Timeout:             25 * time.Second,
-						PermitWithoutStream: true,
-					},
-				},
-			},
-		},
-		{
-			name: "Compression",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint:    endpoint,
-					Compression: configgrpc.CompressionGzip,
-				},
-			},
-		},
-		{
-			name: "Headers",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-					Headers: map[string]string{
-						"hdr1": "val1",
-						"hdr2": "val2",
-					},
-				},
-			},
-		},
-		{
-			name: "NumConsumers",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-				},
-			},
-		},
-		{
-			name: "CompressionError",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint:    endpoint,
-					Compression: "unknown compression",
-				},
-			},
-			mustFail: true,
-		},
-		{
-			name: "CaCert",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-					TLSSetting: configtls.TLSClientSetting{
-						TLSSetting: configtls.TLSSetting{
-							CAFile: "testdata/test_cert.pem",
-						},
-					},
-				},
-			},
-		},
-		{
-			name: "CertPemFileError",
-			config: Config{
-				GRPCClientSettings: configgrpc.GRPCClientSettings{
-					Endpoint: endpoint,
-					TLSSetting: configtls.TLSClientSetting{
-						TLSSetting: configtls.TLSSetting{
-							CAFile: "nosuchfile",
-						},
-					},
-				},
-			},
-			mustFail: true,
-		},
-	}
+}
+
+func TestCreateInstanceViaFactory(t *testing.T) {
+	return // Will populate tomorrow
 }
