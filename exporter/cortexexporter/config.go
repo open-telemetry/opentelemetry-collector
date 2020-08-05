abc6339a385e4b3c34ce777ad83d35e48d02b4b1
Daniel Bang <bangdb@amazon.com>
2020-08-05 03:25:49 -0500

    added export and test export and helpers, will comment and annotate tomorrow

--- exporter/cortexexporter/config.go
+++ exporter/cortexexporter/config.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// !!!!!!!!!!!DO NOT REVIEW THIS FILE IN THIS PR!!!!!!!!!!!!!!!!!!!!!!!
 package cortexexporter
 
 import (
