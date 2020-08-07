16553e088f5e1a5331b06fa0fec427ba6ccb192c
Daniel Bang <bangdb@amazon.com>
2020-08-07 10:45:01 -0500

    yay

--- exporter/prometheusremotewriteexporter/factory.go
+++ exporter/prometheusremotewriteexporter/factory.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Implements a factory interface and design pattern to efficiently instantiate metricsExporter
+// instances.
+
 package prometheusremotewriteexporter
 
 import (
@@ -70,7 +73,6 @@ func createMetricsExporter(_ context.Context, _ component.ExporterCreateParams,
 }
 
 func createDefaultConfig() configmodels.Exporter {
-	// TODO: Enable the queued settings.
 	qs := exporterhelper.CreateDefaultQueueSettings()
 	qs.Enabled = false
 
