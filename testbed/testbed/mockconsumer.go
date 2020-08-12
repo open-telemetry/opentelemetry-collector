a1023a58f7b39ead686954594b636f1a3d0b9d84
Shreyas Darwhatkar <darwhs@amazon.com>
2020-08-11 18:42:00 -0700

    Add code for creating mock receivers

--- testbed/testbed/mockconsumer.go
+++ testbed/testbed/mockconsumer.go
@@ -4,6 +4,6 @@ package testbed
 //keeps the count of number of events received by mock receiver.
 //This is mainly usefull for the Exporters that are not have the matching receiver
 type Mock_DataConsumer interface {
-	// MockConsumeData receives metrics and traces and just count the neumber of event received.
+	// MockConsumeData receives metrics and traces and just count the number of event received.
 	Mock_ConsumeData(spansCount int) error
 }
