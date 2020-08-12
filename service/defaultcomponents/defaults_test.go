e8e98b9d1f204ea9d84904a9441e9c67c6783e69
Yang Hu <yhuz@amazon.com>
2020-08-11 21:10:42 -0500

    add prometheus remote write exporter to default components

--- service/defaultcomponents/defaults_test.go
+++ service/defaultcomponents/defaults_test.go
@@ -56,6 +56,7 @@ func TestDefaultComponents(t *testing.T) {
 	expectedExporters := []configmodels.Type{
 		"opencensus",
 		"prometheus",
+		"prometheusremotewrite",
 		"logging",
 		"zipkin",
 		"jaeger",
