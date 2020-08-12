aee0c6b2d7907677ba8777c3a84a98dbced76576
Shreyas Darwhatkar <darwhs@amazon.com>
2020-08-12 15:28:06 -0700

    restructuring interfaces

--- testbed/testbed/mockconsumer.go
+++ testbed/testbed/mockconsumer.go
@@ -3,9 +3,12 @@ package testbed
 // MockDataConsumer is an interface that
 //keeps the count of number of events received by mock receiver.
 //This is mainly useful for the Exporters that are not have the matching receiver
-type MockDataConsumer interface {
+type MockTraceDataConsumer interface {
 	// MockConsumeTraceData receives metrics and traces and just count the number of event received.
 	MockConsumeTraceData(spansCount int) error
+}
+
+type MockMetricDataConsumer interface {
 	// MockConsumeMetricData receives metrics and traces and just count the number of event received.
 	MockConsumeMetricData(metricsCount int) error
 }
