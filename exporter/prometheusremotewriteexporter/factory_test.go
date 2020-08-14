632dcf0434e66f36c0fef9fc5478fd7443edf42e
Yang Hu <yhuz@amazon.com>
2020-08-14 11:21:40 -0500

    Add Prometheus Remote Write Exporter supporting Cortex - factory and config (#1544)
    * finish Test_validateMetrics
    
    * Testbed for the config and factory constructions:
    
    * Testbed for the config and factory constructions, possible README.md for service package
    
    * Testbed oop
    
    * some more config and factory stuff:
    
    * config and some factory, export skeleton
    
    * config and some factory, export skeleton
    
    * work
    
    * no more const labels for config
    
    * add extra line
    
    * add prometheus remote write exporter
    
    * address lint issues
    
    * improve test coverage
    
    * increase coverage
    
    * fix lint error
    
    * add prometheus remote write exporter to default components
    
    * change metric name label
    
    * add conversion from ns to ms
    
    * rename tests
    
    * factory and config code
    
    * Add README.md
    Add explanation to namespace
    Remove unused dependency
    
    * Undo changes in go.mod
    fix format in README
    
    Co-authored-by: Daniel Bang <bangdb@amazon.com>

---
+++ exporter/prometheusremotewriteexporter/factory_test.go
@@ -0,0 +1,90 @@
+// Copyright 2020 The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheusremotewriteexporter
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configcheck"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/config/configtls"
+)
+
+//Tests whether or not the default Exporter factory can instantiate a properly interfaced Exporter with default conditions
+func Test_createDefaultConfig(t *testing.T) {
+	cfg := createDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+//Tests whether or not a correct Metrics Exporter from the default Config parameters
+func Test_createMetricsExporter(t *testing.T) {
+
+	invalidConfig := createDefaultConfig().(*Config)
+	invalidConfig.HTTPClientSettings = confighttp.HTTPClientSettings{}
+	invalidTLSConfig := createDefaultConfig().(*Config)
+	invalidTLSConfig.HTTPClientSettings.TLSSetting = configtls.TLSClientSetting{
+		TLSSetting: configtls.TLSSetting{
+			CAFile:   "non-existent file",
+			CertFile: "",
+			KeyFile:  "",
+		},
+		Insecure:   false,
+		ServerName: "",
+	}
+	tests := []struct {
+		name        string
+		cfg         configmodels.Exporter
+		params      component.ExporterCreateParams
+		returnError bool
+	}{
+		{"success_case",
+			createDefaultConfig(),
+			component.ExporterCreateParams{},
+			false,
+		},
+		{"fail_case",
+			nil,
+			component.ExporterCreateParams{},
+			true,
+		},
+		{"invalid_config_case",
+			invalidConfig,
+			component.ExporterCreateParams{},
+			true,
+		},
+		{"invalid_tls_config_case",
+			invalidTLSConfig,
+			component.ExporterCreateParams{},
+			true,
+		},
+	}
+	// run tests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := createMetricsExporter(context.Background(), tt.params, tt.cfg)
+			if tt.returnError {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
