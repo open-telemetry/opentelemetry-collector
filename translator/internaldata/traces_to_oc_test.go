9bc00079ea814a990e76803ed73c555ad78a94ef
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2020-08-25 18:09:45 -0500

    fix merge error

--- translator/internaldata/traces_to_oc_test.go
+++ translator/internaldata/traces_to_oc_test.go
@@ -22,7 +22,6 @@ import (
 	occommon "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
 	ocresource "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 	octrace "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
-	"github.com/golang/protobuf/ptypes"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
