6a0d26241cf74d7c0f030732cd80835e53393382
Bogdan Drutu <bogdandrutu@gmail.com>
2020-08-30 19:13:44 -0700

    Change processor tests to use the new metrics internal structs (#1680)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- processor/fanoutconnector_test.go
+++ processor/fanoutconnector_test.go
@@ -25,7 +25,6 @@ import (
 	"go.opentelemetry.io/collector/consumer/pdatautil"
 	"go.opentelemetry.io/collector/exporter/exportertest"
 	"go.opentelemetry.io/collector/internal/data/testdata"
-	"go.opentelemetry.io/collector/internal/dataold/testdataold"
 )
 
 func TestTracesProcessorNotMultiplexing(t *testing.T) {
@@ -100,12 +99,12 @@ func TestMetricsProcessorMultiplexing(t *testing.T) {
 	}
 
 	mfc := NewMetricsFanOutConnector(processors)
-	md := testdataold.GenerateMetricDataOneMetric()
+	md := testdata.GenerateMetricsOneMetric()
 
 	var wantMetricsCount = 0
 	for i := 0; i < 2; i++ {
 		wantMetricsCount += md.MetricCount()
-		err := mfc.ConsumeMetrics(context.Background(), pdatautil.MetricsFromOldInternalMetrics(md))
+		err := mfc.ConsumeMetrics(context.Background(), pdatautil.MetricsFromInternalMetrics(md))
 		if err != nil {
 			t.Errorf("Wanted nil got error")
 			return
@@ -115,7 +114,7 @@ func TestMetricsProcessorMultiplexing(t *testing.T) {
 	for _, p := range processors {
 		m := p.(*exportertest.SinkMetricsExporter)
 		assert.Equal(t, wantMetricsCount, m.MetricsCount())
-		assert.EqualValues(t, md, pdatautil.MetricsToOldInternalMetrics(m.AllMetrics()[0]))
+		assert.EqualValues(t, md, pdatautil.MetricsToInternalMetrics(m.AllMetrics()[0]))
 	}
 }
 
@@ -129,12 +128,12 @@ func TestMetricsProcessorWhenOneErrors(t *testing.T) {
 	processors[1].(*exportertest.SinkMetricsExporter).SetConsumeMetricsError(errors.New("my_error"))
 
 	mfc := NewMetricsFanOutConnector(processors)
-	md := testdataold.GenerateMetricDataOneMetric()
+	md := testdata.GenerateMetricsOneMetric()
 
 	var wantMetricsCount = 0
 	for i := 0; i < 2; i++ {
 		wantMetricsCount += md.MetricCount()
-		err := mfc.ConsumeMetrics(context.Background(), pdatautil.MetricsFromOldInternalMetrics(md))
+		err := mfc.ConsumeMetrics(context.Background(), pdatautil.MetricsFromInternalMetrics(md))
 		if err == nil {
 			t.Errorf("Wanted error got nil")
 			return
