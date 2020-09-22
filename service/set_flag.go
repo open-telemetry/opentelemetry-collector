eb6e633b1637afb9ba3fc7ce89fbccb375ce4089
Pavol Loffay <ploffay@redhat.com>
2020-09-22 09:31:54 +0200

    help message
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- service/set_flag.go
+++ service/set_flag.go
@@ -32,7 +32,7 @@ const (
 )
 
 func addSetFlag(flagSet *pflag.FlagSet) {
-	flagSet.StringArray(setFlagName, []string{}, "Set arbitrary component config property. The component has to be defined in the config file and the flag has a higher precedence over config file. Arrays config properties are overridden and maps are joined, note that only a single (first) array property can be set e.g. -set=processors.attributes.actions.key=some_key. Example --set=processors.batch.timeout=2s")
+	flagSet.StringArray(setFlagName, []string{}, "Set arbitrary component config property. The component has to be defined in the config file and the flag has a higher precedence. Array config properties are overridden and maps are joined, note that only a single (first) array property can be set e.g. -set=processors.attributes.actions.key=some_key. Example --set=processors.batch.timeout=2s")
 }
 
 // AddSetFlagProperties overrides properties from set flag(s) in supplied viper instance.
