200d9504a6b152e49f510b0dadc55e37480bee32
Ben Keith <benkeith@splunk.com>
2020-09-22 18:13:16 +0000

    Log Support for attribute Processor
    This complements the trace span support already present.
    
    It shares the same config struct to maintain backwards-compatibility with spans in the cleanest way possible.

--- processor/attributesprocessor/factory.go
+++ processor/attributesprocessor/factory.go
@@ -21,6 +21,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configmodels"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/internal/processor/filterlog"
 	"go.opentelemetry.io/collector/internal/processor/filterspan"
 	"go.opentelemetry.io/collector/processor/processorhelper"
 )
@@ -37,7 +38,8 @@ func NewFactory() component.ProcessorFactory {
 	return processorhelper.NewFactory(
 		typeStr,
 		createDefaultConfig,
-		processorhelper.WithTraces(createTraceProcessor))
+		processorhelper.WithTraces(createTraceProcessor),
+		processorhelper.WithLogs(createLogProcessor))
 }
 
 // Note: This isn't a valid configuration because the processor would do no work.
@@ -76,6 +78,36 @@ func createTraceProcessor(
 	return processorhelper.NewTraceProcessor(
 		cfg,
 		nextConsumer,
-		newAttributesProcessor(attrProc, include, exclude),
+		newSpanAttributesProcessor(attrProc, include, exclude),
+		processorhelper.WithCapabilities(processorCapabilities))
+}
+
+func createLogProcessor(
+	_ context.Context,
+	_ component.ProcessorCreateParams,
+	cfg configmodels.Processor,
+	nextConsumer consumer.LogsConsumer,
+) (component.LogsProcessor, error) {
+	oCfg := cfg.(*Config)
+	if len(oCfg.Actions) == 0 {
+		return nil, fmt.Errorf("error creating \"attributes\" processor due to missing required field \"actions\" of processor %q", cfg.Name())
+	}
+	attrProc, err := processorhelper.NewAttrProc(&oCfg.Settings)
+	if err != nil {
+		return nil, fmt.Errorf("error creating \"attributes\" processor: %w of processor %q", err, cfg.Name())
+	}
+	include, err := filterlog.NewMatcher(oCfg.Include)
+	if err != nil {
+		return nil, err
+	}
+	exclude, err := filterlog.NewMatcher(oCfg.Exclude)
+	if err != nil {
+		return nil, err
+	}
+
+	return processorhelper.NewLogsProcessor(
+		cfg,
+		nextConsumer,
+		newLogAttributesProcessor(attrProc, include, exclude),
 		processorhelper.WithCapabilities(processorCapabilities))
 }
