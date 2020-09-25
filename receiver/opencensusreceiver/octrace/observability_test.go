36e350dbe8c460664eb1fdc8e4d5ac276d15f726
Ben Keith <benkeith@splunk.com>
2020-09-25 15:39:18 +0000

    OpenCensus Flaky Test Fix
    This might help prevent the issue seen in
    https://app.circleci.com/pipelines/github/open-telemetry/opentelemetry-collector/3229/workflows/8455afc6-e388-4c1b-8ec0-025157fac782/jobs/32259/steps.

--- receiver/opencensusreceiver/octrace/observability_test.go
+++ receiver/opencensusreceiver/octrace/observability_test.go
@@ -178,12 +178,12 @@ func (tote *testOCTraceExporter) ExportSpan(sd *trace.SpanData) {
 // TODO: Determine how to do this deterministic.
 func flush(traceSvcDoneFn func()) {
 	// Give it enough time to process the streamed spans.
-	<-time.After(20 * time.Millisecond)
+	<-time.After(40 * time.Millisecond)
 
 	// End the gRPC service to complete the RPC trace so that we
 	// can examine the RPC trace as well.
 	traceSvcDoneFn()
 
 	// Give it some more time to complete the RPC trace and export.
-	<-time.After(20 * time.Millisecond)
+	<-time.After(40 * time.Millisecond)
 }
