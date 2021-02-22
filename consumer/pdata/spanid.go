d71dd31c40afd7e15eacaeb806fe1105aed388e5
Bogdan Drutu <bogdandrutu@gmail.com>
2021-02-22 12:17:58 -0800

    Remove deprecated function IsValid from trace/span ID (#2522)
    Updates https://github.com/open-telemetry/opentelemetry-collector/issues/2488
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- consumer/pdata/spanid.go
+++ consumer/pdata/spanid.go
@@ -39,12 +39,6 @@ func (t SpanID) HexString() string {
 	return data.SpanID(t).HexString()
 }
 
-// IsValid returns true if id contains at leas one non-zero byte.
-// Deprecated: use !IsEmpty() instead.
-func (t SpanID) IsValid() bool {
-	return !data.SpanID(t).IsEmpty()
-}
-
 // IsEmpty returns true if id doesn't contain at least one non-zero byte.
 func (t SpanID) IsEmpty() bool {
 	return data.SpanID(t).IsEmpty()
