e4be6ddbdddc2d3c986db2373ecf7caa55527011
Bogdan Drutu <bogdandrutu@gmail.com>
2021-03-08 16:27:39 -0800

    Temporary ignore fieldalignment
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/prometheusremotewriteexporter/exporter_test.go
+++ exporter/prometheusremotewriteexporter/exporter_test.go
@@ -130,10 +130,9 @@ func Test_Shutdown(t *testing.T) {
 		closeChan: make(chan struct{}),
 	}
 	wg := new(sync.WaitGroup)
-	errChan := make(chan error, 5)
 	err := prwe.Shutdown(context.Background())
 	require.NoError(t, err)
-	errChan = make(chan error, 5)
+	errChan := make(chan error, 5)
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func() {
