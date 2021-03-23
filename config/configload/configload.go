26d01c6b60bf56bd91a8b74e32d81e95e95cff88
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-03-23 15:34:10 +0100

    Add tests for `ToStringMap` and bring back old implementation

--- config/configload/configload.go
+++ config/configload/configload.go
@@ -16,7 +16,8 @@
 package configload
 
 import (
-	"github.com/spf13/cast"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -58,7 +59,53 @@ func (l *Parser) UnmarshalExact(intoCfg interface{}) error {
 	return l.v.UnmarshalExact(intoCfg)
 }
 
+// deepSearch scans deep maps, following the key indexes listed in the
+// sequence "path".
+// The last value is expected to be another map, and is returned.
+//
+// In case intermediate keys do not exist, or map to a non-map value,
+// a new map is created and inserted, and the search continues from there:
+// the initial map "m" may be modified!
+// This function comes from Viper code https://github.com/spf13/viper/blob/5253694/util.go#L201-L230
+// It is used here because of https://github.com/spf13/viper/issues/819
+func deepSearch(m map[string]interface{}, path []string) map[string]interface{} {
+	for _, k := range path {
+		m2, ok := m[k]
+		if !ok {
+			// intermediate key does not exist
+			// => create it and continue from there
+			m3 := make(map[string]interface{})
+			m[k] = m3
+			m = m3
+			continue
+		}
+		m3, ok := m2.(map[string]interface{})
+		if !ok {
+			// intermediate key is a value
+			// => replace with a new map
+			m3 = make(map[string]interface{})
+			m[k] = m3
+		}
+		// continue search from here
+		m = m3
+	}
+	return m
+}
+
 // ToStringMap creates a map[string]interface{} from a Parser.
 func (l *Parser) ToStringMap() map[string]interface{} {
-	return cast.ToStringMap(l.v)
+	// This is equivalent to l.v.AllSettings() but it maps nil values
+	// We can't use AllSettings here because of https://github.com/spf13/viper/issues/819
+
+	m := map[string]interface{}{}
+	// start from the list of keys, and construct the map one value at a time
+	for _, k := range l.v.AllKeys() {
+		value := l.v.Get(k)
+		path := strings.Split(k, KeyDelimiter)
+		lastKey := strings.ToLower(path[len(path)-1])
+		deepestMap := deepSearch(m, path[0:len(path)-1])
+		// set innermost value
+		deepestMap[lastKey] = value
+	}
+	return m
 }
