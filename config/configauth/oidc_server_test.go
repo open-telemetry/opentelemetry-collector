8f710466d14288cdcada0a38c83ea53ee9659acb
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-04-19 19:42:26 +0200

    Update config/configauth/oidc_server_test.go

--- config/configauth/oidc_server_test.go
+++ config/configauth/oidc_server_test.go
@@ -60,8 +60,7 @@ func newOIDCServer() (*oidcServer, error) {
 	})
 	mux.HandleFunc("/.well-known/jwks.json", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		err := json.NewEncoder(w).Encode(jwks)
-		if err != nil {
+		if err := json.NewEncoder(w).Encode(jwks); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
