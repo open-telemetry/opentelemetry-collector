82201fe2baf5139830520654b1cac9d89cb7c7d8
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-04-28 10:18:31 +0200

    Improve godoc for Authenticator

--- config/configauth/authenticator.go
+++ config/configauth/authenticator.go
@@ -39,25 +39,32 @@ type Authenticator interface {
 	// Authenticate checks whether the given headers map contains valid auth data. Successfully authenticated calls will always return a nil error.
 	// When the authentication fails, an error must be returned and the caller must not retry. This function is typically called from interceptors,
 	// on behalf of receivers, but receivers can still call this directly if the usage of interceptors isn't suitable.
+	// The deadline and cancellation given to this function must be respected, but note that authentication data has to be part of the map, not context.
 	Authenticate(ctx context.Context, headers map[string][]string) error
 
 	// UnaryInterceptor is a helper method to provide a gRPC-compatible UnaryInterceptor, typically calling the authenticator's Authenticate method.
+	// Once the authentication succeeds, the interceptor is expected to call the handler.
 	UnaryInterceptor(ctx context.Context, req interface{}, srvInfo *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error)
 
 	// StreamInterceptor is a helper method to provide a gRPC-compatible StreamInterceptor, typically calling the authenticator's Authenticate method.
+	// Once the authentication succeeds, the interceptor is expected to call the handler.
 	StreamInterceptor(srv interface{}, stream grpc.ServerStream, srvInfo *grpc.StreamServerInfo, handler grpc.StreamHandler) error
 }
 
-// AuthenticateFunc defines the signature for the function responsible for performing the authentication based on the context data.
-type AuthenticateFunc func(context.Context, map[string][]string) error
+// AuthenticateFunc defines the signature for the function responsible for performing the authentication based on the given headers map.
+// See Authenticator.Authenticate.
+type AuthenticateFunc func(ctx context.Context, headers map[string][]string) error
 
 // UnaryInterceptorFunc defines the signature for the function intercepting unary gRPC calls.
+// See grpc.UnaryServerInterceptor and Authenticator.UnaryInterceptor.
 type UnaryInterceptorFunc func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler, authenticate AuthenticateFunc) (interface{}, error)
 
-// StreamInterceptorFunc defines the signature for hte function intercepting streaming gRPC calls.
+// StreamInterceptorFunc defines the signature for the function intercepting streaming gRPC calls.
+// See grpc.StreamServerInterceptor and Authenticator.StreamInterceptor.
 type StreamInterceptorFunc func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler, authenticate AuthenticateFunc) error
 
-// DefaultUnaryInterceptor provides a default implementation of a unary inteceptor, useful for most authenticators.
+// DefaultUnaryInterceptor provides a default implementation of a unary inteceptor, useful for most authenticators. It extracts the headers from the incoming
+// request, under the assumption that the credentials will be part of the resulting map.
 func DefaultUnaryInterceptor(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler, authenticate AuthenticateFunc) (interface{}, error) {
 	headers, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
@@ -71,7 +78,8 @@ func DefaultUnaryInterceptor(ctx context.Context, req interface{}, _ *grpc.Unary
 	return handler(ctx, req)
 }
 
-// DefaultStreamInterceptor provides a default implementation of a stream interceptor, useful for most authenticators.
+// DefaultStreamInterceptor provides a default implementation of a stream interceptor, useful for most authenticators. It extracts the headers from the incoming
+// request, under the assumption that the credentials will be part of the resulting map.
 func DefaultStreamInterceptor(srv interface{}, stream grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler, authenticate AuthenticateFunc) error {
 	ctx := stream.Context()
 	headers, ok := metadata.FromIncomingContext(ctx)
