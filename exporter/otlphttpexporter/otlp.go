d9801d1bd71a8b00d4df05648ae08be3d68dcd62
Jaana Dogan <108380+rakyll@users.noreply.github.com>
2021-05-13 09:21:21 -0700

    Remove redundant code from OTLP HTTP exporter (#3159)

--- exporter/otlphttpexporter/otlp.go
+++ exporter/otlphttpexporter/otlp.go
@@ -91,12 +91,7 @@ func (e *exporter) pushTraceData(ctx context.Context, traces pdata.Traces) error
 		return consumererror.Permanent(err)
 	}
 
-	err = e.export(ctx, e.tracesURL, request)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return e.export(ctx, e.tracesURL, request)
 }
 
 func (e *exporter) pushMetricsData(ctx context.Context, metrics pdata.Metrics) error {
@@ -104,13 +99,7 @@ func (e *exporter) pushMetricsData(ctx context.Context, metrics pdata.Metrics) e
 	if err != nil {
 		return consumererror.Permanent(err)
 	}
-
-	err = e.export(ctx, e.metricsURL, request)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return e.export(ctx, e.metricsURL, request)
 }
 
 func (e *exporter) pushLogData(ctx context.Context, logs pdata.Logs) error {
@@ -119,12 +108,7 @@ func (e *exporter) pushLogData(ctx context.Context, logs pdata.Logs) error {
 		return consumererror.Permanent(err)
 	}
 
-	err = e.export(ctx, e.logsURL, request)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return e.export(ctx, e.logsURL, request)
 }
 
 func (e *exporter) export(ctx context.Context, url string, request []byte) error {
