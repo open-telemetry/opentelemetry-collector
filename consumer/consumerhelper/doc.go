59cd81753c3df6c1d9dea1d78cff5d64d402238d
Eddy Lin <elindy26@gmail.com>
2021-05-20 13:49:33 -0400

    Fixed typo in consumerhelper doc.go

--- consumer/consumerhelper/doc.go
+++ consumer/consumerhelper/doc.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package consumerhelpers defines types and functions used to create consumer
+// Package consumerhelper defines types and functions used to create consumer
 // Logs, Metrics, and Traces.
 package consumerhelper
