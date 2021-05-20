6e5437bf9eab90687032ab3e66bd21fd2445f08b
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-05-20 13:42:01 +0200

    lint
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- service/internal/contexttoresourceconsumer/consumer_test.go
+++ service/internal/contexttoresourceconsumer/consumer_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/collector/auth"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/consumer/pdata"
