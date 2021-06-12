eb7620ad23a7e1ab5f95390af3952b4c42cc7eb4
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-06-13 01:54:03 +0300

    receiver/prometheus: add .ToMetric() directly to pdata.Metric
    Adds point-to-point converters for the various types so that
    we can eventually replace with a direct Prometheus to OTLP
    conversion. This change solely focuses on
    
        (*metricFamilyPdata).ToMetric()
    
    Updates #3137

--- receiver/prometheusreceiver/internal/otlp_metricfamily.go
+++ receiver/prometheusreceiver/internal/otlp_metricfamily.go
@@ -15,6 +15,7 @@
 package internal
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 
@@ -23,6 +24,16 @@ import (
 
 	"go.opentelemetry.io/collector/model/pdata"
 )
+
+// MetricFamilyPdata is unit which is corresponding to the metrics items which shared the same TYPE/UNIT/... metadata from
+// a single scrape.
+type MetricFamilyPdata interface {
+	Add(metricName string, ls labels.Labels, t int64, v float64) error
+	IsSameFamily(metricName string) bool
+	ToMetricPdata() (*pdata.Metric, int, int)
+}
+
+var _ MetricFamilyPdata = (*metricFamilyPdata)(nil)
 
 type metricFamilyPdata struct {
 	// We are composing the already present metricFamily to
@@ -77,21 +88,100 @@ func newMetricFamilyPdata(metricName string, mc MetadataCache) MetricFamily {
 	}
 }
 
+func (mg *metricGroupPdata) toDistributionPoint(orderedLabelKeys []string) *pdata.HistogramDataPoint {
+	if !(mg.hasCount) || len(mg.complexValue) == 0 {
+		return nil
+	}
+
+	mg.sortPoints()
+
+	// for OCAgent Proto, the bounds won't include +inf
+	// TODO: (@odeke-em) should we also check OpenTelemetry Pdata for bucket bounds?
+	bounds := make([]float64, len(mg.complexValue)-1)
+	bucketCounts := make([]uint64, len(mg.complexValue))
+
+	for i := 0; i < len(mg.complexValue); i++ {
+		if i != len(mg.complexValue)-1 {
+			// not need to add +inf as bound to oc proto
+			bounds[i] = mg.complexValue[i].boundary
+		}
+		adjustedCount := mg.complexValue[i].value
+		if i != 0 {
+			adjustedCount -= mg.complexValue[i-1].value
+		}
+		bucketCounts[i] = uint64(adjustedCount)
+	}
+
+	point := pdata.NewHistogramDataPoint()
+	point.SetExplicitBounds(bounds)
+	point.SetCount(uint64(mg.count))
+	point.SetSum(mg.sum)
+	point.SetBucketCounts(bucketCounts)
+	point.SetStartTimestamp(pdata.Timestamp(mg.ts))
+	point.SetTimestamp(pdata.Timestamp(mg.ts))
+
+	return &point
+}
+
+func (mg *metricGroupPdata) toSummaryPoint(orderedLabelKeys []string) *pdata.SummaryDataPoint {
+	// expecting count to be provided, however, in the following two cases, they can be missed.
+	// 1. data is corrupted
+	// 2. ignored by startValue evaluation
+	if !(mg.hasCount) {
+		return nil
+	}
+
+	mg.sortPoints()
+
+	quantileValues := pdata.NewValueAtQuantileSlice()
+	for _, p := range mg.complexValue {
+		quantile := pdata.NewValueAtQuantile()
+		quantile.SetValue(p.value)
+		quantile.SetQuantile(p.boundary * 100)
+	}
+
+	point := pdata.NewSummaryDataPoint()
+	quantileValues.CopyTo(point.QuantileValues())
+
+	strMap := populateLabelValuesPdata(orderedLabelKeys, mg.ls)
+	strMap.CopyTo(point.LabelsMap())
+
+	// Based on the summary description from https://prometheus.io/docs/concepts/metric_types/#summary
+	// the quantiles are calculated over a sliding time window, however, the count is the total count of
+	// observations and the corresponding sum is a sum of all observed values, thus the sum and count used
+	// at the global level of the metricspb.SummaryValue
+	point.SetStartTimestamp(pdata.Timestamp(mg.ts))
+	point.SetTimestamp(pdata.Timestamp(mg.ts))
+	point.SetSum(mg.sum)
+	point.SetCount(uint64(mg.count))
+
+	return &point
+}
+
+func populateLabelValuesPdata(orderedKeys []string, ls labels.Labels) pdata.StringMap {
+	strMap := pdata.NewStringMap()
+	lmap := ls.Map()
+	for _, key := range orderedKeys {
+		strMap.Insert(key, lmap[key])
+	}
+	return strMap
+}
+
 // updateLabelKeys is used to store all the label keys of a same metric family in observed order. since prometheus
 // receiver removes any label with empty value before feeding it to an appender, in order to figure out all the labels
 // from the same metric family we will need to keep track of what labels have ever been observed.
 func (mf *metricFamilyPdata) updateLabelKeys(ls labels.Labels) {
 	for _, l := range ls {
-		if isUsefulLabelPdata(mf.mtype, l.Name) {
-			if _, ok := mf.labelKeys[l.Name]; !ok {
-				mf.labelKeys[l.Name] = true
-				// use insertion sort to maintain order
-				i := sort.SearchStrings(mf.labelKeysOrdered, l.Name)
-				mf.labelKeysOrdered = append(mf.labelKeysOrdered, "")
-				copy(mf.labelKeysOrdered[i+1:], mf.labelKeysOrdered[i:])
-				mf.labelKeysOrdered[i] = l.Name
-
-			}
+		if !isUsefulLabelPdata(mf.mtype, l.Name) {
+			continue
+		}
+		if _, ok := mf.labelKeys[l.Name]; !ok {
+			mf.labelKeys[l.Name] = true
+			// use insertion sort to maintain order
+			i := sort.SearchStrings(mf.labelKeysOrdered, l.Name)
+			mf.labelKeysOrdered = append(mf.labelKeysOrdered, "")
+			copy(mf.labelKeysOrdered[i+1:], mf.labelKeysOrdered[i:])
+			mf.labelKeysOrdered[i] = l.Name
 		}
 	}
 }
@@ -152,4 +242,106 @@ func (mf *metricFamilyPdata) Add(metricName string, ls labels.Labels, t int64, v
 	}
 
 	return nil
+}
+
+// getGroups to return groups in insertion order
+func (mf *metricFamilyPdata) getGroups() []*metricGroupPdata {
+	groups := make([]*metricGroupPdata, len(mf.groupOrders))
+	for k, v := range mf.groupOrders {
+		groups[v] = mf.groups[k]
+	}
+	return groups
+}
+
+func (mf *metricFamilyPdata) ToMetricPdata() (metric *pdata.Metric, nTotalTimeseries, droppedTimeseries int) {
+	numTimeseries := 0
+	switch mf.mtype {
+	// not supported currently
+	// case metricspb.MetricDescriptor_GAUGE_DISTRIBUTION:
+	//	return nil
+	case pdata.MetricDataTypeHistogram:
+		histogramDataPoints := pdata.NewHistogramDataPointSlice()
+		for _, mg := range mf.getGroups() {
+			histogramPoint := mg.toDistributionPoint(mf.labelKeysOrdered)
+			if histogramPoint != nil {
+				histogramDataPoints.Append(*histogramPoint)
+			} else {
+				mf.droppedTimeseries++
+			}
+		}
+
+		numTimeseries = histogramDataPoints.Len()
+		if numTimeseries != 0 {
+			pm := pdata.NewMetric()
+			metric = &pm
+			metric.SetDataType(mf.mtype)
+			histogram := metric.Histogram()
+			histogramDataPoints.CopyTo(histogram.DataPoints())
+		}
+
+	case pdata.MetricDataTypeSummary:
+		summaryDataPoints := pdata.NewSummaryDataPointSlice()
+		for _, mg := range mf.getGroups() {
+			summaryPoint := mg.toSummaryPoint(mf.labelKeysOrdered)
+			if summaryPoint != nil {
+				summaryDataPoints.Append(*summaryPoint)
+			} else {
+				mf.droppedTimeseries++
+			}
+		}
+
+		numTimeseries = summaryDataPoints.Len()
+		if numTimeseries != 0 {
+			pm := pdata.NewMetric()
+			metric = &pm
+			metric.SetDataType(mf.mtype)
+			summary := metric.Summary()
+			summaryDataPoints.CopyTo(summary.DataPoints())
+		}
+
+	default:
+		doubleDataPoints := pdata.NewDoubleDataPointSlice()
+		for _, mg := range mf.getGroups() {
+			doublePoint := mg.toDoublePoint(mf.labelKeysOrdered)
+			if doublePoint != nil {
+				doubleDataPoints.Append(*doublePoint)
+			} else {
+				mf.droppedTimeseries++
+			}
+		}
+
+		numTimeseries = doubleDataPoints.Len()
+		if numTimeseries != 0 {
+			pm := pdata.NewMetric()
+			metric = &pm
+			metric.SetDataType(mf.mtype)
+			panic(fmt.Sprintf("We need a specialized converter for %s", mf.mtype))
+		}
+	}
+
+	// note: the total number of timeseries is the length of timeseries plus the number of dropped timeseries.
+	if numTimeseries == 0 {
+		return nil, mf.droppedTimeseries, mf.droppedTimeseries
+	}
+
+	metric.SetName(mf.name)
+	metric.SetDescription(mf.metadata.Help)
+	metric.SetUnit(heuristicalMetricAndKnownUnits(mf.name, mf.metadata.Unit))
+	metric.SetDataType(mf.mtype)
+	return metric, numTimeseries + mf.droppedTimeseries, mf.droppedTimeseries
+}
+
+func (mg *metricGroupPdata) toDoublePoint(orderedLabelKeys []string) *pdata.DoubleDataPoint {
+	var startTs int64
+	// gauge/undefined types has no start time
+	if mg.family.isCumulativeTypePdata() {
+		startTs = mg.ts
+	}
+
+	point := pdata.NewDoubleDataPoint()
+	point.SetStartTimestamp(pdata.Timestamp(startTs))
+	point.SetTimestamp(pdata.Timestamp(mg.ts))
+	point.SetValue(mg.value)
+
+	return &point
 }
