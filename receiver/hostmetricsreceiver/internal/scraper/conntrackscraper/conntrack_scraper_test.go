0c6639fb1c319107ed221d0ed84873b774a81e45
emil.gelman <emil.gelman@gmail.com>
2021-06-16 14:26:00 +0300

    fix test

--- receiver/hostmetricsreceiver/internal/scraper/conntrackscraper/conntrack_scraper_test.go
+++ receiver/hostmetricsreceiver/internal/scraper/conntrackscraper/conntrack_scraper_test.go
@@ -78,8 +78,8 @@ func TestScrape(t *testing.T) {
 			assert.Equal(t, expectedMetricCount, metrics.Len())
 
 			if expectConntrackMetrics {
-				assertNetworkConntrackMetricValid(t, metrics.At(expectedMetricCount+0), metadata.Metrics.SystemConntrackMax.New())
-				assertNetworkConntrackMetricValid(t, metrics.At(expectedMetricCount+1), metadata.Metrics.SystemConntrackMax.New())
+				assertNetworkConntrackMetricValid(t, metrics.At(0), metadata.Metrics.SystemConntrackMax.New())
+				assertNetworkConntrackMetricValid(t, metrics.At(1), metadata.Metrics.SystemConntrackMax.New())
 			}
 			internal.AssertSameTimeStampForAllMetrics(t, metrics)
 		})
