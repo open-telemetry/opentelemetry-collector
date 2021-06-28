9ee7e2d9f18fc1e5845680c7d400b66783f86346
Bogdan Drutu <bogdandrutu@gmail.com>
2021-06-28 14:15:09 -0700

    Remove unnecessary interfaces from pdata (#3506)
    Based on the current experience, they are not necessary.
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- model/pdata/metrics.go
+++ model/pdata/metrics.go
@@ -15,40 +15,10 @@
 package pdata
 
 import (
-	"fmt"
-
 	"go.opentelemetry.io/collector/internal"
 	otlpcollectormetrics "go.opentelemetry.io/collector/internal/data/protogen/collector/metrics/v1"
 	otlpmetrics "go.opentelemetry.io/collector/internal/data/protogen/metrics/v1"
 )
-
-// MetricsDecoder is an interface to decode bytes into protocol-specific data model.
-type MetricsDecoder interface {
-	// DecodeMetrics decodes bytes into protocol-specific data model.
-	// If the error is not nil, the returned interface cannot be used.
-	DecodeMetrics(buf []byte) (interface{}, error)
-}
-
-// MetricsEncoder is an interface to encode protocol-specific data model into bytes.
-type MetricsEncoder interface {
-	// EncodeMetrics encodes protocol-specific data model into bytes.
-	// If the error is not nil, the returned bytes slice cannot be used.
-	EncodeMetrics(model interface{}) ([]byte, error)
-}
-
-// FromMetricsTranslator is an interface to translate pdata.Metrics into protocol-specific data model.
-type FromMetricsTranslator interface {
-	// FromMetrics translates pdata.Metrics into protocol-specific data model.
-	// If the error is not nil, the returned pdata.Metrics cannot be used.
-	FromMetrics(md Metrics) (interface{}, error)
-}
-
-// ToMetricsTranslator is an interface to translate a protocol-specific data model into pdata.Traces.
-type ToMetricsTranslator interface {
-	// ToMetrics translates a protocol-specific data model into pdata.Metrics.
-	// If the error is not nil, the returned pdata.Metrics cannot be used.
-	ToMetrics(src interface{}) (Metrics, error)
-}
 
 // MetricsMarshaler marshals pdata.Metrics into bytes.
 type MetricsMarshaler interface {
@@ -57,63 +27,11 @@ type MetricsMarshaler interface {
 	MarshalMetrics(td Metrics) ([]byte, error)
 }
 
-type metricsMarshaler struct {
-	encoder    MetricsEncoder
-	translator FromMetricsTranslator
-}
-
-// NewMetricsMarshaler returns a new MetricsMarshaler.
-func NewMetricsMarshaler(encoder MetricsEncoder, translator FromMetricsTranslator) MetricsMarshaler {
-	return &metricsMarshaler{
-		encoder:    encoder,
-		translator: translator,
-	}
-}
-
-// MarshalMetrics pdata.Metrics into bytes.
-func (t *metricsMarshaler) MarshalMetrics(td Metrics) ([]byte, error) {
-	model, err := t.translator.FromMetrics(td)
-	if err != nil {
-		return nil, fmt.Errorf("converting pdata to model failed: %w", err)
-	}
-	buf, err := t.encoder.EncodeMetrics(model)
-	if err != nil {
-		return nil, fmt.Errorf("marshal failed: %w", err)
-	}
-	return buf, nil
-}
-
 // MetricsUnmarshaler unmarshalls bytes into pdata.Metrics.
 type MetricsUnmarshaler interface {
 	// UnmarshalMetrics the given bytes into pdata.Metrics.
 	// If the error is not nil, the returned pdata.Metrics cannot be used.
 	UnmarshalMetrics(buf []byte) (Metrics, error)
-}
-
-type metricsUnmarshaler struct {
-	decoder    MetricsDecoder
-	translator ToMetricsTranslator
-}
-
-// NewMetricsUnmarshaler returns a new MetricsUnmarshaler.
-func NewMetricsUnmarshaler(decoder MetricsDecoder, translator ToMetricsTranslator) MetricsUnmarshaler {
-	return &metricsUnmarshaler{
-		decoder:    decoder,
-		translator: translator,
-	}
-}
-
-// UnmarshalMetrics bytes into pdata.Metrics. On error pdata.Metrics is invalid.
-func (t *metricsUnmarshaler) UnmarshalMetrics(buf []byte) (Metrics, error) {
-	model, err := t.decoder.DecodeMetrics(buf)
-	if err != nil {
-		return Metrics{}, fmt.Errorf("unmarshal failed: %w", err)
-	}
-	td, err := t.translator.ToMetrics(model)
-	if err != nil {
-		return Metrics{}, fmt.Errorf("converting model to pdata failed: %w", err)
-	}
-	return td, nil
 }
 
 // Metrics is an opaque interface that allows transition to the new internal Metrics data, but also facilitates the
