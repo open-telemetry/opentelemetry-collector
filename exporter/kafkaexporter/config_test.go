8148afcd4643c15b558e86eb50e5cd5c2d0749d2
tydhot <583125614@qq.com>
2021-07-08 18:24:25 +0800

    fix test

--- exporter/kafkaexporter/config_test.go
+++ exporter/kafkaexporter/config_test.go
@@ -55,9 +55,10 @@ func TestLoadConfig(t *testing.T) {
 			NumConsumers: 2,
 			QueueSize:    10,
 		},
-		Topic:    "spans",
-		Encoding: "otlp_proto",
-		Brokers:  []string{"foo:123", "bar:456"},
+		Topic:          "spans",
+		Encoding:       "otlp_proto",
+		MessageMaxSize: 1048576,
+		Brokers:        []string{"foo:123", "bar:456"},
 		Authentication: Authentication{
 			PlainText: &PlainTextConfig{
 				Username: "jdoe",
