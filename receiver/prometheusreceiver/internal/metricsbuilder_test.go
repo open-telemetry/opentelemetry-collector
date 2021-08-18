b89198adf05869216873a9e42f272c5c96770cf2
Grace Wehner <grace.wehner@microsoft.com>
2021-08-18 15:39:37 -0700

    Add test case for where metadata key excludes _total but metric name should still have _total

--- receiver/prometheusreceiver/internal/metricsbuilder_test.go
+++ receiver/prometheusreceiver/internal/metricsbuilder_test.go
@@ -225,6 +225,37 @@ func Test_metricBuilder_counters(t *testing.T) {
 				},
 			},
 		},
+		// Some counters such as "python_gc_collections_total" have metadata key as "python_gc_collections" but still need
+		// to be converted using full metric name as "python_gc_collections_total" to match Prometheus functionality
+		{
+			name: "counter-with-metadata-without-total-suffix",
+			inputs: []*testScrapedPage{
+				{
+					pts: []*testDataPoint{
+						createDataPoint("counter_test_total", 100, "foo", "bar"),
+					},
+				},
+			},
+			wants: [][]*metricspb.Metric{
+				{
+					{
+						MetricDescriptor: &metricspb.MetricDescriptor{
+							Name:      "counter_test_total",
+							Type:      metricspb.MetricDescriptor_CUMULATIVE_DOUBLE,
+							LabelKeys: []*metricspb.LabelKey{{Key: "foo"}}},
+						Timeseries: []*metricspb.TimeSeries{
+							{
+								StartTimestamp: timestampFromMs(startTs),
+								LabelValues:    []*metricspb.LabelValue{{Value: "bar", HasValue: true}},
+								Points: []*metricspb.Point{
+									{Timestamp: timestampFromMs(startTs), Value: &metricspb.Point_DoubleValue{DoubleValue: 100.0}},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 		{
 			name: "two-items",
 			inputs: []*testScrapedPage{
