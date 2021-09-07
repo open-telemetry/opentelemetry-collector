3af4b9efa9b8fdafc205401942d5645c7e00fff7
Alex Boten <aboten@lightstep.com>
2021-09-07 14:38:12 -0700

    revert change to attributeArrayToSlice

--- model/pdata/common.go
+++ model/pdata/common.go
@@ -837,19 +837,23 @@ func AttributeMapToMap(attrMap AttributeMap) map[string]interface{} {
 }
 
 // attributeArrayToSlice creates a slice out of a AnyValueArray.
-func attributeArrayToSlice(attrArray AnyValueArray) []AttributeValue {
-	rawSlice := make([]AttributeValue, 0, attrArray.Len())
+func attributeArrayToSlice(attrArray AnyValueArray) []interface{} {
+	rawSlice := make([]interface{}, 0, attrArray.Len())
 	for i := 0; i < attrArray.Len(); i++ {
 		v := attrArray.At(i)
 		switch v.Type() {
-		case AttributeValueTypeString,
-			AttributeValueTypeInt,
-			AttributeValueTypeDouble,
-			AttributeValueTypeBool,
-			AttributeValueTypeNull:
-			rawSlice = append(rawSlice, v)
+		case AttributeValueTypeString:
+			rawSlice = append(rawSlice, v.StringVal())
+		case AttributeValueTypeInt:
+			rawSlice = append(rawSlice, v.IntVal())
+		case AttributeValueTypeDouble:
+			rawSlice = append(rawSlice, v.DoubleVal())
+		case AttributeValueTypeBool:
+			rawSlice = append(rawSlice, v.BoolVal())
+		case AttributeValueTypeNull:
+			rawSlice = append(rawSlice, nil)
 		default:
-			rawSlice = append(rawSlice, NewAttributeValueString("<Invalid array value>"))
+			rawSlice = append(rawSlice, "<Invalid array value>")
 		}
 	}
 	return rawSlice
