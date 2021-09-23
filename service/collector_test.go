55414594f13b1c99f8e938839d3ecba7f43925af
Bogdan Drutu <bogdandrutu@gmail.com>
2021-09-23 08:37:23 +0300

    Use in-memory provider instead of custom provider (#4089)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- service/collector_test.go
+++ service/collector_test.go
@@ -42,6 +42,26 @@ import (
 	"go.opentelemetry.io/collector/service/parserprovider"
 )
 
+const configStr = `
+receivers:
+  otlp:
+    protocols:
+      grpc:
+exporters:
+  otlp:
+    endpoint: "localhost:4317"
+processors:
+  batch:
+extensions:
+service:
+  extensions:
+  pipelines:
+    traces:
+      receivers: [otlp]
+      processors: [batch]
+      exporters: [otlp]
+`
+
 func TestCollector_Start(t *testing.T) {
 	factories, err := defaultcomponents.Components()
 	require.NoError(t, err)
@@ -147,7 +167,7 @@ func TestCollector_StartAsGoRoutine(t *testing.T) {
 	set := CollectorSettings{
 		BuildInfo:      component.DefaultBuildInfo(),
 		Factories:      factories,
-		ParserProvider: new(minimalParserLoader),
+		ParserProvider: parserprovider.NewInMemory(strings.NewReader(configStr)),
 	}
 	col, err := New(set)
 	require.NoError(t, err)
@@ -233,39 +253,6 @@ func assertZPages(t *testing.T) {
 	}
 }
 
-type minimalParserLoader struct{}
-
-func (*minimalParserLoader) Get(context.Context) (*configparser.ConfigMap, error) {
-	configStr := `
-receivers:
-  otlp:
-    protocols:
-      grpc:
-
-exporters:
-  otlp:
-    endpoint: "localhost:4317"
-
-processors:
-  batch:
-
-extensions:
-
-service:
-  extensions:
-  pipelines:
-    traces:
-      receivers: [otlp]
-      processors: [batch]
-      exporters: [otlp]
-`
-	return configparser.NewConfigMapFromBuffer(strings.NewReader(configStr))
-}
-
-func (*minimalParserLoader) Close(context.Context) error {
-	return nil
-}
-
 type errParserLoader struct {
 	err error
 }
@@ -306,7 +293,7 @@ func TestCollector_reloadService(t *testing.T) {
 		},
 		{
 			name:           "retire_service_ok_load_ok",
-			parserProvider: new(minimalParserLoader),
+			parserProvider: parserprovider.NewInMemory(strings.NewReader(configStr)),
 			service: &service{
 				logger:          zap.NewNop(),
 				builtExporters:  builder.Exporters{},
