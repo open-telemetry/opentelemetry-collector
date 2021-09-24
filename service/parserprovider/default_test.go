49443b47b42d3709e1781aa6835de4291a3156b1
Bogdan Drutu <bogdandrutu@gmail.com>
2021-09-24 08:39:45 -0700

    Polish tests for DefaultMapProvider
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- service/parserprovider/default_test.go
+++ service/parserprovider/default_test.go
@@ -17,123 +17,123 @@ package parserprovider
 import (
 	"context"
 	"flag"
-	"sort"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/collector/config"
-	"go.opentelemetry.io/collector/config/configunmarshaler"
-	"go.opentelemetry.io/collector/processor/batchprocessor"
-	"go.opentelemetry.io/collector/receiver/otlpreceiver"
-	"go.opentelemetry.io/collector/service/defaultcomponents"
 )
 
-func TestDefault(t *testing.T) {
-	factories, err := defaultcomponents.Components()
-	require.NoError(t, err)
-	t.Run("unknown_component", func(t *testing.T) {
-		flags := new(flag.FlagSet)
-		Flags(flags)
-		err = flags.Parse([]string{
-			"--config=testdata/otelcol-config.yaml",
-			"--set=processors.doesnotexist.timeout=2s",
-		})
-		require.NoError(t, err)
-		pl := NewDefaultMapProvider()
-		require.NotNil(t, pl)
-		var cp *config.Map
-		cp, err = pl.Get(context.Background())
-		require.NoError(t, err)
-		require.NotNil(t, cp)
-		var cfg *config.Config
-		cfg, err = configunmarshaler.NewDefault().Unmarshal(cp, factories)
-		require.Error(t, err)
-		require.Nil(t, cfg)
-
-	})
-	t.Run("component_not_added_to_pipeline", func(t *testing.T) {
-		flags := new(flag.FlagSet)
-		Flags(flags)
-		err = flags.Parse([]string{
-			"--config=testdata/otelcol-config.yaml",
-			"--set=processors.batch/foo.timeout=2s",
-		})
-		require.NoError(t, err)
-		pl := NewDefaultMapProvider()
-		require.NotNil(t, pl)
-		var cp *config.Map
-		cp, err = pl.Get(context.Background())
-		require.NoError(t, err)
-		require.NotNil(t, cp)
-		var cfg *config.Config
-		cfg, err = configunmarshaler.NewDefault().Unmarshal(cp, factories)
-		require.NoError(t, err)
-		assert.NotNil(t, cfg)
-		err = cfg.Validate()
-		require.NoError(t, err)
-
-		var processors []string
-		for k := range cfg.Processors {
-			processors = append(processors, k.String())
-		}
-		sort.Strings(processors)
-		// batch/foo is not added to the pipeline
-		assert.Equal(t, []string{"batch", "batch/foo"}, processors)
-		assert.Equal(t, []config.ComponentID{config.NewID("batch")}, cfg.Service.Pipelines["traces"].Processors)
-	})
-	t.Run("ok", func(t *testing.T) {
-		flags := new(flag.FlagSet)
-		Flags(flags)
-		err = flags.Parse([]string{
-			"--config=testdata/otelcol-config.yaml",
-			"--set=processors.batch.timeout=2s",
-			"--set=receivers.otlp.protocols.grpc.endpoint=localhost:12345",
-		})
-		require.NoError(t, err)
-		pl := NewDefaultMapProvider()
-		require.NotNil(t, pl)
-		var cp *config.Map
-		cp, err = pl.Get(context.Background())
-		require.NoError(t, err)
-		require.NotNil(t, cp)
-		var cfg *config.Config
-		cfg, err = configunmarshaler.NewDefault().Unmarshal(cp, factories)
-		require.NoError(t, err)
-		require.NotNil(t, cfg)
-		err = cfg.Validate()
-		require.NoError(t, err)
-
-		batch, hasBatch := cfg.Processors[config.NewID("batch")].(*batchprocessor.Config)
-		require.True(t, hasBatch)
-		assert.Equal(t, time.Second*2, batch.Timeout)
-		otlp, hasOTLP := cfg.Receivers[config.NewID("otlp")].(*otlpreceiver.Config)
-		require.True(t, hasOTLP)
-		assert.Equal(t, "localhost:12345", otlp.GRPC.NetAddr.Endpoint)
-	})
-}
-
-func TestDefault_ComponentDoesNotExist(t *testing.T) {
-	factories, err := defaultcomponents.Components()
-	require.NoError(t, err)
-
+func TestDefaultMapProvider(t *testing.T) {
 	flags := new(flag.FlagSet)
 	Flags(flags)
-	err = flags.Parse([]string{
-		"--config=testdata/otelcol-config.yaml",
-		"--set=processors.batch.timeout=2s",
-		"--set=receivers.otlp.protocols.grpc.endpoint=localhost:12345",
+	err := flags.Parse([]string{
+		"--config=testdata/default-config.yaml",
+		"",
 	})
 	require.NoError(t, err)
+	mp := NewDefaultMapProvider()
+	require.NotNil(t, mp)
+	var cm *config.Map
+	cm, err = mp.Get(context.Background())
+	require.NoError(t, err)
+	require.NotNil(t, cm)
 
-	pl := NewDefaultMapProvider()
-	require.NotNil(t, pl)
-	cp, err := pl.Get(context.Background())
+	expectedMap, err := config.NewMapFromBuffer(strings.NewReader(`
+processors:
+  batch:
+exporters:
+  otlp:
+    endpoint: "localhost:4317"`))
 	require.NoError(t, err)
-	require.NotNil(t, cp)
-	cfg, err := configunmarshaler.NewDefault().Unmarshal(cp, factories)
+	assert.Equal(t, expectedMap, cm)
+
+	assert.NoError(t, mp.Close(context.Background()))
+}
+
+func TestDefaultMapProvider_AddNewConfig(t *testing.T) {
+	flags := new(flag.FlagSet)
+	Flags(flags)
+	err := flags.Parse([]string{
+		"--config=testdata/default-config.yaml",
+		"--set=processors.batch.timeout=2s",
+	})
 	require.NoError(t, err)
-	require.NotNil(t, cfg)
+	mp := NewDefaultMapProvider()
+	require.NotNil(t, mp)
+	var cm *config.Map
+	cm, err = mp.Get(context.Background())
+	require.NoError(t, err)
+	require.NotNil(t, cm)
+
+	expectedMap, err := config.NewMapFromBuffer(strings.NewReader(`
+processors:
+  batch:
+    timeout: 2s
+exporters:
+  otlp:
+    endpoint: "localhost:4317"`))
+	require.NoError(t, err)
+	assert.Equal(t, expectedMap, cm)
+
+	assert.NoError(t, mp.Close(context.Background()))
+}
+
+func TestDefaultMapProvider_OverwriteConfig(t *testing.T) {
+	flags := new(flag.FlagSet)
+	Flags(flags)
+	err := flags.Parse([]string{
+		"--config=testdata/default-config.yaml",
+		"--set=processors.batch.timeout=2s",
+		"--set=exporters.otlp.endpoint=localhost:1234",
+	})
+	require.NoError(t, err)
+	mp := NewDefaultMapProvider()
+	require.NotNil(t, mp)
+	var cm *config.Map
+	cm, err = mp.Get(context.Background())
+	require.NoError(t, err)
+	require.NotNil(t, cm)
+
+	expectedMap, err := config.NewMapFromBuffer(strings.NewReader(`
+processors:
+  batch:
+    timeout: 2s
+exporters:
+  otlp:
+    endpoint: "localhost:1234"`))
+	require.NoError(t, err)
+	assert.Equal(t, expectedMap, cm)
+
+	assert.NoError(t, mp.Close(context.Background()))
+}
+
+func TestDefaultMapProvider_InvalidFile(t *testing.T) {
+	flags := new(flag.FlagSet)
+	Flags(flags)
+	err := flags.Parse([]string{
+		"--config=testdata/otelcol-config.yaml",
+	})
+	require.NoError(t, err)
+	mp := NewDefaultMapProvider()
+	require.NotNil(t, mp)
+	_, err = mp.Get(context.Background())
+	require.Error(t, err)
+
+	assert.NoError(t, mp.Close(context.Background()))
+}
+
+func TestDefaultMapProvider_EmptyFile(t *testing.T) {
+	flags := new(flag.FlagSet)
+	Flags(flags)
+	err := flags.Parse([]string{})
+	require.NoError(t, err)
+	mp := NewDefaultMapProvider()
+	require.NotNil(t, mp)
+	_, err = mp.Get(context.Background())
+	require.Error(t, err)
+
+	assert.NoError(t, mp.Close(context.Background()))
 }
