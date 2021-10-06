0fa6bbcb7c8c066f026c7f63f7ab0593bd70e641
José Carlos Chávez <jcchavezs@gmail.com>
2021-10-06 22:45:47 +0200

    chore: includes porto as a tool.

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -26,6 +26,7 @@ import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/google/addlicense"
+	_ "github.com/jcchavezs/porto/cmd/porto"
 	_ "github.com/mjibson/esc"
 	_ "github.com/ory/go-acc"
 	_ "github.com/pavius/impi/cmd/impi"
