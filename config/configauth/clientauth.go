90f5c8a976eab016abf7b3a83a01f49a9a641c9d
Bogdan Drutu <bogdandrutu@gmail.com>
2021-10-20 14:16:51 -0700

    Refactor configauth, getters use the map instead of iteration
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- config/configauth/clientauth.go
+++ config/configauth/clientauth.go
@@ -15,13 +15,11 @@
 package configauth // import "go.opentelemetry.io/collector/config/configauth"
 
 import (
-	"fmt"
 	"net/http"
 
 	"google.golang.org/grpc/credentials"
 
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/config"
 )
 
 // ClientAuthenticator is an Extension that can be used as an authenticator for the configauth.Authentication option.
@@ -43,36 +41,4 @@ type HTTPClientAuthenticator interface {
 type GRPCClientAuthenticator interface {
 	ClientAuthenticator
 	PerRPCCredentials() (credentials.PerRPCCredentials, error)
-}
-
-// GetHTTPClientAuthenticator attempts to select the appropriate HTTPClientAuthenticator from the list of extensions,
-// based on the component id of the extension. If an authenticator is not found, an error is returned.
-// This should be only used by HTTP clients.
-func GetHTTPClientAuthenticator(extensions map[config.ComponentID]component.Extension,
-	componentID config.ComponentID) (HTTPClientAuthenticator, error) {
-	for id, ext := range extensions {
-		if id == componentID {
-			if auth, ok := ext.(HTTPClientAuthenticator); ok {
-				return auth, nil
-			}
-			return nil, fmt.Errorf("requested authenticator is not for HTTP clients")
-		}
-	}
-	return nil, fmt.Errorf("failed to resolve authenticator %q: %w", componentID.String(), errAuthenticatorNotFound)
-}
-
-// GetGRPCClientAuthenticator attempts to select the appropriate GRPCClientAuthenticator from the list of extensions,
-// based on the component id of the extension. If an authenticator is not found, an error is returned.
-// This should only be used by gRPC clients.
-func GetGRPCClientAuthenticator(extensions map[config.ComponentID]component.Extension,
-	componentID config.ComponentID) (GRPCClientAuthenticator, error) {
-	for id, ext := range extensions {
-		if id == componentID {
-			if auth, ok := ext.(GRPCClientAuthenticator); ok {
-				return auth, nil
-			}
-			return nil, fmt.Errorf("requested authenticator is not for gRPC clients")
-		}
-	}
-	return nil, fmt.Errorf("failed to resolve authenticator %q: %w", componentID.String(), errAuthenticatorNotFound)
 }
