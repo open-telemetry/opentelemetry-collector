e9eac1b55f6bf85af23a601651cb2ce370b2b111
Bogdan Drutu <bogdandrutu@gmail.com>
2021-11-12 15:42:07 -0800

    Update otel dependencies to latest (#4425)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- service/telemetry.go
+++ service/telemetry.go
@@ -26,7 +26,7 @@ import (
 	"go.opencensus.io/stats/view"
 	otelprometheus "go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric/global"
-	export "go.opentelemetry.io/otel/sdk/export/metric"
+	"go.opentelemetry.io/otel/sdk/export/metric/aggregation"
 	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
 	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
 	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
@@ -189,7 +189,7 @@ func (tel *colTelemetry) initOpenTelemetry() (http.Handler, error) {
 			selector.NewWithHistogramDistribution(
 				histogram.WithExplicitBoundaries(config.DefaultHistogramBoundaries),
 			),
-			export.CumulativeExportKindSelector(),
+			aggregation.CumulativeTemporalitySelector(),
 			processor.WithMemory(true),
 		),
 	)
