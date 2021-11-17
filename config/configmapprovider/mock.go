a25482802153f10b68e8bf20107da676317a4fd7
José Carlos Chávez <jcchavezs@gmail.com>
2021-11-18 00:31:03 +0100

    chore: fixes vanity import.

--- config/configmapprovider/mock.go
+++ config/configmapprovider/mock.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package configmapprovider
+package configmapprovider // import "go.opentelemetry.io/collector/config/configmapprovider"
 
 import (
 	"context"
