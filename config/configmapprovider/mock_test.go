79bb7e7f1ff5ec6501109f6061cfd72646746056
Bogdan Drutu <lazy@splunk.com>
2021-11-22 08:18:07 -0800

    Update mock_test.go

--- config/configmapprovider/mock_test.go
+++ config/configmapprovider/mock_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package configmapprovider // import "go.opentelemetry.io/collector/config/configmapprovider"
+package configmapprovider
 
 import (
 	"context"
