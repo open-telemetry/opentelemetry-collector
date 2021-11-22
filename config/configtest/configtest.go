86b69e4db8b2daa74ab281e49a3d12b2f7ba9f12
Tigran Najaryan <tnajaryan@splunk.com>
2021-11-22 17:01:55 -0500

    Delete unneeded Manager and Expand provider

--- config/configtest/configtest.go
+++ config/configtest/configtest.go
@@ -35,7 +35,7 @@ var configFieldTagRegExp = regexp.MustCompile("^[a-z0-9][a-z0-9_]*$")
 // LoadConfig loads a config from file, and does NOT validate the configuration.
 func LoadConfig(fileName string, factories component.Factories) (*config.Config, error) {
 	// Read yaml config from file
-	cp, err := configmapprovider.NewExpand(configmapprovider.NewFile(fileName)).Retrieve(context.Background(), nil)
+	cp, err := configmapprovider.NewFile(fileName).Retrieve(context.Background(), nil)
 	if err != nil {
 		return nil, err
 	}
