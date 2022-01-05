d4b36c68b7fce7220589581016b880593ef10b46
Bogdan Drutu <bogdandrutu@gmail.com>
2022-01-04 16:22:32 -0800

    Move package from internal/configprovider to service/internal/configprovider
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- service/internal/configprovider/expand.go
+++ service/internal/configprovider/expand.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package configprovider // import "go.opentelemetry.io/collector/internal/configprovider"
+package configprovider // import "go.opentelemetry.io/collector/service/internal/configprovider"
 
 import (
 	"context"
