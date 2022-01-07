6a456be5ae754dedd8abd4735db8176767246a91
Hyunuk <dev.hyunuk@gmail.com>
2022-01-07 14:19:54 -0800

    fix: lint error

--- config/configcompression/compressionType.go
+++ config/configcompression/compressionType.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package configcompression
+package configcompression // import "go.opentelemetry.io/collector/config/configcompression"
 
 import "fmt"
 
