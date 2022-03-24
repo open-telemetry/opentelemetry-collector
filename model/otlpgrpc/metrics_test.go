3516b0ba0fc9cc135a6167c4d99714cd2043b221
Bogdan Drutu <bogdandrutu@gmail.com>
2022-03-24 20:28:27 +0200

    Add unittests for grpc otlp client/server (#5081)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- model/otlpgrpc/metrics_test.go
+++ model/otlpgrpc/metrics_test.go
@@ -15,11 +15,23 @@
 package otlpgrpc
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"net"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/test/bufconn"
+
+	"go.opentelemetry.io/collector/model/pdata"
 )
 
 var _ json.Unmarshaler = MetricsResponse{}
@@ -55,4 +67,102 @@ func TestMetricsRequestJSON(t *testing.T) {
 	got, err := mr.MarshalJSON()
 	assert.NoError(t, err)
 	assert.Equal(t, strings.Join(strings.Fields(string(metricsRequestJSON)), ""), string(got))
+}
+
+func TestMetricsRequestJSON_Deprecated(t *testing.T) {
+	mr, err := UnmarshalJSONMetricsRequest(metricsRequestJSON)
+	assert.NoError(t, err)
+	assert.Equal(t, "test_metric", mr.Metrics().ResourceMetrics().At(0).InstrumentationLibraryMetrics().At(0).Metrics().At(0).Name())
+
+	got, err := mr.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, strings.Join(strings.Fields(string(metricsRequestJSON)), ""), string(got))
+}
+
+func TestMetricsGrpc(t *testing.T) {
+	lis := bufconn.Listen(1024 * 1024)
+	s := grpc.NewServer()
+	RegisterMetricsServer(s, &fakeMetricsServer{t: t})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		assert.NoError(t, s.Serve(lis))
+	}()
+	t.Cleanup(func() {
+		s.Stop()
+		wg.Wait()
+	})
+
+	cc, err := grpc.Dial("bufnet",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return lis.Dial()
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, cc.Close())
+	})
+
+	logClient := NewMetricsClient(cc)
+
+	resp, err := logClient.Export(context.Background(), generateMetricsRequest())
+	assert.NoError(t, err)
+	assert.Equal(t, NewMetricsResponse(), resp)
+}
+
+func TestMetricsGrpcError(t *testing.T) {
+	lis := bufconn.Listen(1024 * 1024)
+	s := grpc.NewServer()
+	RegisterMetricsServer(s, &fakeMetricsServer{t: t, err: errors.New("my error")})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		assert.NoError(t, s.Serve(lis))
+	}()
+	t.Cleanup(func() {
+		s.Stop()
+		wg.Wait()
+	})
+
+	cc, err := grpc.Dial("bufnet",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return lis.Dial()
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, cc.Close())
+	})
+
+	logClient := NewMetricsClient(cc)
+	resp, err := logClient.Export(context.Background(), generateMetricsRequest())
+	require.Error(t, err)
+	st, okSt := status.FromError(err)
+	require.True(t, okSt)
+	assert.Equal(t, "my error", st.Message())
+	assert.Equal(t, codes.Unknown, st.Code())
+	assert.Equal(t, MetricsResponse{}, resp)
+}
+
+type fakeMetricsServer struct {
+	t   *testing.T
+	err error
+}
+
+func (f fakeMetricsServer) Export(_ context.Context, request MetricsRequest) (MetricsResponse, error) {
+	assert.Equal(f.t, generateMetricsRequest(), request)
+	return NewMetricsResponse(), f.err
+}
+
+func generateMetricsRequest() MetricsRequest {
+	md := pdata.NewMetrics()
+	md.ResourceMetrics().AppendEmpty().InstrumentationLibraryMetrics().AppendEmpty().Metrics().AppendEmpty().SetName("test_metric")
+
+	mr := NewMetricsRequest()
+	mr.SetMetrics(md)
+	return mr
 }
