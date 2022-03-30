51bf67a0536bb8844e0057ff8f4748957d725263
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2022-03-30 14:00:35 -0400

    Update builder version number to 0.48.0 (#5129)
    Contributes to https://github.com/open-telemetry/opentelemetry-collector/issues/5120

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -25,7 +25,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const defaultOtelColVersion = "0.47.0"
+const defaultOtelColVersion = "0.48.0"
 
 // ErrInvalidGoMod indicates an invalid gomod
 var ErrInvalidGoMod = errors.New("invalid gomod specification for module")
