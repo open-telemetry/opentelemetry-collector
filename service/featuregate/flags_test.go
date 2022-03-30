335875b1b3ea5b303bf5c28797f0052cdbc4fe5c
Bogdan Drutu <bogdandrutu@gmail.com>
2022-03-30 15:54:29 -0700

    Remove deprecated structs/funcs from previous versions
    "pdata" deprecated structs/funcs are kept for 2 versions since external dependencies are using these.
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- service/featuregate/flags_test.go
+++ service/featuregate/flags_test.go
@@ -15,21 +15,10 @@
 package featuregate
 
 import (
-	"flag"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestFlags(t *testing.T) {
-	fs := flag.NewFlagSet("test", flag.ContinueOnError)
-	Flags(fs)
-	assert.Equal(t, gatesList, fs.Lookup(gatesListCfg).Value)
-}
-
-func TestGetFlags(t *testing.T) {
-	assert.Equal(t, gatesList, GetFlags())
-}
 
 func TestFlagValue_basic(t *testing.T) {
 	for _, tc := range []struct {
@@ -106,36 +95,6 @@ func TestFlagValue_withPlus(t *testing.T) {
 			v := FlagValue{}
 			assert.NoError(t, v.Set(tc.expected))
 			assert.Equal(t, tc.input, v)
-		})
-	}
-}
-
-func TestFlagValue_SetSlice(t *testing.T) {
-	for _, tc := range []struct {
-		name     string
-		input    []string
-		expected FlagValue
-	}{
-		{
-			name:     "single item",
-			input:    []string{"foo"},
-			expected: FlagValue{"foo": true},
-		},
-		{
-			name:     "multiple items",
-			input:    []string{"foo", "-bar", "+baz"},
-			expected: FlagValue{"foo": true, "bar": false, "baz": true},
-		},
-		{
-			name:     "repeated items",
-			input:    []string{"foo", "-bar", "-foo"},
-			expected: FlagValue{"foo": true, "bar": false},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			v := FlagValue{}
-			assert.NoError(t, v.SetSlice(tc.input))
-			assert.Equal(t, tc.expected, v)
 		})
 	}
 }
