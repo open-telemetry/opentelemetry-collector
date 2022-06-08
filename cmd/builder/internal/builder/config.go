c65f4d6de4909482dcfe73cf29261dd87748850a
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-06-08 11:09:18 +0200

    Update the collector version in the collector builder to the new release in ./cmd/builder/internal/builder/config.go.

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -25,7 +25,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const defaultOtelColVersion = "0.52.0"
+const defaultOtelColVersion = "0.53.0"
 
 // ErrInvalidGoMod indicates an invalid gomod
 var ErrInvalidGoMod = errors.New("invalid gomod specification for module")
