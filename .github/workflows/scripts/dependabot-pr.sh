5834b9b20bf0612a9ef6d2680494ee8e7e187f21
Alex Boten <aboten@lightstep.com>
2022-06-13 09:40:28 -0700

    [chore] add gh action to consolidate dependabot PR
    This PR is the core repo version of https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/eac0998a9d39121e3647180059acf2dd02dd3f77
    
    This pr adds a workflow to replace the manual work required to commit all the dependabot PRs. The workflow:
    - checks out the code
    - runs .github/workflows/scripts/dependabot-pr.sh which looks for all dependabot PRs and applies the dependency update
    - runs tidy/builds the collector
    - commits a change and creates a PR

---
+++ .github/workflows/scripts/dependabot-pr.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -ex
+
+git config user.name $GITHUB_ACTOR
+git config user.email $GITHUB_ACTOR@users.noreply.github.com
+
+PR_NAME=dependabot-prs/`date +'%Y-%m-%dT%H%M%S'`
+git checkout -b $PR_NAME
+
+IFS=$'\n'
+requests=$(gh pr list --search "author:app/dependabot" --json number,title --template '{{range .}}{{tablerow .title}}{{end}}')
+message=""
+dirs=`find . -type f -name "go.mod" -exec dirname {} \; | sort | egrep  '^./'`
+
+for line in $requests; do
+    if [[ $line != Bump* ]]; then 
+        continue
+    fi
+
+    module=$(echo $line | cut -f 2 -d " ")
+    if [[ $module == go.opentelemetry.io/collector* ]]; then
+        continue
+    fi
+    version=$(echo $line | cut -f 6 -d " ")
+    
+    topdir=`pwd`
+    for dir in $dirs; do
+        echo "checking $dir"
+        cd $dir && if grep -q "$module " go.mod; then go get "$module"@v"$version"; fi
+        cd $topdir
+    done
+    message+=$line
+    message+=$'\n'
+done
+
+make gotidy
+make otelcorecol
+
+git add go.sum go.mod
+git add "**/go.sum" "**/go.mod"
+git commit -m "dependabot updates `date`
+$message"
+git push origin $PR_NAME
+
+gh pr create --title "dependabot updates `date`" --body "$message" -l "Skip Changelog"
