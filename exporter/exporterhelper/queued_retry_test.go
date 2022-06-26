d49b15293d01350d54190b423fc59765d3465f5f
Ziqi Zhao <zhaoziqi9146@gmail.com>
2022-06-27 06:59:02 +0800

    fix unittest
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- exporter/exporterhelper/queued_retry_test.go
+++ exporter/exporterhelper/queued_retry_test.go
@@ -342,11 +342,11 @@ func TestQueuedRetry_QueueMetricsReported(t *testing.T) {
 	be := newBaseExporter(&defaultExporterCfg, componenttest.NewNopExporterCreateSettings(), fromOptions(WithRetry(rCfg), WithQueue(qCfg)), "", nopRequestUnmarshaler())
 	require.NoError(t, be.Start(context.Background(), componenttest.NewNopHost()))
 
+	checkValueForGlobalManager(t, defaultExporterTags, int64(5000), "exporter/queue_capacity")
 	for i := 0; i < 7; i++ {
 		require.NoError(t, be.sender.send(newErrorRequest(context.Background())))
 	}
 	checkValueForGlobalManager(t, defaultExporterTags, int64(7), "exporter/queue_size")
-	checkValueForGlobalManager(t, defaultExporterTags, int64(5000), "exporter/queue_capacity")
 
 	assert.NoError(t, be.Shutdown(context.Background()))
 	checkValueForGlobalManager(t, defaultExporterTags, int64(0), "exporter/queue_size")
