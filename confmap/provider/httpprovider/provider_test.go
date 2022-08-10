f0380a9b5274a542f45664abfa7d63123c886ad5
Junhao Yu <yujunhao@amazon.com>
2022-08-10 16:09:55 -0700

    Removed NewTestProvider/NewTestInvalidProvider/NewTestNonExistProvider and replace with New; Minor tweaks for Empty URI and Non-exist cases; Added one more test for shutdowned server

--- confmap/provider/httpprovider/provider_test.go
+++ confmap/provider/httpprovider/provider_test.go
@@ -25,27 +25,11 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
-// Create a provider mocking httpmapprovider works in normal cases
-func NewTestProvider() confmap.Provider {
-	return &provider{}
-}
-
-// Create a provider mocking httpmapprovider works when the returned config file is invalid
-func NewTestInvalidProvider() confmap.Provider {
-	return &provider{}
-}
-
-// Create a provider mocking httpmapprovider works when there is no corresponding config file according to the given http-uri
-func NewTestNonExistProvider() confmap.Provider {
-	return &provider{}
-}
-
 func TestFunctionalityDownloadFileHTTP(t *testing.T) {
-	fp := NewTestProvider()
+	fp := New()
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		f, err := os.ReadFile("./testdata/otel-config.yaml")
 		if err != nil {
@@ -69,20 +53,16 @@ func TestFunctionalityDownloadFileHTTP(t *testing.T) {
 }
 
 func TestUnsupportedScheme(t *testing.T) {
-	fp := NewTestProvider()
+	fp := New()
 	_, err := fp.Retrieve(context.Background(), "https://...", nil)
 	assert.Error(t, err)
 	assert.NoError(t, fp.Shutdown(context.Background()))
 }
 
 func TestEmptyURI(t *testing.T) {
-	fp := NewTestProvider()
+	fp := New()
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(404)
-		_, err := w.Write([]byte(""))
-		if err != nil {
-			fmt.Println("Write failed: ", err)
-		}
+		w.WriteHeader(400)
 	}))
 	defer ts.Close()
 	_, err := fp.Retrieve(context.Background(), ts.URL, nil)
@@ -90,16 +70,23 @@ func TestEmptyURI(t *testing.T) {
 	require.NoError(t, fp.Shutdown(context.Background()))
 }
 
+func TestRetrieveFromShutdownServer(t *testing.T) {
+	fp := New()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+	}))
+	defer ts.Close()
+	_, err := fp.Retrieve(context.Background(), ts.URL, nil)
+	assert.Error(t, err)
+	require.NoError(t, fp.Shutdown(context.Background()))
+}
+
 func TestNonExistent(t *testing.T) {
-	fp := NewTestNonExistProvider()
+	fp := New()
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		f, err := os.ReadFile("./testdata/nonexist-otel-config.yaml")
 		if err != nil {
 			w.WriteHeader(404)
-			_, innerErr := w.Write([]byte("Cannot find the config file"))
-			if innerErr != nil {
-				fmt.Println("Write failed: ", innerErr)
-			}
 			return
 		}
 		w.WriteHeader(200)
@@ -115,7 +102,7 @@ func TestNonExistent(t *testing.T) {
 }
 
 func TestInvalidYAML(t *testing.T) {
-	fp := NewTestInvalidProvider()
+	fp := New()
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		_, err := w.Write([]byte("wrong : ["))
@@ -130,7 +117,7 @@ func TestInvalidYAML(t *testing.T) {
 }
 
 func TestScheme(t *testing.T) {
-	fp := NewTestProvider()
+	fp := New()
 	assert.Equal(t, "http", fp.Scheme())
 	require.NoError(t, fp.Shutdown(context.Background()))
 }
