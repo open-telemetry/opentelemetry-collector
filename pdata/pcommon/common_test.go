1557e7edc76699cb429dba55aa5258d946ccc604
Bogdan Drutu <bogdandrutu@gmail.com>
2022-09-12 12:18:20 -0700

    [chore] avoid unnecessary copy when doing fromraw (#6050)
    Signed-off-by: Bogdan <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan <bogdandrutu@gmail.com>

--- pdata/pcommon/common_test.go
+++ pdata/pcommon/common_test.go
@@ -1207,7 +1207,8 @@ func TestNewValueFromRaw(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := newValueFromRaw(tt.input)
+			actual := NewValueEmpty()
+			actual.fromRaw(tt.input)
 			assert.Equal(t, tt.expected, actual)
 		})
 	}
