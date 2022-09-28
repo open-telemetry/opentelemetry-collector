30bfd76f06cec953607c625426b12c89860047d8
Alex Boten <aboten@lightstep.com>
2022-09-28 11:36:18 -0700

    prepare release 0.61.0

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -26,7 +26,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const defaultOtelColVersion = "0.60.0"
+const defaultOtelColVersion = "0.61.0"
 
 // ErrInvalidGoMod indicates an invalid gomod
 var ErrInvalidGoMod = errors.New("invalid gomod specification for module")
