8efcb44cc981583a8374149de2fa44a524220b4d
Bogdan Drutu <bogdandrutu@gmail.com>
2022-10-04 13:04:46 -0700

    Move internal/testcomponents to service/internal/testcomponents, reduce scope (#6133)
    Signed-off-by: Bogdan <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan <bogdandrutu@gmail.com>

--- service/internal/testcomponents/example_factories.go
+++ service/internal/testcomponents/example_factories.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testcomponents // import "go.opentelemetry.io/collector/internal/testcomponents"
+package testcomponents // import "go.opentelemetry.io/collector/service/internal/testcomponents"
 
 import (
 	"go.opentelemetry.io/collector/component"
