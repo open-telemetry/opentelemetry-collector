f48556be7e62829ccc34b77628e752b34f136b86
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-10-17 12:47:17 +0200

    [exporter/logging] Deprecated 'loglevel' in favor of 'verbosity'

--- exporter/loggingexporter/logging_exporter_test.go
+++ exporter/loggingexporter/logging_exporter_test.go
@@ -20,10 +20,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/configtelemetry"
 	"go.opentelemetry.io/collector/internal/testdata"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -70,7 +70,7 @@ func TestLoggingLogsExporterNoErrors(t *testing.T) {
 }
 
 func TestLoggingExporterErrors(t *testing.T) {
-	le := newLoggingExporter(zaptest.NewLogger(t), zapcore.DebugLevel)
+	le := newLoggingExporter(zaptest.NewLogger(t), configtelemetry.LevelDetailed)
 	require.NotNil(t, le)
 
 	errWant := errors.New("my error")
