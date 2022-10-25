db0c250c83e61ea2993afe79a0e71e8f6a7f747f
Sakshi Patle <55292176+sakshi1215@users.noreply.github.com>
2022-10-25 08:08:44 -0700

    [chore]: dont set path for go in validate (#6273)
    * fix: dont set path for go in validate
    
    Signed-off-by: sakshi1215 <sakshi.patle121@gmail.com>
    
    * test
    
    Signed-off-by: sakshi1215 <sakshi.patle121@gmail.com>
    
    * fix
    
    Signed-off-by: sakshi1215 <sakshi.patle121@gmail.com>
    
    * add changelog entry
    
    Signed-off-by: sakshi1215 <sakshi.patle121@gmail.com>
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update cmd/builder/internal/builder/config.go
    
    Co-authored-by: Dmitrii Anoshin <anoshindx@gmail.com>
    
    * fix
    
    Signed-off-by: Sakshi Patle <sakshi.patle121@gmail.com>
    
    * fix
    
    Signed-off-by: Sakshi Patle <sakshi.patle121@gmail.com>
    
    Signed-off-by: sakshi1215 <sakshi.patle121@gmail.com>
    Signed-off-by: Sakshi Patle <sakshi.patle121@gmail.com>
    Co-authored-by: Dmitrii Anoshin <anoshindx@gmail.com>

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -90,6 +90,11 @@ func NewDefaultConfig() Config {
 
 // Validate checks whether the current configuration is valid
 func (c *Config) Validate() error {
+	return multierr.Combine(validateModules(c.Extensions), validateModules(c.Receivers), validateModules(c.Exporters), validateModules(c.Processors))
+}
+
+// SetGoPath sets go path
+func (c *Config) SetGoPath() error {
 	if !c.SkipCompilation || !c.SkipGetModules {
 		// #nosec G204
 		if _, err := exec.Command(c.Distribution.Go, "env").CombinedOutput(); err != nil {
@@ -99,11 +104,9 @@ func (c *Config) Validate() error {
 			}
 			c.Distribution.Go = path
 		}
-
 		c.Logger.Info("Using go", zap.String("go-executable", c.Distribution.Go))
 	}
-
-	return multierr.Combine(validateModules(c.Extensions), validateModules(c.Receivers), validateModules(c.Exporters), validateModules(c.Processors))
+	return nil
 }
 
 // ParseModules will parse the Modules entries and populate the missing values
