6a3cbe1aa07819b81ca17f3a7ae37264e0ec0c94
Maureen <amaka013@gmail.com>
2022-11-07 22:08:51 +0100

    vanity import
    Signed-off-by: Maureen <amaka013@gmail.com>

--- service/command_build_info.go
+++ service/command_build_info.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package service
+package service // import "go.opentelemetry.io/collector/service"
 
 import (
 	"fmt"
