5376db4058964b6dc1a3a425bbe3b08744d9391b
Bogdan Drutu <bogdandrutu@gmail.com>
2022-11-22 10:18:38 -0800

    Add recursive validation check for configs (#6545)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- component/config.go
+++ component/config.go
@@ -15,6 +15,10 @@
 package component // import "go.opentelemetry.io/collector/component"
 
 import (
+	"reflect"
+
+	"go.uber.org/multierr"
+
 	"go.opentelemetry.io/collector/confmap"
 )
 
@@ -23,6 +27,10 @@ type Config interface {
 
 	privateConfig()
 }
+
+// As interface types are only used for static typing, a common idiom to find the reflection Type
+// for an interface type Foo is to use a *Foo value.
+var configValidatorType = reflect.TypeOf((*ConfigValidator)(nil)).Elem()
 
 // ConfigValidator defines an optional interface for configurations to implement to do validation.
 type ConfigValidator interface {
@@ -33,12 +41,67 @@ type ConfigValidator interface {
 // ValidateConfig validates a config, by doing this:
 //   - Call Validate on the config itself if the config implements ConfigValidator.
 func ValidateConfig(cfg Config) error {
-	validator, ok := cfg.(ConfigValidator)
-	if !ok {
+	return validate(reflect.ValueOf(cfg))
+}
+
+func validate(v reflect.Value) error {
+	// Validate the value itself.
+	switch v.Kind() {
+	case reflect.Invalid:
 		return nil
+	case reflect.Ptr:
+		return validate(v.Elem())
+	case reflect.Struct:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		// Reflect on the pointed data and check each of its fields.
+		for i := 0; i < v.NumField(); i++ {
+			if !v.Type().Field(i).IsExported() {
+				continue
+			}
+			errs = multierr.Append(errs, validate(v.Field(i)))
+		}
+		return errs
+	case reflect.Slice, reflect.Array:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		// Reflect on the pointed data and check each of its fields.
+		for i := 0; i < v.Len(); i++ {
+			errs = multierr.Append(errs, validate(v.Index(i)))
+		}
+		return errs
+	case reflect.Map:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		iter := v.MapRange()
+		for iter.Next() {
+			errs = multierr.Append(errs, validate(iter.Key()))
+			errs = multierr.Append(errs, validate(iter.Value()))
+		}
+		return errs
+	default:
+		return callValidateIfPossible(v)
+	}
+}
+
+func callValidateIfPossible(v reflect.Value) error {
+	// If the value type implements ConfigValidator just call Validate
+	if v.Type().Implements(configValidatorType) {
+		return v.Interface().(ConfigValidator).Validate()
 	}
 
-	return validator.Validate()
+	// If the pointer type implements ConfigValidator call Validate on the pointer to the current value.
+	if reflect.PtrTo(v.Type()).Implements(configValidatorType) {
+		// If not addressable, then create a new *V pointer and set the value to current v.
+		if !v.CanAddr() {
+			pv := reflect.New(reflect.PtrTo(v.Type()).Elem())
+			pv.Elem().Set(v)
+			v = pv.Elem()
+		}
+		return v.Addr().Interface().(ConfigValidator).Validate()
+	}
+
+	return nil
 }
 
 // Type is the component type as it is used in the config.
