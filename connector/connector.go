8be6bd24f0cd29520b36cf0bf90fa9abafbfb153
Dan Jaglowski <jaglows3@gmail.com>
2022-11-23 08:59:07 -0500

    Add connector factory

--- connector/connector.go
+++ connector/connector.go
@@ -35,3 +35,61 @@ type CreateSettings struct {
 	// BuildInfo can be used by components for informational purposes
 	BuildInfo component.BuildInfo
 }
+
+// Factory is factory interface for connectors.
+//
+// This interface cannot be directly implemented. Implementations must
+// use the NewFactory to implement it.
+type Factory interface {
+	component.Factory
+
+	// CreateDefaultConfig creates the default configuration for the Connector.
+	// This method can be called multiple times depending on the pipeline
+	// configuration and should not cause side-effects that prevent the creation
+	// of multiple instances of the Connector.
+	// The object returned by this method needs to pass the checks implemented by
+	// 'configtest.CheckConfigStruct'. It is recommended to have these checks in the
+	// tests of any implementation of the Factory interface.
+	CreateDefaultConfig() component.Config
+}
+
+// FactoryOption applies changes to Factory.
+type FactoryOption interface {
+	// apply applies the option.
+	apply(o *factory)
+}
+
+var _ FactoryOption = (*factoryOptionFunc)(nil)
+
+// factoryOptionFunc is an FactoryOption created through a function.
+type factoryOptionFunc func(*factory)
+
+func (f factoryOptionFunc) apply(o *factory) {
+	f(o)
+}
+
+// factory implements Factory.
+type factory struct {
+	component.Factory
+	cfgType component.Type
+	component.CreateDefaultConfigFunc
+}
+
+var _ Factory = (*factory)(nil)
+
+// Type returns the type of component.
+func (f *factory) Type() component.Type {
+	return f.cfgType
+}
+
+// NewFactory returns a Factory.
+func NewFactory(cfgType component.Type, createDefaultConfig component.CreateDefaultConfigFunc, options ...FactoryOption) Factory {
+	f := &factory{
+		cfgType:                 cfgType,
+		CreateDefaultConfigFunc: createDefaultConfig,
+	}
+	for _, opt := range options {
+		opt.apply(f)
+	}
+	return f
+}
