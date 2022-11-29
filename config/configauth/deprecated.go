c2805d13f6e44d592e53735a41cb741ef711315e
Ziqi Zhao <zhaoziqi9146@gmail.com>
2022-11-29 14:02:18 +0800

    fix reviews
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- config/configauth/deprecated.go
+++ config/configauth/deprecated.go
@@ -15,11 +15,6 @@
 package configauth // import "go.opentelemetry.io/collector/config/configauth"
 
 import (
-	"net/http"
-
-	"google.golang.org/grpc/credentials"
-
-	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/extension/auth"
 	"go.opentelemetry.io/collector/extension/auth/authtest"
 )
@@ -34,36 +29,26 @@ type ClientOption = auth.ClientOption
 // WithClientStart overrides the default `Start` function for a component.Component.
 // The default always returns nil.
 // Deprecated: [v0.67.0] Use auth.WithClientStart
-func WithClientStart(startFunc component.StartFunc) ClientOption {
-	return auth.WithClientStart(startFunc)
-}
+var WithClientStart = auth.WithClientStart
 
 // WithClientShutdown overrides the default `Shutdown` function for a component.Component.
 // The default always returns nil.
 // Deprecated: [v0.67.0] Use auth.WithClientShutdown
-func WithClientShutdown(shutdownFunc component.ShutdownFunc) ClientOption {
-	return auth.WithClientShutdown(shutdownFunc)
-}
+var WithClientShutdown = auth.WithClientShutdown
 
 // WithClientRoundTripper provides a `RoundTripper` function for this client authenticator.
 // The default round tripper is no-op.
 // Deprecated: [v0.67.0] Use auth.WithClientRoundTripper
-func WithClientRoundTripper(roundTripperFunc func(base http.RoundTripper) (http.RoundTripper, error)) ClientOption {
-	return auth.WithClientRoundTripper(roundTripperFunc)
-}
+var WithClientRoundTripper = auth.WithClientRoundTripper
 
 // WithPerRPCCredentials provides a `PerRPCCredentials` function for this client authenticator.
 // There's no default.
 // Deprecated: [v0.67.0] Use auth.WithPerRPCCredentials
-func WithPerRPCCredentials(perRPCCredentialsFunc func() (credentials.PerRPCCredentials, error)) ClientOption {
-	return auth.WithPerRPCCredentials(perRPCCredentialsFunc)
-}
+var WithPerRPCCredentials = auth.WithPerRPCCredentials
 
 // NewClientAuthenticator returns a ClientAuthenticator configured with the provided options.
 // Deprecated: [v0.67.0] Use auth.NewClient
-func NewClientAuthenticator(options ...ClientOption) ClientAuthenticator {
-	return auth.NewClient(options...)
-}
+var NewClientAuthenticator = auth.NewClient
 
 // Deprecated: [v0.67.0] Use auth.Server
 type ServerAuthenticator = auth.Server
@@ -77,29 +62,21 @@ type Option = auth.Option
 
 // WithAuthenticate specifies which function to use to perform the authentication.
 // Deprecated: [v0.67.0] Use auth.WithAuthenticate
-func WithAuthenticate(authenticateFunc AuthenticateFunc) Option {
-	return auth.WithAuthenticate(authenticateFunc)
-}
+var WithAuthenticate = auth.WithAuthenticate
 
 // WithStart overrides the default `Start` function for a component.Component.
 // The default always returns nil.
 // Deprecated: [v0.67.0] Use auth.WithStart
-func WithStart(startFunc component.StartFunc) Option {
-	return auth.WithStart(startFunc)
-}
+var WithStart = auth.WithStart
 
 // WithShutdown overrides the default `Shutdown` function for a component.Component.
 // The default always returns nil.
 // Deprecated: [v0.67.0] Use auth.WithShutdown
-func WithShutdown(shutdownFunc component.ShutdownFunc) Option {
-	return auth.WithShutdown(shutdownFunc)
-}
+var WithShutdown = auth.WithShutdown
 
 // NewServerAuthenticator returns a ServerAuthenticator configured with the provided options.
 // Deprecated: [v0.67.0] Use auth.NewServer
-func NewServerAuthenticator(options ...Option) ServerAuthenticator {
-	return auth.NewServer(options...)
-}
+var NewServerAuthenticator = auth.NewServer
 
 // Deprecated: [v0.67.0] Use auth.MockClient
 type MockClientAuthenticator = authtest.MockClient
