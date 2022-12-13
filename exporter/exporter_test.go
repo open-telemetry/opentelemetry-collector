4b828628f09e92dab72258e2bad983f2a61ef2ac
Bogdan Drutu <bogdandrutu@gmail.com>
2022-12-13 11:44:06 -0800

    [chore] remove fixed TODO from exporter tests (#6791)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporter_test.go
+++ exporter/exporter_test.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Move tests back to component package after config.*Settings are removed.
-
 package exporter
 
 import (
