e856f1ad155493c26fa10e09cb1b060d71c61c4a
Eric Lin <ericl@splunk.com>
2022-12-21 15:04:40 -0800

    add import comment in test_util

--- exporter/exporterhelper/internal/test_util.go
+++ exporter/exporterhelper/internal/test_util.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package internal
+package internal // import "go.opentelemetry.io/collector/exporter/exporterhelper/internal"
 
 import (
 	"context"
