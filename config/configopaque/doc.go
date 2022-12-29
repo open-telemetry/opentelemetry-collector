2e4cc3a8b38f1f07cb27d9937ebda322ad4c1a54
Jacky CSL <jcslmu@gmail.com>
2022-12-29 14:19:40 +0800

    fix package level comment

--- config/configopaque/doc.go
+++ config/configopaque/doc.go
@@ -15,7 +15,7 @@
 // Package configopaque implements String type alias to mask sensitive information.
 // Use configopaque.String on the type of sensitive fields, to mask the
 // opaque string into a string of '*' of equal length.
-
+//
 // This ensure that no sensitive information is leaked when printing the
 // full Collector configurations.
 package configopaque // import "go.opentelemetry.io/collector/config/configopaque"
