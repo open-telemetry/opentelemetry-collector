2606ce237b42a48ac9773475c016fffa879b8181
Daniel Jaglowski <jaglows3@gmail.com>
2023-01-03 16:44:49 -0500

    [chore] Add component 'Builder' tests (#6862)

--- extension/extension_test.go
+++ extension/extension_test.go
@@ -19,8 +19,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
 )
 
 type nopExtension struct {
@@ -83,5 +85,58 @@ func TestMakeFactoryMap(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, tt.out, out)
 		})
+	}
+}
+
+func TestBuilder(t *testing.T) {
+	const typeStr = "test"
+	defaultCfg := struct{}{}
+	nopExtensionInstance := new(nopExtension)
+	testID := component.NewID(typeStr)
+	unknownID := component.NewID("unknown")
+
+	factories, err := MakeFactoryMap([]Factory{
+		NewFactory(
+			typeStr,
+			func() component.Config { return &defaultCfg },
+			func(ctx context.Context, settings CreateSettings, extension component.Config) (Extension, error) {
+				return nopExtensionInstance, nil
+			},
+			component.StabilityLevelDevelopment),
+	}...)
+	require.NoError(t, err)
+
+	cfgs := map[component.ID]component.Config{testID: defaultCfg, unknownID: defaultCfg}
+	b := NewBuilder(cfgs, factories)
+
+	e, err := b.Create(context.Background(), createSettings(testID))
+	assert.NoError(t, err)
+	assert.NotNil(t, e)
+
+	missingType, err := b.Create(context.Background(), createSettings(unknownID))
+	assert.EqualError(t, err, "extension factory not available for: \"unknown\"")
+	assert.Nil(t, missingType)
+
+	missingCfg, err := b.Create(context.Background(), createSettings(component.NewIDWithName(typeStr, "foo")))
+	assert.EqualError(t, err, "extension \"test/foo\" is not configured")
+	assert.Nil(t, missingCfg)
+}
+
+func TestBuilderFactory(t *testing.T) {
+	factories, err := MakeFactoryMap([]Factory{NewFactory("foo", nil, nil, component.StabilityLevelDevelopment)}...)
+	require.NoError(t, err)
+
+	cfgs := map[component.ID]component.Config{component.NewID("foo"): struct{}{}}
+	b := NewBuilder(cfgs, factories)
+
+	assert.NotNil(t, b.Factory(component.NewID("foo").Type()))
+	assert.Nil(t, b.Factory(component.NewID("bar").Type()))
+}
+
+func createSettings(id component.ID) CreateSettings {
+	return CreateSettings{
+		ID:                id,
+		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
+		BuildInfo:         component.NewDefaultBuildInfo(),
 	}
 }
