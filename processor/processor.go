ba6d664150cbfbcfd5c9a51c6f3fdd93af280833
Daniel Jaglowski <jaglows3@gmail.com>
2023-01-03 21:18:33 -0500

    Remove deprecated component.ProcessorFactory (#6881)

--- processor/processor.go
+++ processor/processor.go
@@ -43,28 +43,137 @@ type CreateSettings = component.ProcessorCreateSettings //nolint:staticcheck
 type Factory = component.ProcessorFactory //nolint:staticcheck
 
 // FactoryOption apply changes to Options.
-type FactoryOption = component.ProcessorFactoryOption //nolint:staticcheck
+type FactoryOption interface {
+	// applyProcessorFactoryOption applies the option.
+	applyProcessorFactoryOption(o *factory)
+}
+
+var _ FactoryOption = (*factoryOptionFunc)(nil)
+
+// factoryOptionFunc is a FactoryOption created through a function.
+type factoryOptionFunc func(*factory)
+
+func (f factoryOptionFunc) applyProcessorFactoryOption(o *factory) {
+	f(o)
+}
 
 // CreateTracesFunc is the equivalent of Factory.CreateTraces().
-type CreateTracesFunc = component.CreateTracesProcessorFunc //nolint:staticcheck
+type CreateTracesFunc func(context.Context, CreateSettings, component.Config, consumer.Traces) (Traces, error)
+
+// CreateTracesProcessor implements Factory.CreateTracesProcessor().
+func (f CreateTracesFunc) CreateTracesProcessor(
+	ctx context.Context,
+	set CreateSettings,
+	cfg component.Config,
+	nextConsumer consumer.Traces) (Traces, error) {
+	if f == nil {
+		return nil, component.ErrDataTypeIsNotSupported
+	}
+	return f(ctx, set, cfg, nextConsumer)
+}
 
 // CreateMetricsFunc is the equivalent of Factory.CreateMetrics().
-type CreateMetricsFunc = component.CreateMetricsProcessorFunc //nolint:staticcheck
+type CreateMetricsFunc func(context.Context, CreateSettings, component.Config, consumer.Metrics) (Metrics, error)
+
+// CreateMetricsProcessor implements Factory.CreateMetricsProcessor().
+func (f CreateMetricsFunc) CreateMetricsProcessor(
+	ctx context.Context,
+	set CreateSettings,
+	cfg component.Config,
+	nextConsumer consumer.Metrics,
+) (Metrics, error) {
+	if f == nil {
+		return nil, component.ErrDataTypeIsNotSupported
+	}
+	return f(ctx, set, cfg, nextConsumer)
+}
 
 // CreateLogsFunc is the equivalent of Factory.CreateLogs().
-type CreateLogsFunc = component.CreateLogsProcessorFunc //nolint:staticcheck
+type CreateLogsFunc func(context.Context, CreateSettings, component.Config, consumer.Logs) (Logs, error)
+
+// CreateLogsProcessor implements Factory.CreateLogsProcessor().
+func (f CreateLogsFunc) CreateLogsProcessor(
+	ctx context.Context,
+	set CreateSettings,
+	cfg component.Config,
+	nextConsumer consumer.Logs,
+) (Logs, error) {
+	if f == nil {
+		return nil, component.ErrDataTypeIsNotSupported
+	}
+	return f(ctx, set, cfg, nextConsumer)
+}
+
+type factory struct {
+	component.Factory
+	cfgType component.Type
+	component.CreateDefaultConfigFunc
+	CreateTracesFunc
+	tracesStabilityLevel component.StabilityLevel
+	CreateMetricsFunc
+	metricsStabilityLevel component.StabilityLevel
+	CreateLogsFunc
+	logsStabilityLevel component.StabilityLevel
+}
+
+func (f *factory) Type() component.Type {
+	return f.cfgType
+}
+
+// CreateDefaultConfig creates the default configuration for the Component.
+//
+// TODO: Remove this when we remove the private func from component.Factory and add it to every specialized Factory.
+func (f *factory) CreateDefaultConfig() component.Config {
+	return f.CreateDefaultConfigFunc()
+}
+
+func (f factory) TracesProcessorStability() component.StabilityLevel {
+	return f.tracesStabilityLevel
+}
+
+func (f factory) MetricsProcessorStability() component.StabilityLevel {
+	return f.metricsStabilityLevel
+}
+
+func (f factory) LogsProcessorStability() component.StabilityLevel {
+	return f.logsStabilityLevel
+}
 
 // WithTraces overrides the default "error not supported" implementation for CreateTraces and the default "undefined" stability level.
-var WithTraces = component.WithTracesProcessor //nolint:staticcheck
+func WithTraces(createTraces CreateTracesFunc, sl component.StabilityLevel) FactoryOption {
+	return factoryOptionFunc(func(o *factory) {
+		o.tracesStabilityLevel = sl
+		o.CreateTracesFunc = createTraces
+	})
+}
 
 // WithMetrics overrides the default "error not supported" implementation for CreateMetrics and the default "undefined" stability level.
-var WithMetrics = component.WithMetricsProcessor //nolint:staticcheck
+func WithMetrics(createMetrics CreateMetricsFunc, sl component.StabilityLevel) FactoryOption {
+	return factoryOptionFunc(func(o *factory) {
+		o.metricsStabilityLevel = sl
+		o.CreateMetricsFunc = createMetrics
+	})
+}
 
 // WithLogs overrides the default "error not supported" implementation for CreateLogs and the default "undefined" stability level.
-var WithLogs = component.WithLogsProcessor //nolint:staticcheck
+func WithLogs(createLogs CreateLogsFunc, sl component.StabilityLevel) FactoryOption {
+	return factoryOptionFunc(func(o *factory) {
+		o.logsStabilityLevel = sl
+		o.CreateLogsFunc = createLogs
+	})
+}
 
 // NewFactory returns a Factory.
-var NewFactory = component.NewProcessorFactory //nolint:staticcheck
+func NewFactory(cfgType component.Type, createDefaultConfig component.CreateDefaultConfigFunc, options ...FactoryOption) Factory {
+	f := &factory{
+		cfgType:                 cfgType,
+		CreateDefaultConfigFunc: createDefaultConfig,
+	}
+	for _, opt := range options {
+		opt.applyProcessorFactoryOption(f)
+	}
+	return f
+}
 
 // MakeFactoryMap takes a list of factories and returns a map with Factory type as keys.
 // It returns a non-nil error when there are factories with duplicate type.
