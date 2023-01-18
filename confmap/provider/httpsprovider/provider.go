8b7be0ece11486906afab81f377b82413ed5a41e
Raphael Philipe Mendes da Silva <rapphil@gmail.com>
2023-01-18 12:27:12 -0800

    Fix comments
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

--- confmap/provider/httpsprovider/provider.go
+++ confmap/provider/httpsprovider/provider.go
@@ -21,7 +21,7 @@ import (
 
 // New returns a new confmap.Provider that reads the configuration from a https server.
 //
-// This Provider supports "https" scheme. One example for https-uri is: https://localhost:3333/getConfig
+// This Provider supports "https" scheme. One example of an HTTPS URI is: https://localhost:3333/getConfig
 //
 // To add extra CA certificates you need to install certificates in the system pool. This procedure is operating system
 // dependent. E.g.: on Linux please refer to the `update-ca-trust` command.
