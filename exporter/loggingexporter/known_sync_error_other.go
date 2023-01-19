fb50717caa5d6ffdeac488a5e2cbf5a52d14ac8e
Bogdan Drutu <bogdandrutu@gmail.com>
2023-01-19 10:05:32 -0800

    Generalize the plan9 build restriction to any other platform (#6968)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/loggingexporter/known_sync_error_other.go
+++ exporter/loggingexporter/known_sync_error_other.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build plan9
-// +build plan9
+//go:build !linux && !darwin && !windows
+// +build !linux,!darwin,!windows
 
 package loggingexporter // import "go.opentelemetry.io/collector/exporter/loggingexporter"
 
