52d7a05d72935bdd8f4f444e71434c99279de7d2
Alex Boten <aboten@lightstep.com>
2023-01-24 13:27:44 -0800

    [chore] use opentelemetrybot
    This works around the limitation that a PR created by an action doesn't trigger workflows automatically by using the opentelemetrybot's token. See https://github.com/open-telemetry/community/blob/main/assets.md#opentelemetry-bot
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- .github/workflows/scripts/release-prepare-release.sh
+++ .github/workflows/scripts/release-prepare-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 make chlog-update VERSION="${CANDIDATE_STABLE}/${CANDIDATE_BETA}"
-git config user.name "$GITHUB_ACTOR"
-git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config user.name opentelemetrybot
+git config user.email 107717825+opentelemetrybot@users.noreply.github.com
 BRANCH="prepare-release-prs/${CANDIDATE_BETA}-${CANDIDATE_STABLE}"
 git checkout -b "${BRANCH}"
 git add --all
