893da3bf642050201d4b59fe884d74e56ed790f9
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2023-02-08 00:01:49 +0000

    prepare release 0.71.0

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -26,7 +26,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const defaultOtelColVersion = "0.70.0"
+const defaultOtelColVersion = "0.71.0"
 
 // ErrInvalidGoMod indicates an invalid gomod
 var ErrInvalidGoMod = errors.New("invalid gomod specification for module")
