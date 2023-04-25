616a69c5db9c3f68ffaa4cc795305953a28d3ae7
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2023-04-25 16:02:10 +0000

    add multimod changes 0.76.0

--- cmd/otelcorecol/main.go
+++ cmd/otelcorecol/main.go
@@ -19,7 +19,7 @@ func main() {
 	info := component.BuildInfo{
 		Command:     "otelcorecol",
 		Description: "Local OpenTelemetry Collector binary, testing only.",
-		Version:     "0.75.0-dev",
+		Version:     "0.76.0-dev",
 	}
 
 	if err := run(otelcol.CollectorSettings{BuildInfo: info, Factories: factories}); err != nil {
