1ae03e65bd31dc47e34503510225a27d1379d569
Alex Boten <aboten@lightstep.com>
2023-05-02 15:52:24 -0700

    add line break
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- .github/workflows/scripts/release-prepare-release.sh
+++ .github/workflows/scripts/release-prepare-release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+#
 # Copyright The OpenTelemetry Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- internal/buildscripts/compare-apidiff.sh
+++ internal/buildscripts/compare-apidiff.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#
 # Copyright The OpenTelemetry Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
