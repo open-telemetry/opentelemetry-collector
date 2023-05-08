4230ffbf987fecb834c245e856a94d11cdbb4727
Dmitry Anoshin <anoshindx@gmail.com>
2023-05-08 11:02:53 -0700

    [chore] Do not include unreleased versions in the release titles
    Update release scripts to not include unreleased version in the release titles

--- .github/workflows/scripts/release-prepare-release.sh
+++ .github/workflows/scripts/release-prepare-release.sh
@@ -19,17 +19,16 @@ if [ "${CANDIDATE_STABLE}" == "" ] && [ "${CANDIDATE_BETA}" == "" ]; then
     exit 1
 fi
 
-STABLE="${CANDIDATE_STABLE}"
-if [ "${STABLE}" == "" ]; then
-    STABLE="${CURRENT_STABLE}"
+RELEASE_VERSION=v${CANDIDATE_STABLE}/v${CANDIDATE_BETA}
+if [ "${CANDIDATE_STABLE}" == "" ]; then
+    RELEASE_VERSION="v${CANDIDATE_BETA}"
 fi
-BETA="${CANDIDATE_BETA}"
-if [ "${BETA}" == "" ]; then
-    BETA="${CURRENT_BETA}"
+if [ "${CANDIDATE_BETA}" == "" ]; then
+    RELEASE_VERSION="v${CANDIDATE_STABLE}"
 fi
-RELEASE_VERSION=v${STABLE}/v${BETA}
 
 make chlog-update VERSION="${RELEASE_VERSION}"
+COMMANDS="- make chlog-update VERSION=${RELEASE_VERSION}"
 git config user.name opentelemetrybot
 git config user.email 107717825+opentelemetrybot@users.noreply.github.com
 BRANCH="prepare-release-prs/${CANDIDATE_BETA}"
@@ -39,15 +38,17 @@ git commit -m "Changelog update ${RELEASE_VERSION}"
 
 if [ "${CANDIDATE_STABLE}" != "" ]; then
     make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_STABLE}" RELEASE_CANDIDATE="${CANDIDATE_STABLE}" MODSET=stable
+    COMMANDS+="
+- make prepare-release GH=none PREVIOUS_VERSION=${CURRENT_STABLE} RELEASE_CANDIDATE=${CANDIDATE_STABLE} MODSET=stable"
 fi
 if [ "${CANDIDATE_BETA}" != "" ]; then
     make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_BETA}" RELEASE_CANDIDATE="${CANDIDATE_BETA}" MODSET=beta
+    COMMANDS+="
+- make prepare-release GH=none PREVIOUS_VERSION=${CURRENT_BETA} RELEASE_CANDIDATE=${CANDIDATE_BETA} MODSET=beta"
 fi
 git push origin "${BRANCH}"
 
 gh pr create --title "[chore] Prepare release ${RELEASE_VERSION}" --body "
 The following commands were run to prepare this release:
-- make chlog-update VERSION=${RELEASE_VERSION}
-- make prepare-release GH=none PREVIOUS_VERSION=${CURRENT_STABLE} RELEASE_CANDIDATE=${CANDIDATE_STABLE} MODSET=stable
-- make prepare-release GH=none PREVIOUS_VERSION=${CURRENT_BETA} RELEASE_CANDIDATE=${CANDIDATE_BETA} MODSET=beta
+${COMMANDS}
 "
