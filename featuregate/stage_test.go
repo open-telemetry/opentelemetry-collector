9b978f74f37af17e18896b297dd7d929b362674b
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-05-09 18:06:42 -0600

    [featuregate] Fix issue where StageDeprecated cause an error on register (#7586)
    * Update Deprecated to work like Stable but false
    
    * Add changelog
    
    * Fix lint
    
    * Update README

--- featuregate/stage_test.go
+++ featuregate/stage_test.go
@@ -24,5 +24,6 @@ func TestStageString(t *testing.T) {
 	assert.Equal(t, "Alpha", StageAlpha.String())
 	assert.Equal(t, "Beta", StageBeta.String())
 	assert.Equal(t, "Stable", StageStable.String())
+	assert.Equal(t, "Deprecated", StageDeprecated.String())
 	assert.Equal(t, "Unknown", Stage(-1).String())
 }
