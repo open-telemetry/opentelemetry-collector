30e60619dc09f08964f387cd8967de008ff8d806
Alex Boten <aboten@lightstep.com>
2023-09-20 15:56:53 -0700

    rename exporterhelper obsreport method
    This aligns better with the other new methods in the various helper modules.
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- obsreport/obsreport_exporter.go
+++ obsreport/obsreport_exporter.go
@@ -7,20 +7,17 @@ import "go.opentelemetry.io/collector/exporter/exporterhelper"
 
 // Exporter is a helper to add observability to an exporter.
 //
-// Deprecated: [0.85.0] Use exporterhelper.Exporter instead.
-type Exporter = exporterhelper.Exporter
+// Deprecated: [0.85.0] Use exporterhelper.ObsReport instead.
+type Exporter = exporterhelper.ObsReport
 
 // ExporterSettings are settings for creating an Exporter.
 //
-// Deprecated: [0.85.0] Use exporterhelper.Settings instead.
-type ExporterSettings = exporterhelper.Settings
+// Deprecated: [0.85.0] Use exporterhelper.ObsReportSettings instead.
+type ExporterSettings = exporterhelper.ObsReportSettings
 
 // NewExporter creates a new Exporter.
 //
 // Deprecated: [0.85.0] Use exporterhelper.New instead.
-func NewExporter(cfg ExporterSettings) (*exporterhelper.Exporter, error) {
-	return exporterhelper.New(exporterhelper.Settings{
-		ExporterID:             cfg.ExporterID,
-		ExporterCreateSettings: cfg.ExporterCreateSettings,
-	})
+func NewExporter(cfg ExporterSettings) (*exporterhelper.ObsReport, error) {
+	return exporterhelper.NewObsReport(cfg)
 }
