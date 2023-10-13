07afa2a318d4015a5a01606b12223f191d7156d8
Antoine Toulme <antoine@lunar-ocean.com>
2023-10-13 15:30:25 -0700

    goimports

--- exporter/exporterhelper/queue_sender_test.go
+++ exporter/exporterhelper/queue_sender_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumererror"
