aec4873a7fd3e80b221fa56ca36fcc9c624ec5bd
Bogdan Drutu <bogdandrutu@gmail.com>
2023-11-13 19:20:36 -0800

    Fix connector logger zap kind key
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- component/component.go
+++ component/component.go
@@ -96,6 +96,22 @@ const (
 	KindConnector
 )
 
+func (k Kind) String() string {
+	switch k {
+	case KindReceiver:
+		return "Receiver"
+	case KindProcessor:
+		return "Processor"
+	case KindExporter:
+		return "Exporter"
+	case KindExtension:
+		return "Extension"
+	case KindConnector:
+		return "Connector"
+	}
+	return ""
+}
+
 // StabilityLevel represents the stability level of the component created by the factory.
 // The stability level is used to determine if the component should be used in production
 // or not. For more details see:
