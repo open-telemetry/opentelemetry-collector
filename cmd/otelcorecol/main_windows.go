d77a32989a5a5d26e673b66d845cc1ef2e200b84
Paulo Janotti <pjanotti@splunk.com>
2023-12-05 22:31:26 -0800

    Use errors.Is instead of casting

--- cmd/otelcorecol/main_windows.go
+++ cmd/otelcorecol/main_windows.go
@@ -6,8 +6,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"syscall"
 
 	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/svc"
@@ -19,8 +19,7 @@ func run(params otelcol.CollectorSettings) error {
 	// No need to supply service name when startup is invoked through
 	// the Service Control Manager directly.
 	if err := svc.Run("", otelcol.NewSvcHandler(params)); err != nil {
-		errno, ok := err.(syscall.Errno)
-		if ok && errno == windows.ERROR_FAILED_SERVICE_CONTROLLER_CONNECT {
+		if errors.Is(err, windows.ERROR_FAILED_SERVICE_CONTROLLER_CONNECT) {
 			// Per https://learn.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-startservicectrldispatchera#return-value
 			// this means that the process is not running as a service, so run interactively.
 			return runInteractive(params)
