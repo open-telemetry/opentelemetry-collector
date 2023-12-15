c9141653b660334d5b50683f594a9ec36aa12153
Berkeli Halmyradov <berkelih@gmail.com>
2023-12-15 18:16:28 +0000

    [chore] [pdata] add comment as suggestion

--- pdata/pcommon/map.go
+++ pdata/pcommon/map.go
@@ -56,6 +56,7 @@ func (m Map) EnsureCapacity(capacity int) {
 // Such modification will be applied to the value stored in this map.
 //
 // If the key does not exist returns a zero-initialized KeyValue and false.
+// Calling any functions on the returned invalid instance may cause a panic.
 func (m Map) Get(key string) (Value, bool) {
 	for i := range *m.getOrig() {
 		akv := &(*m.getOrig())[i]
