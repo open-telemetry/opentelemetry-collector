db08c45c44932d3c03dc72fe0ce8459bceca7a8f
Antoine Toulme <antoine@lunar-ocean.com>
2023-12-19 14:32:45 -0800

    [chore] some doc changes to consumer package

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -10,7 +10,7 @@ import (
 // Capabilities describes the capabilities of a Processor.
 type Capabilities struct {
 	// MutatesData is set to true if Consume* function of the
-	// processor modifies the input TraceData or MetricsData argument.
+	// processor modifies the input Traces, Logs or Metrics argument.
 	// Processors which modify the input data MUST set this flag to true. If the processor
 	// does not modify the data it MUST set this flag to false. If the processor creates
 	// a copy of the data before modifying then this flag can be safely set to false.
