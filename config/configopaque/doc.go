9027a8d9cc3fd006b3f9b8bc531cd6996d946adc
Antoine Toulme <antoine@lunar-ocean.com>
2023-12-21 00:54:27 -0800

    [chore] typo on configopaque doc (#9166)

--- config/configopaque/doc.go
+++ config/configopaque/doc.go
@@ -5,6 +5,6 @@
 // Use configopaque.String on the type of sensitive fields, to mask the
 // opaque string as `[REDACTED]`.
 //
-// This ensure that no sensitive information is leaked when printing the
+// This ensures that no sensitive information is leaked when printing the
 // full Collector configurations.
 package configopaque // import "go.opentelemetry.io/collector/config/configopaque"
