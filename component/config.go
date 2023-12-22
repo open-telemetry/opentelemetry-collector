6155cc25ddaad253abcced4fcd8108d1be98e9c1
Antoine Toulme <antoine@lunar-ocean.com>
2023-12-22 11:11:39 -0800

    Make the option `WithErrorUnused` enabled by default when unmarshaling configuration (#9154)
    **Description:**
    Make the option `WithErrorUnused` enabled by default when unmarshaling
    configuration
    The option `WithErrorUnused` is now enabled by default, and a new option
    `WithIgnoreUnused` is introduced to ignore
    errors about unused fields.
    
    **Link to tracking Issue:**
    This relates to #7102 to some extent.
    
    **Testing:**
    N/A
    
    **Documentation:**
    N/A

--- component/config.go
+++ component/config.go
@@ -32,7 +32,7 @@ func UnmarshalConfig(conf *confmap.Conf, intoCfg Config) error {
 		return cu.Unmarshal(conf)
 	}
 
-	return conf.Unmarshal(intoCfg, confmap.WithErrorUnused())
+	return conf.Unmarshal(intoCfg)
 }
 
 // ConfigValidator defines an optional interface for configurations to implement to do validation.
