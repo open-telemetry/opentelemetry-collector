e29f9b7a08a9f2c0eec1ae378d42cb3f49f4b6bb
Curtis Robert <crobert@splunk.com>
2023-12-21 16:07:09 -0800

    Changes requested in PR
    - Add test case for uint64(-1000) conversion
    - Clean existing test case

--- confmap/confmap_test.go
+++ confmap/confmap_test.go
@@ -219,11 +219,6 @@ type UintConfig struct {
 }
 
 func TestUintUnmarshalerSuccess(t *testing.T) {
-	testValue := 1000
-	stringMap := map[string]any{
-		"uint_test": testValue,
-	}
-
 	tests := []struct {
 		name      string
 		testValue int
@@ -242,15 +237,36 @@ func TestUintUnmarshalerSuccess(t *testing.T) {
 		tt := tt
 
 		t.Run(tt.name, func(t *testing.T) {
+			stringMap := map[string]any{
+				"uint_test": tt.testValue,
+			}
 			conf := NewFromStringMap(stringMap)
 			cfg := &UintConfig{}
 			err := conf.Unmarshal(cfg)
 
 			assert.NoError(t, err)
-			assert.Equal(t, cfg.UintTest, uint32(testValue))
+			assert.Equal(t, cfg.UintTest, uint32(tt.testValue))
 		})
 	}
+}
 
+func TestUint64Unmarshaler(t *testing.T) {
+	negativeInt := -1000
+	testValue := uint64(negativeInt)
+
+	type Uint64Config struct {
+		UintTest uint64 `mapstructure:"uint_test"`
+	}
+	stringMap := map[string]any{
+		"uint_test": testValue,
+	}
+
+	conf := NewFromStringMap(stringMap)
+	cfg := &Uint64Config{}
+	err := conf.Unmarshal(cfg)
+
+	assert.NoError(t, err)
+	assert.Equal(t, cfg.UintTest, testValue)
 }
 
 func TestUintUnmarshalerFailure(t *testing.T) {
