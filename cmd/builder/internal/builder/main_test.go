031c48a7ad6dd4b2db236e260d875608ab3297c4
Curtis Robert <crobert@splunk.com>
2023-12-21 16:24:05 -0800

    Fix import order

--- cmd/builder/internal/builder/main_test.go
+++ cmd/builder/internal/builder/main_test.go
@@ -12,10 +12,9 @@ import (
 	"testing"
 	"time"
 
-	"go.uber.org/goleak"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 )
 
 func TestGenerateDefault(t *testing.T) {
