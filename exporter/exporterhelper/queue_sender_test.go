aafc28a7a9769c55046a2d53a79efd78f22be66e
Curtis Robert <crobert@splunk.com>
2024-01-04 08:48:40 -0800

    Use t.Cleanup to make sure shutdown is called.

--- exporter/exporterhelper/queue_sender_test.go
+++ exporter/exporterhelper/queue_sender_test.go
@@ -325,10 +325,10 @@ func TestQueuedRetryPersistentEnabled_NoDataLossOnShutdown(t *testing.T) {
 		newNoopObsrepSender, WithRetry(rCfg), WithQueue(qCfg))
 	require.NoError(t, err)
 	require.NoError(t, be.Start(context.Background(), host))
+	t.Cleanup(func() { require.NoError(t, be.Shutdown(context.Background())) })
 
 	// wait for the item to be consumed from the queue
 	replacedReq.checkNumRequests(t, 1)
-	require.NoError(t, be.Shutdown(context.Background()))
 }
 
 func TestQueueSenderNoStartShutdown(t *testing.T) {
