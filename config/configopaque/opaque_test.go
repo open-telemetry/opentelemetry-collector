1cea6363f60421d4faed7469ccdcae25f82231b2
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2024-01-15 19:36:57 +0100

    [configopaque] Implement fmt.GoStringer interface for String (#9271)
    **Description:** Follow up to #9214.
    
    To make sure the output when using `%#v` is redacted, we must also
    implement the `fmt.GoStringer` interface.
    
    **Link to tracking Issue:** Relates to #9213
    
    **Testing:** Adds some tests with YAML and JSON as well as more thorough
    testing of all `fmt` verbs
    
    **Documentation:** Clarify that printing this is also redacted.

--- config/configopaque/opaque_test.go
+++ config/configopaque/opaque_test.go
@@ -4,11 +4,13 @@
 package configopaque // import "go.opentelemetry.io/collector/config/configopaque"
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestStringMarshalText(t *testing.T) {
@@ -16,17 +18,65 @@ func TestStringMarshalText(t *testing.T) {
 	for _, example := range examples {
 		opaque, err := example.MarshalText()
 		require.NoError(t, err)
-		assert.Equal(t, "[REDACTED]", string(opaque))
+		assert.Equal(t, maskedString, string(opaque))
 	}
+}
+
+type TestStruct struct {
+	Opaque String `json:"opaque" yaml:"opaque"`
+	Plain  string `json:"plain" yaml:"plain"`
+}
+
+var example = TestStruct{
+	Opaque: "opaque",
+	Plain:  "plain",
+}
+
+func TestStringJSON(t *testing.T) {
+	bytes, err := json.Marshal(example)
+	require.NoError(t, err)
+	assert.Equal(t, `{"opaque":"[REDACTED]","plain":"plain"}`, string(bytes))
+}
+
+func TestStringYAML(t *testing.T) {
+	bytes, err := yaml.Marshal(example)
+	require.NoError(t, err)
+	assert.Equal(t, "opaque: '[REDACTED]'\nplain: plain\n", string(bytes))
 }
 
 func TestStringFmt(t *testing.T) {
 	examples := []String{"opaque", "s", "veryveryveryveryveryveryveryveryveryverylong"}
+	verbs := []string{"%s", "%q", "%v", "%#v", "%+v", "%x"}
 	for _, example := range examples {
-		// nolint S1025
-		assert.Equal(t, "[REDACTED]", fmt.Sprintf("%s", example))
-		// converting to a string allows to output as an unredacted string still:
-		// nolint S1025
-		assert.Equal(t, string(example), fmt.Sprintf("%s", string(example)))
+		for _, verb := range verbs {
+			t.Run(fmt.Sprintf("%s/%s", string(example), verb), func(t *testing.T) {
+				assert.Equal(t,
+					fmt.Sprintf(verb, maskedString),
+					fmt.Sprintf(verb, example),
+				)
+			})
+		}
+
+		for _, verb := range verbs {
+			t.Run(fmt.Sprintf("string(%s)/%s", string(example), verb), func(t *testing.T) {
+				// converting to a string allows to output as an unredacted string still:
+				var expected string
+				switch verb {
+				case "%s", "%v", "%+v":
+					expected = string(example)
+				case "%q", "%#v":
+					expected = "\"" + string(example) + "\""
+				case "%x":
+					expected = fmt.Sprintf("%x", []byte(example))
+				default:
+					t.Errorf("unexpected verb %q", verb)
+				}
+
+				assert.Equal(t,
+					expected,
+					fmt.Sprintf(verb, string(example)),
+				)
+			})
+		}
 	}
 }
