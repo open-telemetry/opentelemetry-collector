9047c0e373f97f46037256908ee86e21a25c7241
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2024-01-24 13:33:50 +0100

    [receiver/otlp, internal, featuregate] Add featuregate to switch to localhost defaults for server-like components (#8622)
    **Description:** 
    
    - Define `component.UseLocalHostAsDefaultHost` in the
    `internal/localhostgate` package.
    - Define `featuregate.ErrIsAlreadyRegistered` error, returned by
    `Register` when a gate is already registered.
    - Adds support for the localhost gate on the OTLP receiver.
    
    This PR does not remove the current warning in any way, we can remove
    this separately.
    
    **Link to tracking Issue:** Updates #8510
    
    **Testing:** Adds unit tests
    
    **Documentation:** Document on OTLP receiver template and add related
    logging.

--- service/service.go
+++ service/service.go
@@ -19,6 +19,7 @@ import (
 	"go.opentelemetry.io/collector/connector"
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/internal/localhostgate"
 	"go.opentelemetry.io/collector/internal/obsreportconfig"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/processor"
@@ -163,6 +164,7 @@ func (srv *Service) Start(ctx context.Context) error {
 	}
 
 	srv.telemetrySettings.Logger.Info("Everything is ready. Begin running and processing data.")
+	localhostgate.LogAboutUseLocalHostAsDefault(srv.telemetrySettings.Logger)
 	return nil
 }
 
