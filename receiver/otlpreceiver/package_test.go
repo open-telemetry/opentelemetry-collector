34ee08798e3dd82680cdb39fee3e7d7a68a49589
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-04-12 11:08:45 -0700

    [chore] these top level ignores are no longer needed
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/otlpreceiver/package_test.go
+++ receiver/otlpreceiver/package_test.go
@@ -13,5 +13,5 @@ import (
 // defaultWorker.Start which at this time is part of the package's init call.
 // See https://github.com/open-telemetry/opentelemetry-collector/issues/9165#issuecomment-1874836336 for more context.
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m, goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"))
+	goleak.VerifyTestMain(m)
 }
