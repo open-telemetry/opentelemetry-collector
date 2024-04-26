13b3ff954d61eab82f5efb4a3bd35b50cbf314c0
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-04-26 14:37:48 -0700

    gogenerate
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- cmd/mdatagen/main.go
+++ cmd/mdatagen/main.go
@@ -62,6 +62,10 @@ func run(ymlPath string) error {
 				filepath.Join(codeDir, "generated_status.go"), md, "metadata"); err != nil {
 				return err
 			}
+			if err = generateFile(filepath.Join(tmplDir, "telemetry.go.tmpl"),
+				filepath.Join(codeDir, "generated_telemetry.go"), md, "metadata"); err != nil {
+				return err
+			}
 			if err = generateFile(filepath.Join(tmplDir, "component_test.go.tmpl"),
 				filepath.Join(ymlDir, "generated_component_test.go"), md, packageName); err != nil {
 				return err
