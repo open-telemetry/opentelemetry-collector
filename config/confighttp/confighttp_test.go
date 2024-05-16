56943e7db5dbbbaf0770f419f8dda84beb52eec9
Vibhu Pandey <vibhupandey28@gmail.com>
2024-05-16 19:01:12 +0000

    test(confighttp): add sample test demoing different beahviour in otlp/http

--- config/confighttp/confighttp_test.go
+++ config/confighttp/confighttp_test.go
@@ -1207,6 +1207,83 @@ func TestServerAuth(t *testing.T) {
 	assert.True(t, authCalled)
 }
 
+func TestClientMetadataWithAuthInterceptorsAndIncludeMetadata(t *testing.T) {
+	testCases := []struct {
+		desc            string
+		includeMetadata bool
+	}{
+		{
+			desc:            "metadata:true",
+			includeMetadata: true,
+		},
+		{
+			desc:            "metadata:false",
+			includeMetadata: false,
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			host := &mockHost{
+				ext: map[component.ID]component.Component{
+					mockID: auth.NewServer(auth.WithServerAuthenticate(func(ctx context.Context, headers map[string][]string) (context.Context, error) {
+						return client.NewContext(ctx, client.Info{
+							Metadata: client.NewMetadata(metadata.Pairs("some-key-set-in-auth", "some-value-set-in-auth")),
+						}), nil
+					})),
+				},
+			}
+
+			hss := ServerConfig{
+				Endpoint: "localhost:0",
+				Auth: &configauth.Authentication{
+					AuthenticatorID: mockID,
+				},
+				IncludeMetadata: tC.includeMetadata,
+			}
+
+			httpMux := http.NewServeMux()
+			httpMux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+				cl := client.FromContext(req.Context())
+				// We will always find this regardless of whether include_metadata is true or false
+				assert.Equal(t, cl.Metadata.Get("some-key-set-in-auth")[0], "some-value-set-in-auth")
+				// We will never find the below as we are overriding metadata in auth
+				assert.Nil(t, cl.Metadata.Get("header1"))
+
+				resp.WriteHeader(http.StatusOK)
+			})
+			srv, err := hss.ToServer(context.Background(), host, componenttest.NewNopTelemetrySettings(), httpMux)
+			require.NoError(t, err)
+
+			ln, err := hss.ToListener(context.Background())
+			require.NoError(t, err)
+
+			go func() {
+				_ = srv.Serve(ln)
+			}()
+			defer func() {
+				_ = srv.Close()
+			}()
+
+			hcs := &ClientConfig{
+				Endpoint: "http://" + ln.Addr().String(),
+				Headers: map[string]configopaque.String{
+					"header1": "value1",
+				},
+			}
+			c, err := hcs.ToClient(context.Background(), componenttest.NewNopHost(), component.TelemetrySettings{})
+			require.NoError(t, err)
+
+			req, err := http.NewRequest("GET", hcs.Endpoint, nil)
+			require.NoError(t, err)
+
+			_, err = c.Do(req)
+			require.NoError(t, err)
+		})
+	}
+}
+
+
 func TestInvalidServerAuth(t *testing.T) {
 	hss := ServerConfig{
 		Auth: &configauth.Authentication{
