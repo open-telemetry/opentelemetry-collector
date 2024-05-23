63f3d442940c3276dc5edecfdbbc4d54e13404d0
Ankit Patel <ankit.patel@datadoghq.com>
2024-05-22 19:17:00 -0700

    porto

--- confmap/internal/envvar/pattern.go
+++ confmap/internal/envvar/pattern.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package envvar
+package envvar // import "go.opentelemetry.io/collector/confmap/internal/envvar"
 
 import "regexp"
 
