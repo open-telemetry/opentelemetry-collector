c7201c51afc36d7e6dbd8a5a192eb479cde05fcb
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-05-31 11:28:43 -0600

    Removed deprecated field

--- otelcol/command_components_test.go
+++ otelcol/command_components_test.go
@@ -17,13 +17,10 @@ import (
 )
 
 func TestNewBuildSubCommand(t *testing.T) {
-	cfgProvider, err := NewConfigProvider(newDefaultConfigProviderSettings([]string{filepath.Join("testdata", "otelcol-nop.yaml")}))
-	require.NoError(t, err)
-
 	set := CollectorSettings{
-		BuildInfo:      component.NewDefaultBuildInfo(),
-		Factories:      nopFactories,
-		ConfigProvider: cfgProvider,
+		BuildInfo:              component.NewDefaultBuildInfo(),
+		Factories:              nopFactories,
+		ConfigProviderSettings: newDefaultConfigProviderSettings([]string{filepath.Join("testdata", "otelcol-nop.yaml")}),
 	}
 	cmd := NewCommand(set)
 	cmd.SetArgs([]string{"components"})
