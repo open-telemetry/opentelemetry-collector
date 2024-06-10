aed15f82882f1f2324dceb048f97e7d45e680b88
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-06-10 09:52:41 -0600

    Fix test

--- otelcol/command_test.go
+++ otelcol/command_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -25,6 +26,30 @@ func TestNewCommandVersion(t *testing.T) {
 func TestNewCommandNoConfigURI(t *testing.T) {
 	cmd := NewCommandMustProviderSettings(CollectorSettings{Factories: nopFactories})
 	require.Error(t, cmd.Execute())
+}
+
+// This test emulates usage of Collector in Jaeger all-in-one, which
+// allows running the binary with no explicit configuration.
+func TestNewCommandProgrammaticallyPassedConfig(t *testing.T) {
+	cmd := NewCommandMustProviderSettings(CollectorSettings{Factories: nopFactories, ConfigProviderSettings: ConfigProviderSettings{
+		ResolverSettings: confmap.ResolverSettings{
+			ProviderFactories: []confmap.ProviderFactory{confmap.NewProviderFactory(newFailureProvider)},
+		},
+	}})
+	otelRunE := cmd.RunE
+	cmd.RunE = func(c *cobra.Command, args []string) error {
+		configFlag := c.Flag("config")
+		cfg := `
+service:
+  extensions: [invalid_component_name]
+receivers:
+  invalid_component_name:
+`
+		require.NoError(t, configFlag.Value.Set("yaml:"+cfg))
+		return otelRunE(cmd, args)
+	}
+	// verify that cmd.Execute was run with the implicitly provided config.
+	require.ErrorContains(t, cmd.Execute(), "invalid_component_name")
 }
 
 func TestAddFlagToSettings(t *testing.T) {
