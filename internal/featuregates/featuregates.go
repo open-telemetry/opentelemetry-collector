f2299382d7456194b5ca974c2e2873bdd537b953
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-06-13 13:49:21 -0600

    make goporto

--- internal/featuregates/featuregates.go
+++ internal/featuregates/featuregates.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package featuregates
+package featuregates // import "go.opentelemetry.io/collector/internal/featuregates"
 
 import "go.opentelemetry.io/collector/featuregate"
 
