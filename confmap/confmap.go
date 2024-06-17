7a3c35cb77f5eb2d1d3fa3478ce8d94a56a6434a
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2024-06-17 11:29:35 +0000

    [confmap] Add strict type validation under a feature gate (#10400)
    #### Description
    
    <!-- Issue number if applicable -->
    
    - Add `confmap.strictlyTypedInput` feature gate that introduces stricter
    type checks when resolving configuration
    - Make `confmap.NewRetrievedFromYAML` function public so that external
    providers have consistent behavior when resolving YAML
    - Adds `confmap.Retrieved.AsString` method to retrieve string
    representation for retrieved values
    
    #### Link to tracking issue
    
    Relates to #9854, updates #8565, #9532

--- confmap/confmap.go
+++ confmap/confmap.go
@@ -16,6 +16,7 @@ import (
 	"github.com/knadh/koanf/providers/confmap"
 	"github.com/knadh/koanf/v2"
 
+	"go.opentelemetry.io/collector/confmap/internal"
 	encoder "go.opentelemetry.io/collector/confmap/internal/mapstructure"
 )
 
@@ -156,7 +157,7 @@ func decodeConfig(m *Conf, result any, errorUnused bool, skipTopLevelUnmarshaler
 		ErrorUnused:      errorUnused,
 		Result:           result,
 		TagName:          "mapstructure",
-		WeaklyTypedInput: true,
+		WeaklyTypedInput: !internal.StrictlyTypedInputGate.IsEnabled(),
 		MatchName:        caseSensitiveMatchName,
 		DecodeHook: mapstructure.ComposeDecodeHookFunc(
 			expandNilStructPointersHookFunc(),
