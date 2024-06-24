018174f9a55881acebc85c31d815f7a47eba787d
Andrzej Stencel <andrzej.stencel@elastic.co>
2024-06-24 09:23:15 +0200

    make goporto

--- exporter/debugexporter/internal/normal/common.go
+++ exporter/debugexporter/internal/normal/common.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package normal
+package normal // import "go.opentelemetry.io/collector/exporter/debugexporter/internal/normal"
 
 import (
 	"fmt"
