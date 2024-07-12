3087c5386a092c0d874ec0ee25a9ee2c2974b097
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2024-07-12 08:21:03 -0700

    [otelcol] Obtain the Collector's effective configuration from `otelcol.Config` (#10139)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    The `otelcol.ConfmapProvider` interface accurately reports the config
    provided to the Collector by the user, but fails to effectively report
    the Collector's effective configuration. In particular, it misses:
    * Default values for fields in Config structs.
    * Transformations done to Config structs by their `Unmarshal` or
    `Validate` methods.
    * Custom marshaling of types after we know the type of the config. This
    is most obvious with `configopaque.String`, where we want these values
    to always be redacted when sent out of the Collector.
    
    I think we should attempt to get the Collector's effective configuration
    from `otelcol.Config` instead of using the map compiled by the
    confmap.Resolver. This also allows us to get rid of
    `otelcol.ConfmapProvider`.
    
    I have manually tested this using an OpAMP Supervisor setup, but wasn't
    able to add unit tests due to the way `otelcol.Collector` is written.
    
    ---------
    
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- otelcol/configprovider.go
+++ otelcol/configprovider.go
@@ -58,6 +58,9 @@ type ConfigProvider interface {
 //
 // The purpose of this interface is that otelcol.ConfigProvider structs do not
 // necessarily need to use confmap.Conf as their underlying config structure.
+//
+// Deprecated: [v0.105.0] This interface is deprecated. otelcol.Collector will now obtain
+// a confmap.Conf object from the unmarshaled config itself.
 type ConfmapProvider interface {
 	// GetConfmap resolves the Collector's configuration and provides it as a confmap.Conf object.
 	//
@@ -70,7 +73,6 @@ type configProvider struct {
 }
 
 var _ ConfigProvider = (*configProvider)(nil)
-var _ ConfmapProvider = (*configProvider)(nil)
 
 // ConfigProviderSettings are the settings to configure the behavior of the ConfigProvider.
 type ConfigProviderSettings struct {
@@ -143,8 +145,11 @@ func (cm *configProvider) Shutdown(ctx context.Context) error {
 	return cm.mapResolver.Shutdown(ctx)
 }
 
+// Deprecated: [v0.105.0] Call `(*confmap.Conf).Marshal(*otelcol.Config)` to get
+// the Collector's configuration instead.
 func (cm *configProvider) GetConfmap(ctx context.Context) (*confmap.Conf, error) {
 	conf, err := cm.mapResolver.Resolve(ctx)
+
 	if err != nil {
 		return nil, fmt.Errorf("cannot resolve the configuration: %w", err)
 	}
