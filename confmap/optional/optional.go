8c255ab7f88acb25c4aede0130fe34cb381bb722
Yuri Shkuro <github@ysh.us>
2024-07-14 01:24:54 -0400

    Add comments
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- confmap/optional/optional.go
+++ confmap/optional/optional.go
@@ -27,8 +27,13 @@ func None[T any]() Optional[T] {
 
 // WithDefault creates an Optional which has no value
 // unless user config provides some, in which case
-// the defaultValue is used as a starting point,
-// which may be overridden by the user provided values.
+// the defaultFn is used to create the initial value,
+// which may be overridden by the user provided config.
+//
+// The reason we pass a function instead of T directly
+// is to allow this to work with T being a pointer,
+// because we wouldn't want to copy the value of a pointer
+// since it might reuse (and override) some shared state.
 func WithDefault[T any](defaultFn DefaultFunc[T]) Optional[T] {
 	return Optional[T]{defaultFn: defaultFn}
 }
