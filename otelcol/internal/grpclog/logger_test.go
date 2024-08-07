f91842c437d7b59aa64c3b83d9bdee876d8b0a22
vvs-personalstash <viralkverma0987@gmail.com>
2024-08-08 02:36:38 +0530

    Add Test for Correct Caller Location by Implementing a Dummy channelz logger as in grpc

--- otelcol/internal/grpclog/logger_test.go
+++ otelcol/internal/grpclog/logger_test.go
@@ -4,11 +4,13 @@
 package grpclog
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc/grpclog"
 )
 
 func TestGRPCLogger(t *testing.T) {
@@ -27,7 +29,7 @@ func TestGRPCLogger(t *testing.T) {
 			},
 			false,
 			true,
-			false,
+			true,
 		},
 		{
 			"collector_debug_level_grpc_log_debug",
@@ -48,7 +50,7 @@ func TestGRPCLogger(t *testing.T) {
 			},
 			false,
 			true,
-			false,
+			true,
 		},
 	}
 	for _, test := range tests {
@@ -63,7 +65,7 @@ func TestGRPCLogger(t *testing.T) {
 					obsWarn = true
 				}
 				if test.checkCallerLocation {
-					CallerInfo = entry.Caller.TrimmedPath()
+					CallerInfo = entry.Caller.String()
 				}
 				return nil
 			})
@@ -75,28 +77,106 @@ func TestGRPCLogger(t *testing.T) {
 			// create colGRPCLogger
 			glogger := SetLogger(logger, test.cfg.Level.Level())
 			assert.NotNil(t, glogger)
-
+			alogger := grpclog.Component("channelz")
 			glogger.Info(test.name)
 			glogger.Warning(test.name)
-			// create a wrapper function to test the caller location
-			if test.checkCallerLocation {
-				wrapper := func() {
-					glogger.Info("test message")
-				}
-				wrapper1 := func() {
-					wrapper()
-				}
-				wrapper2 := func() {
-					wrapper1()
-				}
-				wrapper3 := func() {
-					wrapper2()
-				}
-				wrapper3()
-				assert.Contains(t, CallerInfo, "grpclog/logger_test.go:95") // change line number to the line where wrapper3() is called in case of making changes
+			Info(alogger, nil, "Hello World")
+			if test.name != "collector_debug_level_grpc_log_debug" { // As info logs will only be logged in the zapcore.DebugLevel config logger
+				Warning(alogger, nil, "Hello World Warning") // and for the other 2 tests will only log warning logs so for testing infof we dont call this at last
+			}
+			if obsInfo && obsWarn {
+				assert.Contains(t, CallerInfo, "grpclog/logger_test.go:83")
+			} else {
+				assert.Contains(t, CallerInfo, "grpclog/logger_test.go:85")
 			}
 			assert.Equal(t, obsInfo, test.infoLogged)
 			assert.Equal(t, obsWarn, test.warnLogged)
 		})
 	}
+}
+
+// This is to emulate how logging happens in the channelz of grpc
+type Entity interface {
+	isEntity()
+	fmt.Stringer
+	id() int64
+}
+
+type Severity int
+
+const (
+	// CtUnknown indicates unknown severity of a trace event.
+	CtUnknown Severity = iota
+	// CtInfo indicates info level severity of a trace event.
+	CtInfo
+	// CtWarning indicates warning level severity of a trace event.
+	CtWarning
+	// CtError indicates error level severity of a trace event.
+	CtError
+)
+
+type TraceEvent struct {
+	Desc     string
+	Severity Severity
+	Parent   *TraceEvent
+}
+
+func AddTraceEvent(l grpclog.DepthLoggerV2, e Entity, depth int, desc *TraceEvent) {
+	// Log only the trace description associated with the bottom most entity.
+	d := fmt.Sprintf("[%s]%s", e, desc.Desc)
+	switch desc.Severity {
+	case CtUnknown, CtInfo:
+		l.InfoDepth(depth+1, d)
+	case CtWarning:
+		l.WarningDepth(depth+1, d)
+	case CtError:
+		l.ErrorDepth(depth+1, d)
+	}
+}
+
+// Info logs and adds a trace event if channelz is on.
+func Info(l grpclog.DepthLoggerV2, e Entity, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprint(args...),
+		Severity: 1,
+	})
+}
+
+// Infof logs and adds a trace event if channelz is on.
+func Infof(l grpclog.DepthLoggerV2, e Entity, format string, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprintf(format, args...),
+		Severity: CtInfo,
+	})
+}
+
+func Warning(l grpclog.DepthLoggerV2, e Entity, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprint(args...),
+		Severity: CtWarning,
+	})
+}
+
+// Warningf logs and adds a trace event if channelz is on.
+func Warningf(l grpclog.DepthLoggerV2, e Entity, format string, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprintf(format, args...),
+		Severity: CtWarning,
+	})
+}
+
+// Error logs and adds a trace event if channelz is on.
+func Error(l grpclog.DepthLoggerV2, e Entity, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprint(args...),
+		Severity: CtError,
+	})
+}
+
+// Errorf logs and adds a trace event if channelz is on.
+func Errorf(l grpclog.DepthLoggerV2, e Entity, format string, args ...any) {
+	AddTraceEvent(l, e, 1, &TraceEvent{
+		Desc:     fmt.Sprintf(format, args...),
+		Severity: CtError,
+	})
 }
