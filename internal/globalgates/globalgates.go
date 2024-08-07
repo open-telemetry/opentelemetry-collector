bebb922486d394db0a4f173af429ec4d9632846b
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-08-07 10:23:23 -0600

    Update internal/globalgates/globalgates.go

--- internal/globalgates/globalgates.go
+++ internal/globalgates/globalgates.go
@@ -8,7 +8,7 @@ import "go.opentelemetry.io/collector/featuregate"
 var UseUnifiedEnvVarExpansionRules = featuregate.GlobalRegistry().MustRegister("confmap.unifyEnvVarExpansion",
 	featuregate.StageStable,
 	featuregate.WithRegisterFromVersion("v0.103.0"),
-	featuregate.WithRegisterToVersion("v0.106.0"),
+	featuregate.WithRegisterToVersion("v0.109.0"),
 	featuregate.WithRegisterDescription("`${FOO}` will now be expanded as if it was `${env:FOO}` and no longer expands $ENV syntax. See https://github.com/open-telemetry/opentelemetry-collector/blob/main/docs/rfcs/env-vars.md for more details. When this feature gate is stable, expandconverter will be removed."))
 
 const StrictlyTypedInputID = "confmap.strictlyTypedInput"
