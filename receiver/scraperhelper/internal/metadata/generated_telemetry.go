7638bb298172330241375ed5dc3c50637caa645f
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-09 08:12:05 -0700

    [mdatagen] export ScopeName in internal/metadata package (#10845)
    This can be used by components that need to set their scope name
    manually. Will save component owners from having to store a variable,
    which may diverge from the scope name used by the component for emitting
    its own telemetry. There are about a dozen components in contrib that
    could use this today.
    
    Alternatively, I could just update the scope name strings in the
    components manually.
    
    ---------
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/scraperhelper/internal/metadata/generated_telemetry.go
+++ receiver/scraperhelper/internal/metadata/generated_telemetry.go
@@ -13,12 +13,14 @@ import (
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
+const ScopeName = "go.opentelemetry.io/collector/receiver/scraperhelper"
+
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter("go.opentelemetry.io/collector/receiver/scraperhelper")
+	return settings.MeterProvider.Meter(ScopeName)
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer("go.opentelemetry.io/collector/receiver/scraperhelper")
+	return settings.TracerProvider.Tracer(ScopeName)
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
