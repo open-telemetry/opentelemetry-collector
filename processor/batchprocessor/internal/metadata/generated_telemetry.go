ef07ea073562c4eb4d63a9032e84a802d8424fe2
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-09 12:10:11 -0700

    [mdatagen] move ScopeName to generated_status (#10848)
    This will make it available to most components, as the
    generated_telemetry is only generated for components that have internal
    telemetry configured. Arguably the scope name should be moved to its own
    file, but i'd rather not add yet another file if possible.
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- processor/batchprocessor/internal/metadata/generated_telemetry.go
+++ processor/batchprocessor/internal/metadata/generated_telemetry.go
@@ -14,14 +14,12 @@ import (
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
-const ScopeName = "go.opentelemetry.io/collector/processor/batchprocessor"
-
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter(ScopeName)
+	return settings.MeterProvider.Meter("go.opentelemetry.io/collector/processor/batchprocessor")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer(ScopeName)
+	return settings.TracerProvider.Tracer("go.opentelemetry.io/collector/processor/batchprocessor")
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
--- receiver/scraperhelper/internal/metadata/generated_telemetry.go
+++ receiver/scraperhelper/internal/metadata/generated_telemetry.go
@@ -13,14 +13,12 @@ import (
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
-const ScopeName = "go.opentelemetry.io/collector/receiver/scraperhelper"
-
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter(ScopeName)
+	return settings.MeterProvider.Meter("go.opentelemetry.io/collector/receiver/scraperhelper")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer(ScopeName)
+	return settings.TracerProvider.Tracer("go.opentelemetry.io/collector/receiver/scraperhelper")
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
