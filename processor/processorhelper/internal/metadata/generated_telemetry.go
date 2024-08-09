40efa0042c243683d42f6013208a911a39f4b335
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-09 11:42:47 -0700

    [mdatagen] move ScopeName to generated_status
    This will make it available to most components, as the generated_telemetry is only generated for components that have internal telemetry configured. Arguably the scope name should be moved to its own file, but i'd rather not add yet another file if possible.
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- processor/processorhelper/internal/metadata/generated_telemetry.go
+++ processor/processorhelper/internal/metadata/generated_telemetry.go
@@ -13,14 +13,12 @@ import (
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
-const ScopeName = "go.opentelemetry.io/collector/processor/processorhelper"
-
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter(ScopeName)
+	return settings.MeterProvider.Meter("go.opentelemetry.io/collector/processor/processorhelper")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer(ScopeName)
+	return settings.TracerProvider.Tracer("go.opentelemetry.io/collector/processor/processorhelper")
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
