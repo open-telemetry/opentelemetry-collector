998412ced7c4e894a7234da390644b48056b4dbc
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-12 16:26:27 -0700

    [service] Allow users to disable the tracer provider via the feature gate `service.noopTracerProvider` (#10859)
    Previously the service was returning an instance of a SDK tracer
    provider regardless of whether there were any processors configured
    causing resources to be consumed unnecessarily.
    
    Fixes #10858
    
    ---------
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- internal/globalgates/globalgates.go
+++ internal/globalgates/globalgates.go
@@ -24,3 +24,8 @@ var DisableOpenCensusBridge = featuregate.GlobalRegistry().MustRegister("service
 	featuregate.WithRegisterFromVersion("v0.105.0"),
 	featuregate.WithRegisterToVersion("v0.109.0"),
 	featuregate.WithRegisterDescription("`Disables the OpenCensus bridge meaning any component still using the OpenCensus SDK will no longer be able to produce telemetry."))
+
+var NoopTracerProvider = featuregate.GlobalRegistry().MustRegister("service.noopTracerProvider",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterFromVersion("v0.107.0"),
+	featuregate.WithRegisterDescription("Sets a Noop OpenTelemetry TracerProvider to reduce memory allocations. This featuregate is incompatible with the zPages extension."))
