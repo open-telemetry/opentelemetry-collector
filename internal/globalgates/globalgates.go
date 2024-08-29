5e296551d2007e59100b644de0fddac391b955cd
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-08-29 11:41:16 -0600

    [confmap] Remove stable confmap.unifyEnvVarExpansion feature gate (#11007)
    #### Description
    
    Removes stable `confmap.unifyEnvVarExpansion` feature gate.

--- internal/globalgates/globalgates.go
+++ internal/globalgates/globalgates.go
@@ -5,12 +5,6 @@ package globalgates // import "go.opentelemetry.io/collector/internal/globalgate
 
 import "go.opentelemetry.io/collector/featuregate"
 
-var UseUnifiedEnvVarExpansionRules = featuregate.GlobalRegistry().MustRegister("confmap.unifyEnvVarExpansion",
-	featuregate.StageStable,
-	featuregate.WithRegisterFromVersion("v0.103.0"),
-	featuregate.WithRegisterToVersion("v0.109.0"),
-	featuregate.WithRegisterDescription("`${FOO}` will now be expanded as if it was `${env:FOO}` and no longer expands $ENV syntax. See https://github.com/open-telemetry/opentelemetry-collector/blob/main/docs/rfcs/env-vars.md for more details. When this feature gate is stable, expandconverter will be removed."))
-
 var NoopTracerProvider = featuregate.GlobalRegistry().MustRegister("service.noopTracerProvider",
 	featuregate.StageAlpha,
 	featuregate.WithRegisterFromVersion("v0.107.0"),
