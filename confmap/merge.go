7b70ee8b93b08644377748ef0eab21a89c58a44a
Vihas Makwana <vihas.makwana@elastic.co>
2024-09-04 20:23:29 +0530

    chore: rename

--- confmap/merge.go
+++ confmap/merge.go
@@ -18,61 +18,61 @@ func mergeComponentsAppend(new, old map[string]any) error {
 		if newSer, ok := newService.(map[string]any); ok {
 			mergeServices(newSer, oldSer)
 			// override the `service` in new config
-			new["service"] = oldSer
+			new["service"] = newSer
 		}
 	}
 	maps.Merge(new, old)
 	return nil
 }
 
-func mergeServices(src, dest map[string]any) {
-	for srcKey, sVal := range src {
-		dVal, destOk := dest[srcKey]
-		if !destOk {
-			dest[srcKey] = sVal
+func mergeServices(new, old map[string]any) {
+	for oldKey, oVal := range old {
+		nVal, newOk := new[oldKey]
+		if !newOk {
+			new[oldKey] = oVal
 			continue
 		}
 
-		destVal := reflect.ValueOf(dVal)
-		srcVal := reflect.ValueOf(sVal)
+		newVal := reflect.ValueOf(nVal)
+		oldVal := reflect.ValueOf(oVal)
 
-		if destVal.Kind() != srcVal.Kind() {
+		if newVal.Kind() != oldVal.Kind() {
 			// different kinds, override the old config
-			dest[srcKey] = sVal
+			new[oldKey] = oVal
 			continue
 		}
 
-		switch srcVal.Kind() {
+		switch oldVal.Kind() {
 		case reflect.Array, reflect.Slice:
 			// both of them are array. Merge them
-			dest[srcKey] = mergeSlice(destVal, srcVal)
+			new[oldKey] = mergeSlice(oldVal, newVal)
 		case reflect.Map:
 			// both of them are maps. Recursively call the MergeAppend
-			mergeServices(sVal.(map[string]any), dVal.(map[string]any))
+			mergeServices(nVal.(map[string]any), oVal.(map[string]any))
 		default:
 			// Default case, override the old config
-			dest[srcKey] = sVal
+			new[oldKey] = oVal
 		}
 	}
 }
 
-func mergeSlice(src, dest reflect.Value) any {
-	if src.Type() != dest.Type() {
-		return dest
+func mergeSlice(old, new reflect.Value) any {
+	if old.Type() != new.Type() {
+		return new
 	}
-	slice := reflect.MakeSlice(src.Type(), 0, src.Cap()+dest.Cap())
-	for i := 0; i < src.Len(); i++ {
-		slice = reflect.Append(slice, src.Index(i))
+	slice := reflect.MakeSlice(old.Type(), 0, old.Cap()+new.Cap())
+	for i := 0; i < old.Len(); i++ {
+		slice = reflect.Append(slice, old.Index(i))
 	}
 
 OUTER2:
-	for i := 0; i < dest.Len(); i++ {
+	for i := 0; i < new.Len(); i++ {
 		for j := 0; j < slice.Len(); j++ {
-			if slice.Index(j).Equal(dest.Index(i)) {
+			if slice.Index(j).Equal(new.Index(i)) {
 				continue OUTER2
 			}
 		}
-		slice = reflect.Append(slice, dest.Index(i))
+		slice = reflect.Append(slice, new.Index(i))
 	}
 	return slice.Interface()
 }
