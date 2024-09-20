c3766664b4c34207094f5c01314ed1517bceeaee
Juliano Costa <juliano.costa@datadoghq.com>
2024-09-20 13:10:43 +0200

    Move extended profile to otlptext

--- exporter/internal/otlptext/profiles_test.go
+++ exporter/internal/otlptext/profiles_test.go
@@ -29,7 +29,7 @@ func TestProfilesText(t *testing.T) {
 		},
 		{
 			name: "two_profiles",
-			in:   testdata.GenerateExtendedProfiles(2),
+			in:   extendProfiles(testdata.GenerateProfiles(2)),
 			out:  "two_profiles.out",
 		},
 	}
@@ -43,4 +43,78 @@ func TestProfilesText(t *testing.T) {
 			assert.Equal(t, expected, string(got))
 		})
 	}
+}
+
+// GenerateExtendedProfiles generates dummy profiling data with extended values for tests
+func extendProfiles(profiles pprofile.Profiles) (pprofile.Profiles) {
+	sc := profiles.ResourceProfiles().At(0).ScopeProfiles().At(0)
+	profilesCount := profiles.ResourceProfiles().At(0).ScopeProfiles().At(0).Profiles().Len()
+	for i := 0; i < profilesCount; i++ {
+		switch i % 2 {
+		case 0:
+			profile := sc.Profiles().At(i)
+			profile.Profile().LocationIndices().FromRaw([]int64{1})
+			label := profile.Profile().Sample().At(0).Label().AppendEmpty()
+			label.SetKey(1)
+			label.SetStr(2)
+			label.SetNum(3)
+			label.SetNumUnit(4)
+		
+			location := profile.Profile().Location().AppendEmpty()
+			location.SetID(2)
+			location.SetMappingIndex(3)
+			location.SetAddress(4)
+			line := location.Line().AppendEmpty()
+			line.SetFunctionIndex(1)
+			line.SetLine(2)
+			line.SetColumn(3)
+			location.SetIsFolded(true)
+			location.SetTypeIndex(5)
+			location.Attributes().FromRaw([]uint64{6, 7})
+		
+			profile.Profile().AttributeTable().FromRaw(map[string]any{
+				"key": "answer",
+				"value": map[string]any{
+					"intValue": "42",
+				},
+			})
+		
+			attributeUnits := profile.Profile().AttributeUnits().AppendEmpty()
+			attributeUnits.SetAttributeKey(1)
+			attributeUnits.SetUnit(5)
+		
+			profile.Profile().StringTable().Append("foobar")
+		case 1:
+			profile := sc.Profiles().At(i)
+			profile.Profile().SetDropFrames(1)
+			profile.Profile().SetKeepFrames(2)
+
+			mapping := profile.Profile().Mapping().AppendEmpty()
+			mapping.SetID(1)
+			mapping.SetMemoryStart(2)
+			mapping.SetMemoryLimit(3)
+			mapping.SetFileOffset(4)
+			mapping.SetFilename(5)
+			mapping.SetBuildID(6)
+			mapping.Attributes().FromRaw([]uint64{7, 8})
+			mapping.SetHasFunctions(true)
+			mapping.SetHasFilenames(true)
+			mapping.SetHasLineNumbers(true)
+			mapping.SetHasInlineFrames(true)
+
+			function := profile.Profile().Function().AppendEmpty()
+			function.SetID(1)
+			function.SetName(2)
+			function.SetSystemName(3)
+			function.SetFilename(4)
+			function.SetStartLine(5)
+
+			linkTable := profile.Profile().LinkTable().AppendEmpty()
+			linkTable.SetTraceID([16]byte{0x03, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10})
+			linkTable.SetSpanID([8]byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18})
+
+			profile.Profile().Comment().FromRaw([]int64{1, 2})
+		}
+	}
+	return profiles
 }
