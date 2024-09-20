cff8392708ba56ac2f005bafcce158fc31ee1cef
Vihas Makwana <vihas.makwana@elastic.co>
2024-09-20 20:03:31 +0530

    chore: add an option to add span prefix, changelog, tests

--- config/confighttp/confighttp.go
+++ config/confighttp/confighttp.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/rs/cors"
@@ -377,6 +378,7 @@ func (hss *ServerConfig) ToListener(ctx context.Context) (net.Listener, error) {
 type toServerOptions struct {
 	errHandler func(w http.ResponseWriter, r *http.Request, errorMsg string, statusCode int)
 	decoders   map[string]func(body io.ReadCloser) (io.ReadCloser, error)
+	spanPrefix string
 }
 
 // ToServerOption is an option to change the behavior of the HTTP server
@@ -407,6 +409,14 @@ func WithDecoder(key string, dec func(body io.ReadCloser) (io.ReadCloser, error)
 			opts.decoders = map[string]func(body io.ReadCloser) (io.ReadCloser, error){}
 		}
 		opts.decoders[key] = dec
+	})
+}
+
+// WithSpanPrefix creates a span prefixed with the specified name.
+// Ideally, this prefix should be the component's ID.
+func WithSpanPrefix(spanPrefix string) ToServerOption {
+	return toServerOptionFunc(func(opts *toServerOptions) {
+		opts.spanPrefix = spanPrefix
 	})
 }
 
@@ -462,15 +472,14 @@ func (hss *ServerConfig) ToServer(_ context.Context, host component.Host, settin
 	otelOpts := []otelhttp.Option{
 		otelhttp.WithTracerProvider(settings.TracerProvider),
 		otelhttp.WithPropagators(otel.GetTextMapPropagator()),
-		otelhttp.WithSpanNameFormatter(func(_ string, r *http.Request) string {
-			return r.URL.Path
+		otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+			return strings.TrimPrefix(fmt.Sprintf("%s:%s", operation, r.URL.Path), ":")
 		}),
 		otelhttp.WithMeterProvider(settings.LeveledMeterProvider(configtelemetry.LevelDetailed)),
 	}
 
 	// Enable OpenTelemetry observability plugin.
-	// TODO: Consider to use component ID string as prefix for all the operations.
-	handler = otelhttp.NewHandler(handler, "", otelOpts...)
+	handler = otelhttp.NewHandler(handler, serverOpts.spanPrefix, otelOpts...)
 
 	// wrap the current handler in an interceptor that will add client.Info to the request's context
 	handler = &clientInfoHandler{
