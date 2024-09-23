ff2aee8e5d619c4d26c9486346ac3a3d74b49f1f
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-09-23 11:24:24 -0600

    use pipeline directly

--- service/internal/graph/host.go
+++ service/internal/graph/host.go
@@ -100,7 +100,7 @@ func (host *Host) GetExporters() map[component.DataType]map[component.ID]compone
 // connector. See https://github.com/open-telemetry/opentelemetry-collector/issues/7370 and
 // https://github.com/open-telemetry/opentelemetry-collector/pull/7390#issuecomment-1483710184
 // for additional information.
-func (host *Host) GetExportersWithSignal() map[globalsignal.Signal]map[component.ID]component.Component {
+func (host *Host) GetExportersWithSignal() map[pipeline.Signal]map[component.ID]component.Component {
 	return host.Pipelines.GetExporters()
 }
 
