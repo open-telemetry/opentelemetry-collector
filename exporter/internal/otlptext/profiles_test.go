09187b3e473a248281ecc0a34eeed42d631ad5ba
Juliano Costa <juliano.costa@datadoghq.com>
2024-09-23 15:07:20 +0200

    Simplify attribute table

--- exporter/internal/otlptext/profiles_test.go
+++ exporter/internal/otlptext/profiles_test.go
@@ -6,7 +6,6 @@ package otlptext
 import (
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
 	"testing"
 
@@ -73,22 +72,11 @@ func extendProfiles(profiles pprofile.Profiles) pprofile.Profiles {
 			location.SetTypeIndex(5)
 			location.Attributes().FromRaw([]uint64{6, 7})
 
-			attributeTable := map[string]any{
-				"key": "answer",
+			profile.Profile().AttributeTable().FromRaw(map[string]any{
 				"value": map[string]any{
 					"intValue": "42",
 				},
-			}
-			sortedAttributeTable := make(map[string]any)
-			sortedKeys := make([]string, 0, len(attributeTable))
-			for k := range attributeTable {
-				sortedKeys = append(sortedKeys, k)
-			}
-			sort.Strings(sortedKeys)
-			for _, k := range sortedKeys {
-				sortedAttributeTable[k] = attributeTable[k]
-			}
-			_ = profile.Profile().AttributeTable().FromRaw(sortedAttributeTable)
+			})
 
 			attributeUnits := profile.Profile().AttributeUnits().AppendEmpty()
 			attributeUnits.SetAttributeKey(1)
