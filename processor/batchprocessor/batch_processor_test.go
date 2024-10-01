ff78f2292e74b18ab4537702d5f220d0944169bb
Joshua MacDonald <josh.macdonald@gmail.com>
2024-10-01 13:06:47 -0700

    whitespace

--- processor/batchprocessor/batch_processor_test.go
+++ processor/batchprocessor/batch_processor_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
