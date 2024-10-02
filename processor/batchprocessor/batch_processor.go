b0168e2ac168f5e7ca3e5aa58c496f2d32e3ae95
Shivanth MP <shivanth.metchem@deliveryhero.com>
2024-10-02 23:54:39 +0200

    Update batch_processor.go

--- processor/batchprocessor/batch_processor.go
+++ processor/batchprocessor/batch_processor.go
@@ -1,7 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build !1.23
 
 package batchprocessor // import "go.opentelemetry.io/collector/processor/batchprocessor"
 
