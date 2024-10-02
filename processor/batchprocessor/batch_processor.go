43ccc5e4f6412b2056ace87de7c907528b248ce2
Bogdan Drutu <bogdandrutu@gmail.com>
2024-10-02 15:59:52 -0700

    Update processor/batchprocessor/batch_processor.go
    Co-authored-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- processor/batchprocessor/batch_processor.go
+++ processor/batchprocessor/batch_processor.go
@@ -1,7 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-
 package batchprocessor // import "go.opentelemetry.io/collector/processor/batchprocessor"
 
 import (
