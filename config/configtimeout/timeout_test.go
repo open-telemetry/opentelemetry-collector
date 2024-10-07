a8d664c35f04a3b69f8b6fde672d985f4858bf59
Joshua MacDonald <josh.macdonald@gmail.com>
2024-10-07 14:50:15 -0700

    coverage

--- config/configtimeout/timeout_test.go
+++ config/configtimeout/timeout_test.go
@@ -4,10 +4,14 @@
 package configtimeout
 
 import (
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
 func TestUnmarshalTextValid(t *testing.T) {
@@ -40,7 +44,56 @@ func TestUnmarshalTextInvalid(t *testing.T) {
 	}
 }
 
-func TestTimeoutDefaultPolicy(t *testing.T) {
-	// The default behavior matches the legacy exporterhelper code base.
-	require.Equal(t, PolicySustain, PolicyDefault)
+func TestDefaultConfig(t *testing.T) {
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "default.yaml"))
+	require.NoError(t, err)
+	cfg := NewDefaultConfig()
+	require.NoError(t, cm.Unmarshal(&cfg))
+	assert.Equal(t,
+		TimeoutConfig{
+			Timeout:            5 * time.Second,
+			ShortTimeoutPolicy: "",
+		}, cfg)
+}
+
+func TestDefaulPolicy(t *testing.T) {
+	// The legacy behavior of the exporterhelper is to sustain a short timeout.
+	assert.Equal(t, PolicySustain, PolicyDefault)
+}
+
+func TestUnmarshalConfig(t *testing.T) {
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+	cfg := NewDefaultConfig()
+	require.NoError(t, cm.Unmarshal(&cfg))
+	assert.Equal(t,
+		TimeoutConfig{
+			Timeout:            17 * time.Second,
+			ShortTimeoutPolicy: "abort",
+		}, cfg)
+}
+
+func TestUnmarshalInvalidConfig(t *testing.T) {
+	type test struct {
+		file   string
+		expect string
+	}
+	for _, tt := range []test{
+		{
+			file:   "invalid_timeout.yaml",
+			expect: "'timeout' must be non-negative",
+		},
+		{
+			file:   "invalid_policy.yaml",
+			expect: "unsupported 'short_timeout_policy' hope",
+		},
+	} {
+		t.Run(tt.file, func(t *testing.T) {
+			cm, err := confmaptest.LoadConf(filepath.Join("testdata", tt.file))
+			require.NoError(t, err)
+			cfg := NewDefaultConfig()
+			assert.NoError(t, cm.Unmarshal(&cfg))
+			assert.ErrorContains(t, component.ValidateConfig(cfg), tt.expect)
+		})
+	}
 }
