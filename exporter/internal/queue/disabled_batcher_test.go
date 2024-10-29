f1de0ff6f4e0c6f7eaa6d1fb1b9231eb3fa38e16
Sindy Li <sindy.li@snowflake.com>
2024-10-28 18:45:00 -0700

    [exporterqueue] Limited worker pool support for queue batcher (#11540)
    #### Description
    
    This PR follows
    https://github.com/open-telemetry/opentelemetry-collector/pull/11532 and
    implements support for limited worker pool for queue batcher.
    
    Design doc:
    
    https://docs.google.com/document/d/1y5jt7bQ6HWt04MntF8CjUwMBBeNiJs2gV4uUZfJjAsE/edit?usp=sharing
    
    #### Link to tracking issue
    
    https://github.com/open-telemetry/opentelemetry-collector/issues/8122
    https://github.com/open-telemetry/opentelemetry-collector/issues/10368

--- exporter/internal/queue/disabled_batcher_test.go
+++ exporter/internal/queue/disabled_batcher_test.go
@@ -17,60 +17,58 @@ import (
 	"go.opentelemetry.io/collector/exporter/internal"
 )
 
-func TestDisabledBatcher_InfiniteWorkerPool(t *testing.T) {
-	cfg := exporterbatcher.NewDefaultConfig()
-	cfg.Enabled = false
+func TestDisabledBatcher_Basic(t *testing.T) {
+	tests := []struct {
+		name       string
+		maxWorkers int
+	}{
+		{
+			name:       "infinate_workers",
+			maxWorkers: 0,
+		},
+		{
+			name:       "one_worker",
+			maxWorkers: 1,
+		},
+		{
+			name:       "three_workers",
+			maxWorkers: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := exporterbatcher.NewDefaultConfig()
+			cfg.Enabled = false
 
-	q := NewBoundedMemoryQueue[internal.Request](
-		MemoryQueueSettings[internal.Request]{
-			Sizer:    &RequestSizer[internal.Request]{},
-			Capacity: 10,
+			q := NewBoundedMemoryQueue[internal.Request](
+				MemoryQueueSettings[internal.Request]{
+					Sizer:    &RequestSizer[internal.Request]{},
+					Capacity: 10,
+				})
+
+			ba, err := NewBatcher(cfg, q, tt.maxWorkers)
+			require.NoError(t, err)
+
+			require.NoError(t, q.Start(context.Background(), componenttest.NewNopHost()))
+			require.NoError(t, ba.Start(context.Background(), componenttest.NewNopHost()))
+			t.Cleanup(func() {
+				require.NoError(t, q.Shutdown(context.Background()))
+				require.NoError(t, ba.Shutdown(context.Background()))
+			})
+
+			sink := newFakeRequestSink()
+
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 8, sink: sink}))
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 8, exportErr: errors.New("transient error"), sink: sink}))
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 17, sink: sink}))
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 13, sink: sink}))
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 35, sink: sink}))
+			require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 2, sink: sink}))
+			assert.Eventually(t, func() bool {
+				return sink.requestsCount.Load() == 5 && sink.itemsCount.Load() == 75
+			}, 30*time.Millisecond, 10*time.Millisecond)
 		})
-
-	maxWorkers := 0
-	ba, err := NewBatcher(cfg, q, maxWorkers)
-	require.NoError(t, err)
-
-	require.NoError(t, q.Start(context.Background(), componenttest.NewNopHost()))
-	require.NoError(t, ba.Start(context.Background(), componenttest.NewNopHost()))
-	t.Cleanup(func() {
-		require.NoError(t, q.Shutdown(context.Background()))
-		require.NoError(t, ba.Shutdown(context.Background()))
-	})
-
-	sink := newFakeRequestSink()
-
-	require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 8, sink: sink}))
-	require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 8, exportErr: errors.New("transient error"), sink: sink}))
-	assert.Eventually(t, func() bool {
-		return sink.requestsCount.Load() == 1 && sink.itemsCount.Load() == 8
-	}, 30*time.Millisecond, 10*time.Millisecond)
-
-	require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 17, sink: sink}))
-	assert.Eventually(t, func() bool {
-		return sink.requestsCount.Load() == 2 && sink.itemsCount.Load() == 25
-	}, 30*time.Millisecond, 10*time.Millisecond)
-
-	require.NoError(t, q.Offer(context.Background(), &fakeRequest{items: 13, sink: sink}))
-
-	assert.Eventually(t, func() bool {
-		return sink.requestsCount.Load() == 3 && sink.itemsCount.Load() == 38
-	}, 30*time.Millisecond, 10*time.Millisecond)
-}
-
-func TestDisabledBatcher_LimitedWorkerNotImplemented(t *testing.T) {
-	cfg := exporterbatcher.NewDefaultConfig()
-	cfg.Enabled = false
-	maxWorkers := 1
-
-	q := NewBoundedMemoryQueue[internal.Request](
-		MemoryQueueSettings[internal.Request]{
-			Sizer:    &RequestSizer[internal.Request]{},
-			Capacity: 10,
-		})
-
-	_, err := NewBatcher(cfg, q, maxWorkers)
-	require.Error(t, err)
+	}
 }
 
 func TestDisabledBatcher_BatchingNotImplemented(t *testing.T) {
