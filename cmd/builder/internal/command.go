82651977946ce55895af20aebe768a5cde1e7626
Bogdan Drutu <bogdandrutu@gmail.com>
2024-10-30 10:01:09 -0700

    [chore] Remove unused version checks in builder config (#11568)
    Updates
    https://github.com/open-telemetry/opentelemetry-collector/issues/11405
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- cmd/builder/internal/command.go
+++ cmd/builder/internal/command.go
@@ -66,10 +66,6 @@ configuration is provided, ocb will generate a default Collector.
 				return fmt.Errorf("go not found: %w", err)
 			}
 
-			if err := cfg.SetBackwardsCompatibility(); err != nil {
-				return fmt.Errorf("unable to compare otelcol version: %w", err)
-			}
-
 			if err := cfg.ParseModules(); err != nil {
 				return fmt.Errorf("invalid module configuration: %w", err)
 			}
