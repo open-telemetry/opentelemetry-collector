e63c5c94ffc7e35bb1d0043186a2fff122d71007
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-10-30 15:15:45 -0700

    set meter provider config in SDK initialization
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- service/service.go
+++ service/service.go
@@ -133,6 +133,9 @@ func New(ctx context.Context, set Settings, cfg Config) (*Service, error) {
 				LoggerProvider: &config.LoggerProvider{
 					Processors: cfg.Telemetry.Logs.Processors,
 				},
+				MeterProvider: &config.MeterProvider{
+					Readers: cfg.Telemetry.Metrics.Readers,
+				},
 				TracerProvider: &config.TracerProvider{
 					Processors: cfg.Telemetry.Traces.Processors,
 				},
--- service/telemetry/factory.go
+++ service/telemetry/factory.go
@@ -74,10 +74,10 @@ func NewFactory() Factory {
 			c := *cfg.(*Config)
 			return newTracerProvider(set, c)
 		}),
-		withMeterProvider(func(ctx context.Context, set Settings, cfg component.Config) (metric.MeterProvider, error) {
+		withMeterProvider(func(_ context.Context, set Settings, cfg component.Config) (metric.MeterProvider, error) {
 			c := *cfg.(*Config)
 			disableHighCard := disableHighCardinalityMetricsFeatureGate.IsEnabled()
-			return newMeterProvider(ctx, set, c, disableHighCard)
+			return newMeterProvider(set, c, disableHighCard)
 		}),
 	)
 }
