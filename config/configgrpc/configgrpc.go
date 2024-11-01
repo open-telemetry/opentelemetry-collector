726d8032dd64eab448a913ce7d949e26f4eb5c4f
Alex Kats <akats71@gmail.com>
2024-10-31 23:50:01 -0400

    updated nolint comment

--- config/configgrpc/configgrpc.go
+++ config/configgrpc/configgrpc.go
@@ -261,7 +261,7 @@ func (gcs *ClientConfig) ToClientConn(
 	if err != nil {
 		return nil, err
 	}
-	//nolint:staticcheck
+	//nolint:staticcheck SA1019 see https://github.com/open-telemetry/opentelemetry-collector/pull/11575
 	return grpc.DialContext(ctx, gcs.sanitizedEndpoint(), grpcOpts...)
 }
 
