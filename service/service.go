a98858811df233ddd7a7a7158f128e2ed0bcfbc5
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-11-05 14:56:08 -0800

    ensure metric level none disables metric readers
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- service/service.go
+++ service/service.go
@@ -136,6 +136,11 @@ func New(ctx context.Context, set Settings, cfg Config) (*Service, error) {
 
 	views := disableHighCardinalityMetrics()
 
+	readers := cfg.Telemetry.Metrics.Readers
+	if cfg.Telemetry.Metrics.Level == configtelemetry.LevelNone {
+		readers = []config.MetricReader{}
+	}
+
 	sdk, err := config.NewSDK(
 		config.WithContext(ctx),
 		config.WithOpenTelemetryConfiguration(
@@ -144,7 +149,7 @@ func New(ctx context.Context, set Settings, cfg Config) (*Service, error) {
 					Processors: cfg.Telemetry.Logs.Processors,
 				},
 				MeterProvider: &config.MeterProvider{
-					Readers: cfg.Telemetry.Metrics.Readers,
+					Readers: readers,
 					Views:   views,
 				},
 				TracerProvider: &config.TracerProvider{
