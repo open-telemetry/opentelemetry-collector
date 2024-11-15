2lambda123/open-telemetry-opentelemetry-collector

Rever public API changes in #10931, preserve logic
Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

cmd/mdatagen/internal/samplereceiver/generated_component_telemetry_test.go
@@ -29,6 +29,7 @@ func (tt *componentTestTelemetry) NewSettings() receiver.Settings {
 	settings.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}
+	settings.MetricsLevel = configtelemetry.LevelDetailed
 	settings.ID = component.NewID(component.MustNewType("sample"))
 
 	return settings

processor/batchprocessor/generated_component_telemetry_test.go
@@ -29,6 +29,7 @@ func (tt *componentTestTelemetry) NewSettings() processor.Settings {
 	settings.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}
+	settings.MetricsLevel = configtelemetry.LevelDetailed
 	settings.ID = component.NewID(component.MustNewType("batch"))
 
 	return settings

processor/processorhelper/generated_component_telemetry_test.go
@@ -29,6 +29,7 @@ func (tt *componentTestTelemetry) NewSettings() processor.Settings {
 	settings.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}
+	settings.MetricsLevel = configtelemetry.LevelDetailed
 	settings.ID = component.NewID(component.MustNewType("processorhelper"))
 
 	return settings

receiver/receiverhelper/generated_component_telemetry_test.go
@@ -29,6 +29,7 @@ func (tt *componentTestTelemetry) NewSettings() receiver.Settings {
 	settings.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}
+	settings.MetricsLevel = configtelemetry.LevelDetailed
 	settings.ID = component.NewID(component.MustNewType("receiverhelper"))
 
 	return settings

open-telemetry/opentelemetry-collector

cmd/mdatagen/internal/samplereceiver/generated_component_telemetry_test.go
@@ -26,14 +26,15 @@ type componentTestTelemetry struct {
 
 func (tt *componentTestTelemetry) NewSettings() receiver.Settings {
 	set := receivertest.NewNopSettings()
-	set.TelemetrySettings = tt.newTelemetrySettings()
 	set.ID = component.NewID(component.MustNewType("sample"))
+	set.TelemetrySettings = tt.newTelemetrySettings()
 	return set
 }
 
 func (tt *componentTestTelemetry) newTelemetrySettings() component.TelemetrySettings {
 	set := componenttest.NewNopTelemetrySettings()
 	set.MeterProvider = tt.meterProvider
+	set.MetricsLevel = configtelemetry.LevelDetailed
 	set.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}

processor/batchprocessor/generated_component_telemetry_test.go
@@ -26,14 +26,15 @@ type componentTestTelemetry struct {
 
 func (tt *componentTestTelemetry) NewSettings() processor.Settings {
 	set := processortest.NewNopSettings()
-	set.TelemetrySettings = tt.newTelemetrySettings()
 	set.ID = component.NewID(component.MustNewType("batch"))
+	set.TelemetrySettings = tt.newTelemetrySettings()
 	return set
 }
 
 func (tt *componentTestTelemetry) newTelemetrySettings() component.TelemetrySettings {
 	set := componenttest.NewNopTelemetrySettings()
 	set.MeterProvider = tt.meterProvider
+	set.MetricsLevel = configtelemetry.LevelDetailed
 	set.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}

processor/processorhelper/generated_component_telemetry_test.go
@@ -26,14 +26,15 @@ type componentTestTelemetry struct {
 
 func (tt *componentTestTelemetry) NewSettings() processor.Settings {
 	set := processortest.NewNopSettings()
-	set.TelemetrySettings = tt.newTelemetrySettings()
 	set.ID = component.NewID(component.MustNewType("processorhelper"))
+	set.TelemetrySettings = tt.newTelemetrySettings()
 	return set
 }
 
 func (tt *componentTestTelemetry) newTelemetrySettings() component.TelemetrySettings {
 	set := componenttest.NewNopTelemetrySettings()
 	set.MeterProvider = tt.meterProvider
+	set.MetricsLevel = configtelemetry.LevelDetailed
 	set.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}

receiver/receiverhelper/generated_component_telemetry_test.go
@@ -26,14 +26,15 @@ type componentTestTelemetry struct {
 
 func (tt *componentTestTelemetry) NewSettings() receiver.Settings {
 	set := receivertest.NewNopSettings()
-	set.TelemetrySettings = tt.newTelemetrySettings()
 	set.ID = component.NewID(component.MustNewType("receiverhelper"))
+	set.TelemetrySettings = tt.newTelemetrySettings()
 	return set
 }
 
 func (tt *componentTestTelemetry) newTelemetrySettings() component.TelemetrySettings {
 	set := componenttest.NewNopTelemetrySettings()
 	set.MeterProvider = tt.meterProvider
+	set.MetricsLevel = configtelemetry.LevelDetailed
 	set.LeveledMeterProvider = func(_ configtelemetry.Level) metric.MeterProvider {
 		return tt.meterProvider
 	}