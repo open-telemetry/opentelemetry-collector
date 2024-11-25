9a22e8aa4fd40b86f1ded61cc425b0d428b87d7d
Jade Guiton <jade.guiton@datadoghq.com>
2024-11-25 16:18:58 +0100

    Added copyright notice and uniformized script formatting

--- .github/workflows/scripts/check-merge-freeze.sh
+++ .github/workflows/scripts/check-merge-freeze.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
+#
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
 
-BLOCKERS=$(gh pr list --search "label:release:prepare" --json url --jq '.[].url' --repo "${REPO}")
+BLOCKERS=$( gh pr list --search "label:release:prepare" --json url --jq '.[].url' --repo "${REPO}" )
 if [ "${BLOCKERS}" != "" ]; then
-  echo "Merging in main is frozen by release PR: ${BLOCKERS}"
-  exit 1
+    echo "Merging in main is frozen while release PR is open: ${BLOCKERS}"
+    exit 1
 fi
