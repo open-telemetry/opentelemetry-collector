ea8aa3eea462f7f9f2ef64464bc297af7e986b08
Jade Guiton <jade.guiton@datadoghq.com>
2024-11-26 11:10:26 +0100

    Changed label name, added comments

--- .github/workflows/scripts/check-merge-freeze.sh
+++ .github/workflows/scripts/check-merge-freeze.sh
@@ -3,8 +3,8 @@
 # Copyright The OpenTelemetry Authors
 # SPDX-License-Identifier: Apache-2.0
 
-BLOCKERS=$( gh pr list --search "label:release:prepare" --json url --jq '.[].url' --repo "${REPO}" )
+BLOCKERS=$( gh pr list --search "label:release:merge-freeze" --json url --jq '.[].url' --repo "${REPO}" )
 if [ "${BLOCKERS}" != "" ]; then
-    echo "Merging in main is frozen while release PR is open: ${BLOCKERS}"
+    echo "Merging in main is frozen, as there are open PRs labeled 'release:merge-freeze': ${BLOCKERS}"
     exit 1
 fi
