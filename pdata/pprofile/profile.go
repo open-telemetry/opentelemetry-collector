3201ea6fc062c42575d66d2266383f21e2426f53
Damien Mathieu <42@dmathieu.com>
2024-11-27 18:43:29 +0100

    Update pdata/pprofile/profile.go
    Co-authored-by: Pablo Baeyens <pbaeyens31+github@gmail.com>

--- pdata/pprofile/profile.go
+++ pdata/pprofile/profile.go
@@ -14,6 +14,6 @@ func (ms Profile) EndTime() pcommon.Timestamp {
 
 // SetEndTime replaces the end time associated with this Profile.
 //
-// Deprecated: This field has been removed, and replaced with Duration
+// Deprecated: [v0.115.0] This field has been removed, and replaced with Duration
 func (ms Profile) SetEndTime(pcommon.Timestamp) {
 }
