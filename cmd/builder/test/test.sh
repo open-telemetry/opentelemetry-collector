20441eedda35d2fc7308f36ff8b406d5999102d4
Jade Guiton <jade.guiton@datadoghq.com>
2024-12-04 18:04:21 +0100

    Add comment

--- cmd/builder/test/test.sh
+++ cmd/builder/test/test.sh
@@ -114,6 +114,7 @@ base=$(mktemp -d)
 echo "Running the tests in ${base}"
 
 replaces="$base/replaces"
+# Get path of all core modules, in sorted order, without the initial dot
 core_mods=$(cd ../.. && find . -type f -name "go.mod" -exec dirname {} \; | sort | sed 's/.//')
 echo "replaces:" >> "$replaces"
 for mod in $core_mods; do
