39361583db8c55e082fc9c68c7b7e8c0d300f10e
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-12-11 11:48:41 -0800

    fix package statements
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- service/telemetry/internal/migration/v0.3.0.go
+++ service/telemetry/internal/migration/v0.3.0.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package migration
+package migration // import "go.opentelemetry.io/collector/service/telemetry/internal/migration"
 
 import (
 	"time"
