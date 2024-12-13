680526595548972a324504a5540df799dc22932b
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2024-12-13 12:04:07 -0500

    Demonstrate []any(nil) and []any{} differentiation

--- confmap/confmap_test.go
+++ confmap/confmap_test.go
@@ -691,6 +691,48 @@ func TestZeroSliceHookFunc(t *testing.T) {
 	}
 }
 
+func TestNilValuesUnchanged(t *testing.T) {
+	type structWithSlices struct {
+		Strings []string `mapstructure:"strings"`
+	}
+
+	slicesStruct := &structWithSlices{}
+
+	nilCfg := map[string]any{
+		"strings": []any(nil),
+	}
+	nilConf := NewFromStringMap(nilCfg)
+	err := nilConf.Unmarshal(slicesStruct)
+	require.NoError(t, err)
+
+	confFromStruct := New()
+	confFromStruct.Marshal(slicesStruct)
+
+	require.Equal(t, nilCfg, nilConf.ToStringMap())
+	require.EqualValues(t, nilConf.ToStringMap(), confFromStruct.ToStringMap())
+}
+
+func TestEmptySliceUnchanged(t *testing.T) {
+	type structWithSlices struct {
+		Strings []string `mapstructure:"strings"`
+	}
+
+	slicesStruct := &structWithSlices{}
+
+	nilCfg := map[string]any{
+		"strings": []any{},
+	}
+	nilConf := NewFromStringMap(nilCfg)
+	err := nilConf.Unmarshal(slicesStruct)
+	require.NoError(t, err)
+
+	confFromStruct := New()
+	confFromStruct.Marshal(slicesStruct)
+
+	require.Equal(t, nilCfg, nilConf.ToStringMap())
+	require.EqualValues(t, nilConf.ToStringMap(), confFromStruct.ToStringMap())
+}
+
 type C struct {
 	Modifiers []string `mapstructure:"modifiers"`
 }
