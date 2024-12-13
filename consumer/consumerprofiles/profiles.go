955820ceb0ff67cb0db35952380b5a4635a71ebd
Dmitrii Anoshin <anoshindx@gmail.com>
2024-12-12 22:12:13 -0800

    [chore] Update deprecation comments in consumerprofiles module

--- consumer/consumerprofiles/profiles.go
+++ consumer/consumerprofiles/profiles.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Deprecated: [0.115.0] This package is deprecated. Use "go.opentelemetry.io/collector/consumer/xconsumer" instead.
+// Deprecated: [0.116.0] This package is deprecated. Use "go.opentelemetry.io/collector/consumer/xconsumer" instead.
 package consumerprofiles // import "go.opentelemetry.io/collector/consumer/consumerprofiles"
 
 import (
@@ -15,15 +15,15 @@ var errNilFunc = errors.New("nil consumer func")
 
 // Profiles is an interface that receives pprofile.Profiles, processes it
 // as needed, and sends it to the next processing node if any or to the destination.
-// Deprecated: [0.115.0] Use xconsumer.Profiles instead.
+// Deprecated: [0.116.0] Use xconsumer.Profiles instead.
 type Profiles = xconsumer.Profiles
 
 // ConsumeProfilesFunc is a helper function that is similar to ConsumeProfiles.
-// Deprecated: [0.115.0] Use xconsumer.ConsumeProfilesFunc instead.
+// Deprecated: [0.116.0] Use xconsumer.ConsumeProfilesFunc instead.
 type ConsumeProfilesFunc = xconsumer.ConsumeProfilesFunc
 
 // NewProfiles returns a Profiles configured with the provided options.
-// Deprecated: [0.115.0] Use xconsumer.NewProfiles instead.
+// Deprecated: [0.116.0] Use xconsumer.NewProfiles instead.
 func NewProfiles(consume ConsumeProfilesFunc, options ...consumer.Option) (Profiles, error) {
 	return xconsumer.NewProfiles(consume, options...)
 }
