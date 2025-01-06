33b76d572d990bb6c01adecf139d1ae63895a8ff
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2025-01-06 16:27:41 -0700

    Add validation logic to confmap

--- confmap/confmap.go
+++ confmap/confmap.go
@@ -15,6 +15,7 @@ import (
 	"github.com/knadh/koanf/maps"
 	"github.com/knadh/koanf/providers/confmap"
 	"github.com/knadh/koanf/v2"
+	"go.uber.org/multierr"
 
 	encoder "go.opentelemetry.io/collector/confmap/internal/mapstructure"
 )
@@ -77,6 +78,77 @@ func (fn unmarshalOptionFunc) apply(set *unmarshalOption) {
 	fn(set)
 }
 
+// ConfigValidator defines an optional interface for configurations to implement to do validation.
+type ConfigValidator interface {
+	// Validate the configuration and returns an error if invalid.
+	Validate() error
+}
+
+// As interface types are only used for static typing, a common idiom to find the reflection Type
+// for an interface type Foo is to use a *Foo value.
+var configValidatorType = reflect.TypeOf((*ConfigValidator)(nil)).Elem()
+
+func validate(v reflect.Value) error {
+	// Validate the value itself.
+	k := v.Kind()
+	switch k {
+	case reflect.Invalid:
+		return nil
+	case reflect.Ptr, reflect.Interface:
+		return validate(v.Elem())
+	case reflect.Struct:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		// Reflect on the pointed data and check each of its fields.
+		for i := 0; i < v.NumField(); i++ {
+			if !v.Type().Field(i).IsExported() {
+				continue
+			}
+			errs = multierr.Append(errs, validate(v.Field(i)))
+		}
+		return errs
+	case reflect.Slice, reflect.Array:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		// Reflect on the pointed data and check each of its fields.
+		for i := 0; i < v.Len(); i++ {
+			errs = multierr.Append(errs, validate(v.Index(i)))
+		}
+		return errs
+	case reflect.Map:
+		var errs error
+		errs = multierr.Append(errs, callValidateIfPossible(v))
+		iter := v.MapRange()
+		for iter.Next() {
+			errs = multierr.Append(errs, validate(iter.Key()))
+			errs = multierr.Append(errs, validate(iter.Value()))
+		}
+		return errs
+	default:
+		return callValidateIfPossible(v)
+	}
+}
+
+func callValidateIfPossible(v reflect.Value) error {
+	// If the value type implements ConfigValidator just call Validate
+	if v.Type().Implements(configValidatorType) {
+		return v.Interface().(ConfigValidator).Validate()
+	}
+
+	// If the pointer type implements ConfigValidator call Validate on the pointer to the current value.
+	if reflect.PointerTo(v.Type()).Implements(configValidatorType) {
+		// If not addressable, then create a new *V pointer and set the value to current v.
+		if !v.CanAddr() {
+			pv := reflect.New(reflect.PointerTo(v.Type()).Elem())
+			pv.Elem().Set(v)
+			v = pv.Elem()
+		}
+		return v.Addr().Interface().(ConfigValidator).Validate()
+	}
+
+	return nil
+}
+
 // Unmarshal unmarshalls the config into a struct using the given options.
 // Tags on the fields of the structure must be properly set.
 func (l *Conf) Unmarshal(result any, opts ...UnmarshalOption) error {
@@ -84,7 +156,11 @@ func (l *Conf) Unmarshal(result any, opts ...UnmarshalOption) error {
 	for _, opt := range opts {
 		opt.apply(&set)
 	}
-	return decodeConfig(l, result, !set.ignoreUnused, l.skipTopLevelUnmarshaler)
+	err := decodeConfig(l, result, !set.ignoreUnused, l.skipTopLevelUnmarshaler)
+	if err != nil {
+		return err
+	}
+	return validate(reflect.ValueOf(result))
 }
 
 type marshalOption struct{}
