95eb612a62bbda1daca6f36dcacd8773db380525
Chao <chao.weng@appdynamics.com>
2025-01-08 23:07:13 +0800

    Update

--- scraper/scraperhelper/alias.go
+++ scraper/scraperhelper/alias.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package scraperhelper // import "go.opentelemetry.io/collector/receiver/scraperhelper"
 
 // Deprecated: [v0.118.0] use AddMetricsScraper.
