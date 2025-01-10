98f2ce57a86eefa6a96ee4c5061b99d2511b72a9
Bogdan Drutu <bogdandrutu@gmail.com>
2025-01-10 10:16:35 -0800

    [chore] Cleanup sized channel, no more exceptions for persistent queue
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporterqueue/sized_channel.go
+++ exporter/exporterqueue/sized_channel.go
@@ -3,61 +3,50 @@
 
 package exporterqueue // import "go.opentelemetry.io/collector/exporter/exporterqueue"
 
-import "sync/atomic"
+import (
+	"errors"
+	"sync/atomic"
+)
+
+var errInvalidSize = errors.New("invalid element size")
 
 // sizedChannel is a channel wrapper for sized elements with a capacity set to a total size of all the elements.
 // The channel will accept elements until the total size of the elements reaches the capacity.
 type sizedChannel[T any] struct {
 	sizer sizer[T]
 	used  *atomic.Int64
-
-	// We need to store the capacity in a separate field because the capacity of the channel can be higher.
-	// It happens when we restore a persistent queue from a disk that is bigger than the pre-configured capacity.
-	cap int64
-	ch  chan T
+	ch    chan T
 }
 
-// newSizedChannel creates a sized elements channel. Each element is assigned a size by the provided sizer.
-// chanCapacity is the capacity of the underlying channel which usually should be equal to the capacity of the queue to
-// avoid blocking the producer. Optionally, the channel can be preloaded with the elements and their total size.
+// newSizedChannel creates a sized elements channel. Each element is assigned a positive size by the provided sizer.
+// capacity is the total capacity of the queue.
 func newSizedChannel[T any](capacity int64, sizer sizer[T]) *sizedChannel[T] {
-	used := &atomic.Int64{}
-
-	ch := make(chan T, capacity)
 	return &sizedChannel[T]{
 		sizer: sizer,
-		used:  used,
-		cap:   capacity,
-		ch:    ch,
+		used:  &atomic.Int64{},
+		ch:    make(chan T, capacity),
 	}
 }
 
 // push puts the element into the queue with the given sized if there is enough capacity.
-// Returns an error if the queue is full. The callback is called before the element is committed to the queue.
-// If the callback returns an error, the element is not put into the queue and the error is returned.
-// The size is the size of the element MUST be positive.
+// Returns an error if the queue is full.
 func (vcq *sizedChannel[T]) push(el T) error {
 	elSize := vcq.sizer.Sizeof(el)
-	if vcq.used.Add(elSize) > vcq.cap {
+	if elSize <= 0 {
+		return errInvalidSize
+	}
+	if vcq.used.Add(elSize) > int64(cap(vcq.ch)) {
 		vcq.used.Add(-elSize)
 		return ErrQueueIsFull
 	}
 
-	select {
-	// for persistent queue implementation, channel len can be out of sync with used size. Attempt to put it
-	// into the channel. If it is full, simply returns ErrQueueIsFull error. This prevents potential deadlock issues.
-	case vcq.ch <- el:
-		return nil
-	default:
-		vcq.used.Add(-elSize)
-		return ErrQueueIsFull
-	}
+	vcq.ch <- el
+	return nil
 }
 
 // pop removes the element from the queue and returns it.
 // The call blocks until there is an item available or the queue is stopped.
 // The function returns true when an item is consumed or false if the queue is stopped and emptied.
-// The callback is called before the element is removed from the queue. It must return the size of the element.
 func (vcq *sizedChannel[T]) pop() (T, bool) {
 	el, ok := <-vcq.ch
 	if !ok {
@@ -78,5 +67,5 @@ func (vcq *sizedChannel[T]) Size() int {
 }
 
 func (vcq *sizedChannel[T]) Capacity() int {
-	return int(vcq.cap)
+	return cap(vcq.ch)
 }
