5c69ad0493026b98dadb5dce18fbe2a1b3a1ebb4
Sindy Li <sindy.li@snowflake.com>
2025-01-21 19:22:08 -0800

    Configuration and validation for bytes based batching

--- exporter/exporterbatcher/config_test.go
+++ exporter/exporterbatcher/config_test.go
@@ -5,12 +5,13 @@ package exporterbatcher
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestConfig_Validate(t *testing.T) {
+func TestConfig_ValidateItemBasedBatching(t *testing.T) {
 	cfg := NewDefaultConfig()
 	require.NoError(t, cfg.Validate())
 
@@ -28,4 +29,102 @@ func TestConfig_Validate(t *testing.T) {
 	cfg.MaxSizeItems = 20000
 	cfg.MinSizeItems = 20001
 	assert.EqualError(t, cfg.Validate(), "max_size_items must be greater than or equal to min_size_items")
+}
+
+func TestConfig_ValidateBytesBasedBatching(t *testing.T) {
+	cfg := Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeBytes: 20000,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeBytes: 20001,
+		},
+	}
+	require.NoError(t, cfg.Validate())
+
+	cfg = Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeBytes: -1,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeBytes: 20001,
+		},
+	}
+	require.EqualError(t, cfg.Validate(), "min_size_bytes must be greater than or equal to zero")
+
+	cfg = Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeBytes: 20000,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeBytes: -1,
+		},
+	}
+	require.EqualError(t, cfg.Validate(), "max_size_bytes must be greater than or equal to zero")
+
+	cfg = Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeBytes: 20001,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeBytes: 20000,
+		},
+	}
+	assert.EqualError(t, cfg.Validate(), "max_size_bytes must be greater than or equal to min_size_bytes")
+}
+
+func TestConfig_ItemSizeLimitAndByteSizeLimitShouldNotBeBothSet(t *testing.T) {
+	cfg := Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeItems: 20000,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeBytes: 20001,
+		},
+	}
+	require.EqualError(t, cfg.Validate(), "byte size limit and item limit cannot be specified at the same time")
+
+	cfg = Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeBytes: 20000,
+		},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeItems: 20001,
+		},
+	}
+	require.EqualError(t, cfg.Validate(), "byte size limit and item limit cannot be specified at the same time")
+
+	cfg = Config{
+		Enabled:       true,
+		FlushTimeout:  200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{},
+		MaxSizeConfig: MaxSizeConfig{
+			MaxSizeItems: 20001,
+			MaxSizeBytes: 20001,
+		},
+	}
+	require.EqualError(t, cfg.Validate(), "byte size limit and item limit cannot be specified at the same time")
+
+	cfg = Config{
+		Enabled:      true,
+		FlushTimeout: 200 * time.Millisecond,
+		MinSizeConfig: MinSizeConfig{
+			MinSizeItems: 20001,
+			MinSizeBytes: 20001,
+		},
+		MaxSizeConfig: MaxSizeConfig{},
+	}
+	require.EqualError(t, cfg.Validate(), "byte size limit and item limit cannot be specified at the same time")
 }
