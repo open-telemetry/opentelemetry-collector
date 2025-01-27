35f5bfddbd522b55abeeba7fbd5d5f8155a2b423
dmathieu <42@dmathieu.com>
2025-01-27 09:37:41 +0100

    more review changes

--- pdata/pprofile/attributes.go
+++ pdata/pprofile/attributes.go
@@ -11,39 +11,16 @@ type attributable interface {
 	AttributeIndices() pcommon.Int32Slice
 }
 
-// BuildAttributes builds a [pcommon.Map] containing the attributes of a record.
+// FromAttributeIndices builds a [pcommon.Map] containing the attributes of a
+// record.
 // The record can by any struct that implements an `AttributeIndices` method.
-func BuildAttributes(profile Profile, record attributable) pcommon.Map {
+func FromAttributeIndices(profile Profile, record attributable) pcommon.Map {
 	m := pcommon.NewMap()
+	m.EnsureCapacity(record.AttributeIndices().Len())
 
 	for i := 0; i < record.AttributeIndices().Len(); i++ {
-		id := int(record.AttributeIndices().At(i))
-		a := profile.AttributeTable().At(id)
-
-		key := a.Key()
-		val := a.Value()
-
-		switch val.Type() {
-		case pcommon.ValueTypeEmpty:
-			m.PutStr(key, "")
-		case pcommon.ValueTypeStr:
-			m.PutStr(key, val.AsString())
-		case pcommon.ValueTypeBool:
-			m.PutBool(key, val.Bool())
-		case pcommon.ValueTypeDouble:
-			m.PutDouble(key, val.Double())
-		case pcommon.ValueTypeInt:
-			m.PutInt(key, val.Int())
-		case pcommon.ValueTypeBytes:
-			bs := m.PutEmptyBytes(key)
-			val.Bytes().CopyTo(bs)
-		case pcommon.ValueTypeMap:
-			ma := m.PutEmptyMap(key)
-			val.Map().CopyTo(ma)
-		case pcommon.ValueTypeSlice:
-			sl := m.PutEmptySlice(key)
-			val.Slice().CopyTo(sl)
-		}
+		kv := profile.AttributeTable().At(int(record.AttributeIndices().At(i)))
+		kv.Value().CopyTo(m.PutEmpty(kv.Key()))
 	}
 
 	return m
--- pdata/pprofile/attributes_test.go
+++ pdata/pprofile/attributes_test.go
@@ -8,12 +8,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
-func TestBuildAttributes(t *testing.T) {
+func TestFromAttributeIndices(t *testing.T) {
 	profile := NewProfile()
 	att := profile.AttributeTable().AppendEmpty()
 	att.SetKey("hello")
@@ -22,31 +21,29 @@ func TestBuildAttributes(t *testing.T) {
 	att2.SetKey("bonjour")
 	att2.Value().SetStr("monde")
 
-	attrs := BuildAttributes(profile, profile)
+	attrs := FromAttributeIndices(profile, profile)
 	assert.Equal(t, attrs, pcommon.NewMap())
 
 	// A Location with a single attribute
 	loc := NewLocation()
 	loc.AttributeIndices().Append(0)
 
-	attrs = BuildAttributes(profile, loc)
+	attrs = FromAttributeIndices(profile, loc)
 
-	m := pcommon.NewMap()
-	require.NoError(t, m.FromRaw(map[string]any{"hello": "world"}))
-	assert.Equal(t, attrs, m)
+	m := map[string]any{"hello": "world"}
+	assert.Equal(t, attrs.AsRaw(), m)
 
 	// A Mapping with two attributes
 	mapp := NewLocation()
 	mapp.AttributeIndices().Append(0, 1)
 
-	attrs = BuildAttributes(profile, mapp)
+	attrs = FromAttributeIndices(profile, mapp)
 
-	m = pcommon.NewMap()
-	require.NoError(t, m.FromRaw(map[string]any{"hello": "world", "bonjour": "monde"}))
-	assert.Equal(t, attrs, m)
+	m = map[string]any{"hello": "world", "bonjour": "monde"}
+	assert.Equal(t, attrs.AsRaw(), m)
 }
 
-func BenchmarkBuildAttributes(b *testing.B) {
+func BenchmarkFromAttributeIndices(b *testing.B) {
 	profile := NewProfile()
 
 	for i := range 10 {
@@ -62,6 +59,6 @@ func BenchmarkBuildAttributes(b *testing.B) {
 	b.ReportAllocs()
 
 	for n := 0; n < b.N; n++ {
-		_ = BuildAttributes(profile, obj)
+		_ = FromAttributeIndices(profile, obj)
 	}
 }
