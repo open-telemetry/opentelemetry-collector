dc30863c4ed98db8ccdea17e47477be4c0b423c4
dmathieu <42@dmathieu.com>
2025-01-28 08:42:54 +0100

    document that making changes on the map will not change the record

--- pdata/pprofile/attributes.go
+++ pdata/pprofile/attributes.go
@@ -14,6 +14,7 @@ type attributable interface {
 // FromAttributeIndices builds a [pcommon.Map] containing the attributes of a
 // record.
 // The record can by any struct that implements an `AttributeIndices` method.
+// Updates made to the return map will not be applied back to the record.
 func FromAttributeIndices(table AttributeTableSlice, record attributable) pcommon.Map {
 	m := pcommon.NewMap()
 	m.EnsureCapacity(record.AttributeIndices().Len())
