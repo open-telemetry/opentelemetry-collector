9937fff3ec1d1194abcb36c51823123aec8be74e
Bogdan Drutu <bogdandrutu@gmail.com>
2025-01-30 23:03:58 -0800

    Deprecate metadatatest.Telemetry in favor of componenttest.Telemetry
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- processor/memorylimiterprocessor/internal/metadatatest/generated_telemetrytest.go
+++ processor/memorylimiterprocessor/internal/metadatatest/generated_telemetrytest.go
@@ -16,21 +16,29 @@ import (
 	"go.opentelemetry.io/collector/processor/processortest"
 )
 
+// Deprecated: [v0.119.0] Use componenttest.Telemetry
 type Telemetry struct {
 	*componenttest.Telemetry
 }
 
+// Deprecated: [v0.119.0] Use componenttest.NewTelemetry
 func SetupTelemetry(opts ...componenttest.TelemetryOption) Telemetry {
 	return Telemetry{Telemetry: componenttest.NewTelemetry(opts...)}
 }
 
+// Deprecated: [v0.119.0] Use metadatatest.NewSettings
 func (tt *Telemetry) NewSettings() processor.Settings {
+	return NewSettings(tt.Telemetry)
+}
+
+func NewSettings(tt *componenttest.Telemetry) processor.Settings {
 	set := processortest.NewNopSettings()
 	set.ID = component.NewID(component.MustNewType("memory_limiter"))
 	set.TelemetrySettings = tt.NewTelemetrySettings()
 	return set
 }
 
+// Deprecated: [v0.119.0] Use metadatatest.AssertEqual*
 func (tt *Telemetry) AssertMetrics(t *testing.T, expected []metricdata.Metrics, opts ...metricdatatest.Option) {
 	var md metricdata.ResourceMetrics
 	require.NoError(t, tt.Reader.Collect(context.Background(), &md))
@@ -42,13 +50,6 @@ func (tt *Telemetry) AssertMetrics(t *testing.T, expected []metricdata.Metrics, 
 
 	// ensure no additional metrics are emitted
 	require.Equal(t, len(expected), lenMetrics(md))
-}
-
-func NewSettings(tt *componenttest.Telemetry) processor.Settings {
-	set := processortest.NewNopSettings()
-	set.ID = component.NewID(component.MustNewType("memory_limiter"))
-	set.TelemetrySettings = tt.NewTelemetrySettings()
-	return set
 }
 
 func AssertEqualProcessorAcceptedLogRecords(t *testing.T, tt *componenttest.Telemetry, dps []metricdata.DataPoint[int64], opts ...metricdatatest.Option) {
