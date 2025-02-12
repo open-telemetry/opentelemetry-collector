67729fd880705fca15153e7ab2b9b380ec51eadb
Sindy Li <sindy.li@snowflake.com>
2025-02-11 17:35:36 -0800

    Implemented merged context with link

---
+++ exporter/exporterhelper/internal/batcher/merged_context.go
@@ -0,0 +1,63 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package batcher // import "go.opentelemetry.io/collector/exporter/internal/queue"
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+// mergedContext links the underlying context to all incoming span contexts.
+type mergedContext struct {
+	deadline   time.Time
+	deadlineOk bool
+	ctx        context.Context
+}
+
+func NewMergedContext(ctx context.Context) mergedContext {
+	deadline, ok := ctx.Deadline()
+	return mergedContext{
+		deadline:   deadline,
+		deadlineOk: ok,
+		ctx:        ctx,
+	}
+}
+
+// Merge links the span from incoming context to the span from the first context.
+func (mc *mergedContext) Merge(other context.Context) mergedContext {
+	deadline, deadlineOk := mc.Deadline()
+	if otherDeadline, ok := other.Deadline(); ok {
+		deadlineOk = true
+		if deadline.Before(otherDeadline) {
+			deadline = otherDeadline
+		}
+	}
+
+	link := trace.LinkFromContext(other)
+	trace.SpanFromContext(mc.ctx).AddLink(link)
+	return mergedContext{
+		deadline:   deadline,
+		deadlineOk: deadlineOk,
+		ctx:        mc.ctx,
+	}
+}
+
+// Deadline returns the latest deadline of all context.
+func (mc mergedContext) Deadline() (time.Time, bool) {
+	return mc.deadline, mc.deadlineOk
+}
+
+func (mc mergedContext) Done() <-chan struct{} {
+	return nil
+}
+
+func (mc mergedContext) Err() error {
+	return nil
+}
+
+// Value delegates to the first context.
+func (mc mergedContext) Value(key any) any {
+	return mc.ctx.Value(key)
+}
---
+++ exporter/exporterhelper/internal/batcher/merged_context_test.go
@@ -0,0 +1,68 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package batcher
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func TestMergedContextDeadline(t *testing.T) {
+	now := time.Now()
+	ctx1 := context.Background()
+	mergedContext := NewMergedContext(ctx1)
+
+	deadline, ok := mergedContext.Deadline()
+	require.False(t, ok)
+
+	ctx2, cancel2 := context.WithDeadline(context.Background(), now.Add(200))
+	defer cancel2()
+	mergedContext = mergedContext.Merge(ctx2)
+
+	deadline, ok = mergedContext.Deadline()
+	require.True(t, ok)
+	require.Equal(t, now.Add(200), deadline)
+
+	ctx3, cancel3 := context.WithDeadline(context.Background(), now.Add(300))
+	defer cancel3()
+	ctx4, cancel4 := context.WithDeadline(context.Background(), now.Add(100))
+	defer cancel4()
+	mergedContext = mergedContext.Merge(ctx3)
+	mergedContext = mergedContext.Merge(ctx4)
+
+	deadline, ok = mergedContext.Deadline()
+	require.True(t, ok)
+	require.Equal(t, now.Add(300), deadline)
+}
+
+func TestMergedContextLink(t *testing.T) {
+	tracerProvider := componenttest.NewTelemetry().NewTelemetrySettings().TracerProvider
+	tracer := tracerProvider.Tracer("go.opentelemetry.io/collector/exporter/exporterhelper")
+
+	ctx1 := context.WithValue(context.Background(), "key", "value")
+	ctx2, span2 := tracer.Start(ctx1, "span2")
+	defer span2.End()
+
+	mergedContext := NewMergedContext(ctx2)
+	ctx3, span3 := tracer.Start(ctx1, "span3")
+	defer span3.End()
+	mergedContext = mergedContext.Merge(ctx3)
+	ctx4, span4 := tracer.Start(ctx1, "span3")
+	defer span4.End()
+	mergedContext = mergedContext.Merge(ctx4)
+
+	span2.AddEvent("This is an event.")
+
+	spanFromMergedContext := trace.SpanFromContext(mergedContext)
+	require.Equal(t, span2, spanFromMergedContext)
+
+	require.Equal(t, trace.SpanContextFromContext(ctx3), span2.(sdktrace.ReadOnlySpan).Links()[0].SpanContext)
+	require.Equal(t, trace.SpanContextFromContext(ctx4), span2.(sdktrace.ReadOnlySpan).Links()[1].SpanContext)
+}
