a4ae17511157ab1ac9a4d65d7073253b7a0d5ecf
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-02-13 17:56:20 +0100

    [*test] Add NewNopSettingsWithType everywhere (#12357)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    <!-- Issue number if applicable -->
    
    Creates `NewNopSettingsWithType` function on test modules and deprecates
    the `NewNopSettings` functions on those modules. Replace all usages of
    `NewNopSettings` with `NewNopSettingsWithType`.
    
    Part of #12305 but applied to all component kinds.
    
    #### Link to tracking issue
    Updates #12221

--- connector/connectortest/connector.go
+++ connector/connectortest/connector.go
@@ -17,12 +17,18 @@ import (
 	"go.opentelemetry.io/collector/consumer/xconsumer"
 )
 
-var nopType = component.MustNewType("nop")
+var NopType = component.MustNewType("nop")
 
 // NewNopSettings returns a new nop settings for Create* functions.
+// Deprecated: [v0.120.0] Use NewNopSettingsWithType instead.
 func NewNopSettings() connector.Settings {
+	return NewNopSettingsWithType(NopType)
+}
+
+// NewNopSettingsWithType returns a new nop settings for Create* functions with the given type.
+func NewNopSettingsWithType(typ component.Type) connector.Settings {
 	return connector.Settings{
-		ID:                component.NewIDWithName(nopType, uuid.NewString()),
+		ID:                component.NewIDWithName(typ, uuid.NewString()),
 		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
 		BuildInfo:         component.NewDefaultBuildInfo(),
 	}
@@ -33,7 +39,7 @@ type nopConfig struct{}
 // NewNopFactory returns a connector.Factory that constructs nop processors.
 func NewNopFactory() connector.Factory {
 	return xconnector.NewFactory(
-		nopType,
+		NopType,
 		func() component.Config {
 			return &nopConfig{}
 		},
