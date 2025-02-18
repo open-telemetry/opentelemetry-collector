1fa7525b17a814c8b897574cc5beb7f82da9ffb2
Sindy Li <sindy.li@snowflake.com>
2025-02-17 21:42:50 -0800

    Implement sizer text marshaler

--- exporter/exporterbatcher/config_test.go
+++ exporter/exporterbatcher/config_test.go
@@ -8,7 +8,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/component"
+	"gopkg.in/yaml.v2"
+
+	"go.opentelemetry.io/collector/confmap"
 )
 
 func TestValidateConfig(t *testing.T) {
@@ -33,53 +35,49 @@ func TestValidateConfig(t *testing.T) {
 
 func TestValidateSizeConfig(t *testing.T) {
 	cfg := SizeConfig{
-		SizerType: SizerType{
-			Sizer: "bytes",
-		},
+		Sizer:   MustNewSizer("bytes"),
 		MaxSize: -100,
 		MinSize: 100,
 	}
 	require.EqualError(t, cfg.Validate(), "max_size must be greater than or equal to zero")
 
 	cfg = SizeConfig{
-		SizerType: SizerType{
-			Sizer: "bytes",
-		},
+		Sizer:   MustNewSizer("bytes"),
 		MaxSize: 100,
 		MinSize: -100,
 	}
 	require.EqualError(t, cfg.Validate(), "min_size must be greater than or equal to zero")
 
 	cfg = SizeConfig{
-		SizerType: SizerType{
-			Sizer: "bytes",
-		},
+		Sizer:   MustNewSizer("bytes"),
 		MaxSize: 100,
 		MinSize: 200,
 	}
 	require.EqualError(t, cfg.Validate(), "max_size must be greater than or equal to mix_size")
 }
 
-func TestValidateSizer(t *testing.T) {
-	cfg := SizerType{
-		Sizer: "invalid",
-	}
-	require.EqualError(t, cfg.Validate(), "sizer should either be bytes or items")
+func TestSizeUnmarshaler(t *testing.T) {
+	var rawConf map[string]any
+	cfg := NewDefaultConfig()
 
-	cfg.Sizer = "bytes"
+	require.NoError(t, yaml.Unmarshal([]byte(`sizer: bytes`), &rawConf))
+	require.NoError(t, confmap.NewFromStringMap(rawConf).Unmarshal(&cfg))
 	require.NoError(t, cfg.Validate())
 
-	cfg.Sizer = "items"
+	require.NoError(t, yaml.Unmarshal([]byte(`sizer: "bytes"`), &rawConf))
+	require.NoError(t, confmap.NewFromStringMap(rawConf).Unmarshal(&cfg))
 	require.NoError(t, cfg.Validate())
-}
 
-func TestValidateConfigInvokedOnNestedFieldsAutomatically(t *testing.T) {
-	cfg := SizeConfig{
-		SizerType: SizerType{
-			Sizer: "invalid",
-		},
-		MaxSize: 100,
-		MinSize: 0,
-	}
-	require.EqualError(t, component.ValidateConfig(cfg), "sizer should either be bytes or items")
+	require.NoError(t, yaml.Unmarshal([]byte(`sizer: items`), &rawConf))
+	require.NoError(t, confmap.NewFromStringMap(rawConf).Unmarshal(&cfg))
+	require.NoError(t, cfg.Validate())
+
+	require.NoError(t, yaml.Unmarshal([]byte(`sizer: 'items'`), &rawConf))
+	require.NoError(t, confmap.NewFromStringMap(rawConf).Unmarshal(&cfg))
+	require.NoError(t, cfg.Validate())
+
+	require.NoError(t, yaml.Unmarshal([]byte(`sizer: invalid`), &rawConf))
+	require.EqualError(t,
+		confmap.NewFromStringMap(rawConf).Unmarshal(&cfg),
+		"decoding failed due to the following error(s):\n\nerror decoding 'sizer': invalid sizer: \"invalid\"")
 }
---
+++ exporter/exporterbatcher/sizer.go
@@ -0,0 +1,61 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package exporterbatcher // import "go.opentelemetry.io/collector/exporter/exporterbatcher"
+
+import (
+	"fmt"
+)
+
+type SizerType struct {
+	string
+}
+
+const (
+	itemsSizerString = "items"
+	bytesSizerString = "bytes"
+)
+
+var (
+	ItemsSizer = SizerType{itemsSizerString}
+	BytesSizer = SizerType{bytesSizerString}
+)
+
+// String converts SizerType to a string
+func (s *SizerType) String() string {
+	return s.string
+}
+
+// NewSizer creates a new sizer. It returns an error if the input is invalid.
+func NewSizer(text string) (SizerType, error) {
+	switch text {
+	case itemsSizerString:
+		return ItemsSizer, nil
+	case bytesSizerString:
+		return BytesSizer, nil
+	default:
+		return SizerType{}, fmt.Errorf("invalid sizer: %q", text)
+	}
+}
+
+// MustNewType creates a new sizer. It panics if the input is invalid.
+func MustNewSizer(text string) SizerType {
+	sizer, err := NewSizer(text)
+	if err != nil {
+		panic(err)
+	}
+	return sizer
+}
+
+// UnmarshalText implements TextUnmarshaler interface.
+func (s *SizerType) UnmarshalText(text []byte) error {
+	switch str := string(text); str {
+	case itemsSizerString:
+		s.string = itemsSizerString
+	case bytesSizerString:
+		s.string = bytesSizerString
+	default:
+		return fmt.Errorf("invalid sizer: %q", str)
+	}
+	return nil
+}
