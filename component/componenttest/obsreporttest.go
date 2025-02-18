041e29089e12f9e4501cb8d98d4ef49278026a82
Chao <chao.weng@appdynamics.com>
2025-02-18 17:45:19 +0800

    [component/componenttest] Removing the deprecated CheckReceiverMetrics and CheckReceiverTraces functions

--- component/componenttest/obsreporttest.go
+++ component/componenttest/obsreporttest.go
@@ -11,34 +11,9 @@ import (
 	"go.opentelemetry.io/collector/component"
 )
 
-const (
-	// Names used by the metrics and labels are hard coded here in order to avoid
-	// inadvertent changes: at this point changing metric names and labels should
-	// be treated as a breaking changing and requires a good justification.
-	// Changes to metric names or labels can break alerting, dashboards, etc
-	// that are used to monitor the Collector in production deployments.
-	// DO NOT SWITCH THE VARIABLES BELOW TO SIMILAR ONES DEFINED ON THE PACKAGE.
-	receiverTag  = "receiver"
-	scraperTag   = "scraper"
-	transportTag = "transport"
-	exporterTag  = "exporter"
-)
-
 type TestTelemetry struct {
 	*Telemetry
 	id component.ID
-}
-
-// Deprecated: [v0.120.0] use the metadatatest.AssertEqualMetric series of functions instead.
-// CheckReceiverTraces checks that for the current exported values for trace receiver metrics match given values.
-func (tts *TestTelemetry) CheckReceiverTraces(protocol string, acceptedSpans, droppedSpans int64) error {
-	return checkReceiver(tts.Telemetry, tts.id, "spans", protocol, acceptedSpans, droppedSpans)
-}
-
-// Deprecated: [v0.120.0] use the metadatatest.AssertEqualMetric series of functions instead.
-// CheckReceiverMetrics checks that for the current exported values for metrics receiver metrics match given values.
-func (tts *TestTelemetry) CheckReceiverMetrics(protocol string, acceptedMetricPoints, droppedMetricPoints int64) error {
-	return checkReceiver(tts.Telemetry, tts.id, "metric_points", protocol, acceptedMetricPoints, droppedMetricPoints)
 }
 
 // TelemetrySettings returns the TestTelemetry's TelemetrySettings
