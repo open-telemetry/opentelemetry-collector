e53a1f78bf8a575f422eec20f5082dd18fc4a29b
sudipto baral <sudiptobaral.me@gmail.com>
2025-02-26 13:13:10 -0500

    Add test for cyclic connector use.

--- otelcol/collector_test.go
+++ otelcol/collector_test.go
@@ -511,6 +511,14 @@ func TestCollectorDryRun(t *testing.T) {
 			},
 			expectedErr: `failed to build pipelines: connector "nop/connector1" used as exporter in [logs/in2] pipeline but not used in any supported receiver pipeline`,
 		},
+		"cyclic_connector": {
+			settings: CollectorSettings{
+				BuildInfo:              component.NewDefaultBuildInfo(),
+				Factories:              nopFactories,
+				ConfigProviderSettings: newDefaultConfigProviderSettings(t, []string{filepath.Join("testdata", "otelcol-cyclic-connector.yaml")}),
+			},
+			expectedErr: `failed to build pipelines: cycle detected: connector "nop/forward" (traces to traces) -> connector "nop/forward" (traces to traces)`,
+		},
 	}
 
 	for name, test := range tests {
