5dbeb2a0775b3d703c05655e48f57bc6156c535c
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-03-04 13:59:34 +0100

    [extensionauthtest] Remove extensionauthtest.MockClient

--- extension/extensionauth/extensionauthtest/mock_clientauth.go
+++ extension/extensionauth/extensionauthtest/mock_clientauth.go
@@ -4,52 +4,25 @@
 package extensionauthtest // import "go.opentelemetry.io/collector/extension/extensionauth/extensionauthtest"
 
 import (
-	"context"
 	"errors"
 	"net/http"
 
 	"google.golang.org/grpc/credentials"
 
-	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/extension/extensionauth"
 )
 
-var (
-	_            extensionauth.Client = (*MockClient)(nil)
-	errMockError                      = errors.New("mock Error")
-)
+var errMockError = errors.New("mock Error")
 
-// MockClient provides a mock implementation of GRPCClient and HTTPClient interfaces
-type MockClient struct {
-	ResultRoundTripper      http.RoundTripper
-	ResultPerRPCCredentials credentials.PerRPCCredentials
-	MustError               bool
-}
-
-// Start for the MockClient does nothing
-func (m *MockClient) Start(context.Context, component.Host) error {
-	return nil
-}
-
-// Shutdown for the MockClient does nothing
-func (m *MockClient) Shutdown(context.Context) error {
-	return nil
-}
-
-// RoundTripper for the MockClient either returns error if the mock authenticator is forced to or
-// returns the supplied resultRoundTripper.
-func (m *MockClient) RoundTripper(http.RoundTripper) (http.RoundTripper, error) {
-	if m.MustError {
-		return nil, errMockError
+// NewErrorClient returns a new extensionauth.Client that always returns an error on any of the client methods.
+func NewErrorClient(opts ...extensionauth.ClientOption) (extensionauth.Client, error) {
+	errorOpts := []extensionauth.ClientOption{
+		extensionauth.WithClientRoundTripper(func(base http.RoundTripper) (http.RoundTripper, error) {
+			return nil, errMockError
+		}),
+		extensionauth.WithClientPerRPCCredentials(func() (credentials.PerRPCCredentials, error) {
+			return nil, errMockError
+		}),
 	}
-	return m.ResultRoundTripper, nil
-}
-
-// PerRPCCredentials for the MockClient either returns error if the mock authenticator is forced to or
-// returns the supplied resultPerRPCCredentials.
-func (m *MockClient) PerRPCCredentials() (credentials.PerRPCCredentials, error) {
-	if m.MustError {
-		return nil, errMockError
-	}
-	return m.ResultPerRPCCredentials, nil
+	return extensionauth.NewClient(append(errorOpts, opts...)...)
 }
