92f5fb6bf2c449aca9907c540e8e912749b3fd8f
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-03-06 17:45:27 +0100

    Fix lint

--- extension/extensionauth/extensionauthtest/error_client.go
+++ extension/extensionauth/extensionauthtest/error_client.go
@@ -17,7 +17,7 @@ var errMockError = errors.New("mock Error")
 // NewErrorClient returns a new extensionauth.Client that always returns an error on any of the client methods.
 func NewErrorClient(opts ...extensionauth.ClientOption) (extensionauth.Client, error) {
 	errorOpts := []extensionauth.ClientOption{
-		extensionauth.WithClientRoundTripper(func(base http.RoundTripper) (http.RoundTripper, error) {
+		extensionauth.WithClientRoundTripper(func(http.RoundTripper) (http.RoundTripper, error) {
 			return nil, errMockError
 		}),
 		extensionauth.WithClientPerRPCCredentials(func() (credentials.PerRPCCredentials, error) {
