ffe315ad1c5a1423e83823fa1d9018fe2b9bc58f
Bogdan Drutu <bogdandrutu@gmail.com>
2025-03-11 11:42:43 -0700

    [chore] Cleanup internal names for RequestErrorHandler since moved to request package
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporterhelper/request.go
+++ exporter/exporterhelper/request.go
@@ -18,4 +18,4 @@ type Request = request.Request
 // If not implemented, the original Request will be returned assuming the error is applied to the whole Request.
 // Experimental: This API is at the early stage of development and may change without backward compatibility
 // until https://github.com/open-telemetry/opentelemetry-collector/issues/8122 is resolved.
-type RequestErrorHandler = request.RequestErrorHandler
+type RequestErrorHandler = request.ErrorHandler
