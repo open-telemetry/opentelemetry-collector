dc4fab35a74d1abc1f16f24186f13c12115dbb68
Bogdan Drutu <bogdandrutu@gmail.com>
2025-03-12 14:00:33 +0000

    [chore] Cleanup internal names for RequestErrorHandler since moved to request package (#12614)
    … request package
    
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    <!-- Issue number if applicable -->
    #### Link to tracking issue
    Fixes #
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    <!--Describe the documentation added.-->
    #### Documentation
    
    <!--Please delete paragraphs that you did not use before submitting.-->
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporterhelper/internal/request/request.go
+++ exporter/exporterhelper/internal/request/request.go
@@ -31,13 +31,13 @@ type Request interface {
 	MergeSplit(context.Context, exporterbatcher.SizeConfig, Request) ([]Request, error)
 }
 
-// RequestErrorHandler is an optional interface that can be implemented by Request to provide a way handle partial
+// ErrorHandler is an optional interface that can be implemented by Request to provide a way handle partial
 // temporary failures. For example, if some items failed to process and can be retried, this interface allows to
 // return a new Request that contains the items left to be sent. Otherwise, the original Request should be returned.
 // If not implemented, the original Request will be returned assuming the error is applied to the whole Request.
 // Experimental: This API is at the early stage of development and may change without backward compatibility
 // until https://github.com/open-telemetry/opentelemetry-collector/issues/8122 is resolved.
-type RequestErrorHandler interface {
+type ErrorHandler interface {
 	Request
 	// OnError returns a new Request may contain the items left to be sent if some items failed to process and can be retried.
 	// Otherwise, it should return the original Request.
