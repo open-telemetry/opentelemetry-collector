577082c6ce21714a47fdb61a1d4fd0adede93433
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2025-03-18 20:09:53 +0000

    prepare release 1.28.1

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	defaultBetaOtelColVersion   = "v0.122.0"
-	defaultStableOtelColVersion = "v1.28.0"
+	defaultStableOtelColVersion = "v1.28.1"
 )
 
 // errMissingGoMod indicates an empty gomod field
