5e525d770fc1fea704f109c009845709d17c5daf
Jade Guiton <jade.guiton@datadoghq.com>
2025-03-19 17:39:27 +0100

    Add comments in Zap wrapper

--- internal/telemetry/componentattribute/logger_zap.go
+++ internal/telemetry/componentattribute/logger_zap.go
@@ -11,11 +11,14 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// Interface for Zap cores that support setting and resetting a set of component attributes.
 type coreWithAttributes interface {
 	zapcore.Core
 	withAttributeSet(attribute.Set) zapcore.Core
 }
 
+// Tries setting the attribute set for a Zap core.
+// Does nothing if the core does not support setting component attributes.
 func tryWithAttributeSet(c zapcore.Core, attrs attribute.Set) zapcore.Core {
 	if cwa, ok := c.(coreWithAttributes); ok {
 		return cwa.withAttributeSet(attrs)
@@ -30,6 +33,8 @@ type consoleCoreWithAttributes struct {
 
 var _ coreWithAttributes = (*consoleCoreWithAttributes)(nil)
 
+// Wraps a Zap core to support adding component attributes as Zap fields.
+// Used for console output.
 func NewConsoleCoreWithAttributes(c zapcore.Core, attrs attribute.Set) zapcore.Core {
 	var fields []zap.Field
 	for _, kv := range attrs.ToSlice() {
@@ -55,6 +60,8 @@ type otelTeeCoreWithAttributes struct {
 
 var _ coreWithAttributes = (*otelTeeCoreWithAttributes)(nil)
 
+// Creates a Zap core which forwards its output to both a provided Zap `consoleCore` and to a `LoggerProvider`.
+// Data sent through OTel will have component attributes set as instrumentation scope attributes.
 func NewOTelTeeCoreWithAttributes(consoleCore zapcore.Core, lp log.LoggerProvider, scopeName string, level zapcore.Level, attrs attribute.Set) zapcore.Core {
 	// TODO: Use `otelzap.WithAttributes` and remove `LoggerProviderWithAttributes`
 	// once https://github.com/open-telemetry/opentelemetry-go-contrib/issues/6954 is implemented.
@@ -92,6 +99,9 @@ type wrapperCoreWithAttributes struct {
 
 var _ coreWithAttributes = (*wrapperCoreWithAttributes)(nil)
 
+// Applies a wrapper function to a Zap core similar to `logger.WithOption(zap.WrapCore(...))`,
+// but allows component attribute changes to be forwarded to the underlying core.
+// The wrapper function will be run again when the underlying core is recreated.
 func NewWrapperCoreWithAttributes(from zapcore.Core, wrapper func(zapcore.Core) zapcore.Core) zapcore.Core {
 	return &wrapperCoreWithAttributes{
 		Core:    wrapper(from),
