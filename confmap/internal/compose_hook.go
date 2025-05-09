c0c6789e0214ce30bad6ef705bcc03c96d5be3dc
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-05-09 15:17:57 +0200

    Make linter happy

--- confmap/internal/compose_hook.go
+++ confmap/internal/compose_hook.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-viper/mapstructure/v2"
 )
 
-// typedDecodeHook takes a raw DecodeHookFunc (an interface{}) and turns
+// typedDecodeHook takes a raw DecodeHookFunc (an any) and turns
 // it into the proper DecodeHookFunc type, such as DecodeHookFuncType.
 func typedDecodeHook(h mapstructure.DecodeHookFunc) mapstructure.DecodeHookFunc {
 	// Create variables here so we can reference them with the reflect pkg
@@ -20,7 +20,7 @@ func typedDecodeHook(h mapstructure.DecodeHookFunc) mapstructure.DecodeHookFunc 
 
 	// Fill in the variables into this interface and the rest is done
 	// automatically using the reflect package.
-	potential := []interface{}{f3, f1, f2}
+	potential := []any{f3, f1, f2}
 
 	v := reflect.ValueOf(h)
 	vt := v.Type()
@@ -34,13 +34,13 @@ func typedDecodeHook(h mapstructure.DecodeHookFunc) mapstructure.DecodeHookFunc 
 	return nil
 }
 
-// cachedDecodeHook takes a raw DecodeHookFunc (an interface{}) and turns
+// cachedDecodeHook takes a raw DecodeHookFunc (an any) and turns
 // it into a closure to be used directly
 // if the type fails to convert we return a closure always erroring to keep the previous behaviour
-func cachedDecodeHook(raw mapstructure.DecodeHookFunc) func(from reflect.Value, to reflect.Value) (interface{}, error) {
+func cachedDecodeHook(raw mapstructure.DecodeHookFunc) func(from reflect.Value, to reflect.Value) (any, error) {
 	switch f := typedDecodeHook(raw).(type) {
 	case mapstructure.DecodeHookFuncType:
-		return func(from reflect.Value, to reflect.Value) (interface{}, error) {
+		return func(from reflect.Value, to reflect.Value) (any, error) {
 			// CHANGE FROM UPSTREAM: check if from is valid and return nil if not
 			if !from.IsValid() {
 				return nil, nil
@@ -48,7 +48,7 @@ func cachedDecodeHook(raw mapstructure.DecodeHookFunc) func(from reflect.Value, 
 			return f(from.Type(), to.Type(), from.Interface())
 		}
 	case mapstructure.DecodeHookFuncKind:
-		return func(from reflect.Value, to reflect.Value) (interface{}, error) {
+		return func(from reflect.Value, to reflect.Value) (any, error) {
 			// CHANGE FROM UPSTREAM: check if from is valid and return nil if not
 			if !from.IsValid() {
 				return nil, nil
@@ -56,11 +56,11 @@ func cachedDecodeHook(raw mapstructure.DecodeHookFunc) func(from reflect.Value, 
 			return f(from.Kind(), to.Kind(), from.Interface())
 		}
 	case mapstructure.DecodeHookFuncValue:
-		return func(from reflect.Value, to reflect.Value) (interface{}, error) {
+		return func(from reflect.Value, to reflect.Value) (any, error) {
 			return f(from, to)
 		}
 	default:
-		return func(from reflect.Value, to reflect.Value) (interface{}, error) {
+		return func(from reflect.Value, to reflect.Value) (any, error) {
 			return nil, errors.New("invalid decode hook signature")
 		}
 	}
@@ -75,11 +75,11 @@ func cachedDecodeHook(raw mapstructure.DecodeHookFunc) func(from reflect.Value, 
 // This is a copy of [mapstructure.ComposeDecodeHookFunc] but with
 // validation added.
 func ComposeDecodeHookFunc(fs ...mapstructure.DecodeHookFunc) mapstructure.DecodeHookFunc {
-	cached := make([]func(from reflect.Value, to reflect.Value) (interface{}, error), 0, len(fs))
+	cached := make([]func(from reflect.Value, to reflect.Value) (any, error), 0, len(fs))
 	for _, f := range fs {
 		cached = append(cached, cachedDecodeHook(f))
 	}
-	return func(f reflect.Value, t reflect.Value) (interface{}, error) {
+	return func(f reflect.Value, t reflect.Value) (any, error) {
 		var err error
 
 		// CHANGE FROM UPSTREAM: check if f is valid before calling f.Interface()
