5d424b3ae971beff467f5a54f46203aa78dcb603
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-05-09 14:17:53 +0200

    make goporto

--- confmap/internal/compose_hook.go
+++ confmap/internal/compose_hook.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package internal // import "go.opentelemetry.io/collector/confmap/internal/mapstructure"
+package internal // import "go.opentelemetry.io/collector/confmap/internal"
 
 import (
 	"errors"
