485a5a8282f3e00a2efc5bd9c5c3fbc17c354f7c
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-05-13 17:34:16 +0200

    Make zero value be None

--- config/configoptional/optional.go
+++ config/configoptional/optional.go
@@ -10,11 +10,11 @@ import (
 // Optional is a type that can be used to represent a value that may or may not be present.
 // It supports two flavors: Some(value), and Default(factory).
 type Optional[T any] struct {
-	value T
+	value    T
+	hasValue bool
 
 	// defaultFn returns a default value for the type T.
-	// It also acts as a flag to indicate whether the value is present or not.
-	// If defaultFn is nil, it means the value is present.
+	// It MUST be nil if `hasValue` is true.
 	defaultFn *DefaultFunc[T]
 }
 
@@ -25,7 +25,7 @@ var _ confmap.Unmarshaler = (*Optional[any])(nil)
 
 // Some creates an Optional with a value and no factory.
 func Some[T any](value T) Optional[T] {
-	return Optional[T]{value: value}
+	return Optional[T]{value: value, hasValue: true}
 }
 
 // Factory is an opaque type that can be used to create a default value for the type T.
@@ -46,27 +46,28 @@ func NewFactory[T any](defaultFn DefaultFunc[T]) Factory[T] {
 //
 // On successful unmarshal, the factory is erased.
 func Default[T any](factory Factory[T]) Optional[T] {
-	return Optional[T]{defaultFn: factory.defaultFn}
+	return Optional[T]{defaultFn: factory.defaultFn, hasValue: false}
 }
 
-// None is Default(zeroFactory) where zeroFactory creates a zero value of type T.
+// None is equivalent to Default(zeroFactory) where zeroFactory creates a zero value of type T.
 func None[T any]() Optional[T] {
-	return Default(NewFactory(func() (zero T) { return }))
+	return Optional[T]{}
 }
 
 // GetOrInsertDefault returns the value of the Optional.
 // If the Optional is None(factory), it creates the default value.
 func (o *Optional[T]) GetOrInsertDefault() *T {
-	if !o.HasValue() {
+	if !o.HasValue() && o.defaultFn != nil {
 		o.value = (*o.defaultFn)()
 		o.defaultFn = nil
+		o.hasValue = true
 	}
 	return &o.value
 }
 
 // HasValue checks if the Optional has a value.
 func (o Optional[T]) HasValue() bool {
-	return o.defaultFn == nil
+	return o.hasValue
 }
 
 // Get returns the value of the Optional.
@@ -81,12 +82,16 @@ func (o *Optional[T]) Get() *T {
 // Unmarshal the configuration into the Optional value.
 // If the value was None, on success, the factory is erased.
 func (o *Optional[T]) Unmarshal(conf *confmap.Conf) error {
-	defaultFn := o.defaultFn
-	_ = o.GetOrInsertDefault()
+	// GetOrInsertDefault would erase the factory.
+	// We only want to do so if unmarshaling succeeds.
+	if !o.HasValue() && o.defaultFn != nil {
+		o.value = (*o.defaultFn)()
+	}
 	if err := conf.Unmarshal(&o.value); err != nil {
-		// restore defaultFn if unmarshaling fails
-		o.defaultFn = defaultFn
 		return err
 	}
+
+	o.defaultFn = nil
+	o.hasValue = true
 	return nil
 }
