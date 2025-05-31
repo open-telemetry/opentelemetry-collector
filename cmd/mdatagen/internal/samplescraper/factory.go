0c36a991691e8b783df6f7870458e6800b026226
Chao <chao.weng@appdynamics.com>
2025-06-01 01:41:55 +0800

    fix

--- cmd/mdatagen/internal/samplescraper/factory.go
+++ cmd/mdatagen/internal/samplescraper/factory.go
@@ -5,6 +5,7 @@ package samplescraper // import "go.opentelemetry.io/collector/cmd/mdatagen/inte
 
 import (
 	"context"
+	"go.opentelemetry.io/collector/pdata/plog"
 
 	"go.opentelemetry.io/collector/cmd/mdatagen/internal/samplescraper/internal/metadata"
 	"go.opentelemetry.io/collector/component"
@@ -17,11 +18,16 @@ func NewFactory() scraper.Factory {
 	return scraper.NewFactory(
 		metadata.Type,
 		func() component.Config { return &struct{}{} },
-		scraper.WithMetrics(createMetrics, metadata.MetricsStability))
+		scraper.WithMetrics(createMetrics, metadata.MetricsStability),
+		scraper.WithLogs(createLogs, metadata.LogsStability))
 }
 
 func createMetrics(context.Context, scraper.Settings, component.Config) (scraper.Metrics, error) {
 	return scraper.NewMetrics(func(context.Context) (pmetric.Metrics, error) {
 		return pmetric.NewMetrics(), nil
 	})
+}
+
+func createLogs(context.Context, scraper.Settings, component.Config) (scraper.Logs, error) {
+	return scraper.NewLogs(func(context.Context) (plog.Logs, error) { return plog.Logs{}, nil })
 }
