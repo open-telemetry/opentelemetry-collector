471361a4c24d80eb8174c40c1effe4e751f2de80
Bogdan Drutu <bogdandrutu@gmail.com>
2025-06-02 20:24:13 +0300

    [chore] Fix small nits in persistent queue metadata proto (#13125)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporterhelper/internal/queuebatch/internal/persistentqueue/sizer_conversion.go
+++ exporter/exporterhelper/internal/queuebatch/internal/persistentqueue/sizer_conversion.go
@@ -9,36 +9,29 @@ import (
 	"go.opentelemetry.io/collector/exporter/exporterhelper/internal/request"
 )
 
-// Numeric constants for protobuf serialization
-const (
-	SizerTypeBytes    int32 = iota // 0
-	SizerTypeItems                 // 1
-	SizerTypeRequests              // 2
-)
-
 // SizerTypeToProto converts SizerType to int32 for protobuf serialization
-func SizerTypeToProto(sizerType request.SizerType) (int32, error) {
+func SizerTypeToProto(sizerType request.SizerType) (SizerType, error) {
 	switch sizerType {
-	case request.SizerTypeBytes:
-		return SizerTypeBytes, nil
-	case request.SizerTypeItems:
-		return SizerTypeItems, nil
 	case request.SizerTypeRequests:
-		return SizerTypeRequests, nil
+		return SizerType_REQUESTS, nil
+	case request.SizerTypeItems:
+		return SizerType_ITEMS, nil
+	case request.SizerTypeBytes:
+		return SizerType_BYTES, nil
 	default:
 		return -1, fmt.Errorf("invalid sizer type: %v", sizerType)
 	}
 }
 
 // SizerTypeFromProto creates SizerType from int32 representation
-func SizerTypeFromProto(value int32) (request.SizerType, error) {
+func SizerTypeFromProto(value SizerType) (request.SizerType, error) {
 	switch value {
-	case SizerTypeBytes:
-		return request.SizerTypeBytes, nil
-	case SizerTypeItems:
-		return request.SizerTypeItems, nil
-	case SizerTypeRequests:
+	case SizerType_REQUESTS:
 		return request.SizerTypeRequests, nil
+	case SizerType_ITEMS:
+		return request.SizerTypeItems, nil
+	case SizerType_BYTES:
+		return request.SizerTypeBytes, nil
 	default:
 		return request.SizerType{}, fmt.Errorf("invalid sizer type value: %d", value)
 	}
