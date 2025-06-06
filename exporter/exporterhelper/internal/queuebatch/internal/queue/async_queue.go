bf090bfc8836751c00d64668e4f207df4b0d96d6
Bogdan Drutu <bogdandrutu@gmail.com>
2025-06-06 15:14:43 -0700

    [chore] Extract queue logic into a separate internal package
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/exporterhelper/internal/queuebatch/internal/queue/async_queue.go
+++ exporter/exporterhelper/internal/queuebatch/internal/queue/async_queue.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package queuebatch // import "go.opentelemetry.io/collector/exporter/exporterhelper/internal/queuebatch"
+package queue // import "go.opentelemetry.io/collector/exporter/exporterhelper/internal/queuebatch/internal/queue"
 
 import (
 	"context"
