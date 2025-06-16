e73f2922a5e47a22b219adeaee259265f535645a
dmathieu <42@dmathieu.com>
2025-06-16 10:11:49 +0200

    fix linter

--- pdata/pprofile/mappings_test.go
+++ pdata/pprofile/mappings_test.go
@@ -33,7 +33,7 @@ func TestSetMapping(t *testing.T) {
 	// This sets the index and adds to the table.
 	require.NoError(t, SetMapping(table, loc, m2))
 	assert.Equal(t, 2, table.Len())
-	assert.Equal(t, int32(table.Len() - 1), loc.MappingIndex())
+	assert.Equal(t, int32(table.Len()-1), loc.MappingIndex()) //nolint:gosec // G115
 
 	// Set an existing mapping
 	require.NoError(t, SetMapping(table, loc, m))
@@ -42,7 +42,7 @@ func TestSetMapping(t *testing.T) {
 	// Set another existing mapping
 	require.NoError(t, SetMapping(table, loc, m2))
 	assert.Equal(t, 2, table.Len())
-	assert.Equal(t, int32(table.Len() - 1), loc.MappingIndex())
+	assert.Equal(t, int32(table.Len()-1), loc.MappingIndex()) //nolint:gosec // G115
 }
 
 func BenchmarkSetMapping(b *testing.B) {
