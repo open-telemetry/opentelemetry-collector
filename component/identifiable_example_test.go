14f43a12f7e8a09f952f265891261841b3165b44
vky5 <vky05@proton.me>
2025-07-18 21:16:20 +0530

    docs: add testable examples for component.ID and component.Type

---
+++ component/identifiable_example_test.go
@@ -0,0 +1,61 @@
+package component_test
+
+import (
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+)
+
+func ExampleNewType() {
+	t, err := component.NewType("exampleexporter")
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println(t.String())
+
+	// Output:
+	// exampleexporter
+}
+
+func ExampleMustNewType() {
+	t := component.MustNewType("examplereceiver")
+	fmt.Println(t.String())
+
+	// Output:
+	// examplereceiver
+}
+
+func ExampleNewID() {
+	t := component.MustNewType("exampleprocessor")
+	id := component.NewID(t)
+	fmt.Println(id.String())
+
+	// Output:
+	// exampleprocessor
+}
+
+func ExampleMustNewID() {
+	id := component.MustNewID("examplereceiver")
+	fmt.Println(id.String())
+
+	// Output:
+	// examplereceiver
+}
+
+func ExampleNewIDWithName() {
+	t := component.MustNewType("exampleexporter")
+	id := component.NewIDWithName(t, "customname")
+	fmt.Println(id.String())
+
+	// Output:
+	// exampleexporter/customname
+}
+
+func ExampleMustNewIDWithName() {
+	id := component.MustNewIDWithName("exampleprocessor", "customproc")
+	fmt.Println(id.String())
+
+	// Output:
+	// exampleprocessor/customproc
+}
