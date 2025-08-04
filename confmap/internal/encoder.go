9620f42f5df158527f0c62d4152fd248459861ae
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2025-08-04 10:09:44 -0400

    Move confmap functionality to its internal package

---
+++ confmap/internal/encoder.go
@@ -0,0 +1,30 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package internal // import "go.opentelemetry.io/collector/confmap/internal"
+
+import (
+	"github.com/go-viper/mapstructure/v2"
+	encoder "go.opentelemetry.io/collector/confmap/internal/mapstructure"
+)
+
+// EncoderConfig returns a default encoder.EncoderConfig that includes
+// an EncodeHook that handles both TextMarshaller and Marshaler
+// interfaces.
+func EncoderConfig(rawVal any, set MarshalOptions) *encoder.EncoderConfig {
+	hooks := []mapstructure.DecodeHookFunc{
+		encoder.YamlMarshalerHookFunc(),
+		encoder.TextMarshalerHookFunc(),
+	}
+
+	if set.ScalarMarshalingEncodeHookFunc != nil {
+		hooks = append(hooks, set.ScalarMarshalingEncodeHookFunc)
+	}
+
+	// This hook must come after the scalar marshaling hook, if present.
+	hooks = append(hooks, marshalerHookFunc(rawVal))
+
+	return &encoder.EncoderConfig{
+		EncodeHook: mapstructure.ComposeDecodeHookFunc(hooks...),
+	}
+}
