ed145aa0c1564b5b8a59ba615c5a5e23b571fac1
Jade Guiton <jade.guiton@datadoghq.com>
2025-08-11 19:47:00 +0200

    Remove all methods except UnsafeIntoMap, add release note and test

--- pdata/xpdata/map_builder.go
+++ pdata/xpdata/map_builder.go
@@ -1,10 +1,6 @@
 package xpdata
 
 import (
-	"cmp"
-	"errors"
-	"slices"
-
 	"go.opentelemetry.io/collector/pdata/internal"
 	otlpcommon "go.opentelemetry.io/collector/pdata/internal/data/protogen/common/v1"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -31,10 +27,6 @@ func (mb *MapBuilder) EnsureCapacity(capacity int) {
 	copy(mb.pairs, oldValues)
 }
 
-func compareKeyValues(kv1 otlpcommon.KeyValue, kv2 otlpcommon.KeyValue) int {
-	return cmp.Compare(kv1.Key, kv2.Key)
-}
-
 func (mb *MapBuilder) getValue(i int) pcommon.Value {
 	return pcommon.Value(internal.NewValue(&mb.pairs[i].Value, &mb.state))
 }
@@ -57,72 +49,4 @@ func (mb *MapBuilder) UnsafeIntoMap(m pcommon.Map) {
 	internal.GetMapState(internal.Map(m)).AssertMutable()
 	mb.state = internal.StateReadOnly // to avoid modifying a Map later marked as ReadOnly through builder Values
 	*internal.GetOrigMap(internal.Map(m)) = mb.pairs
-}
-
-// SortedIntoMap transfers the contents of a MapBuilder into a Map, checking that the keys are all distinct,
-// and in alphabetical order.
-// If this check fails, the call will return an error without modifying the Map.
-// This operation has linear time complexity and makes no allocations.
-// After this operation, the MapBuilder becomes read-only.
-func (mb *MapBuilder) SortedIntoMap(m pcommon.Map) error {
-	for i := range len(mb.pairs) - 1 {
-		if mb.pairs[i].Key >= mb.pairs[i+1].Key {
-			return errors.New("Keys added to MapBuilder are not in strictly ascending order")
-		}
-	}
-	mb.UnsafeIntoMap(m)
-	return nil
-}
-
-// SortedIntoMap transfers the contents of a MapBuilder into a Map, checking that the keys are all distinct.
-// If this check fails, the call will return an error without modifying the Map.
-// This operation will sort the keys, and has an O(n log n) time complexity.
-// After this operation, the MapBuilder becomes read-only.
-func (mb *MapBuilder) DistinctIntoMap(m pcommon.Map) error {
-	slices.SortFunc(mb.pairs, compareKeyValues)
-	for i := range len(mb.pairs) - 1 {
-		if mb.pairs[i].Key == mb.pairs[i+1].Key {
-			return errors.New("Keys added to MapBuilder are not distinct")
-		}
-	}
-	mb.UnsafeIntoMap(m)
-	return nil
-}
-
-// SortedIntoMap transfers the contents of a MapBuilder into a Map, merging values with the same key with a
-// user-defined `merge` function.
-// `merge` will be called with a slice `vals` of at least 2 values, and must set `vals[0]` as the merged output.
-// This operation will sort the keys, and has an O(n log n) time complexity (assuming `merge` is linear).
-// After this operation, the MapBuilder becomes read-only.
-func (mb *MapBuilder) MergeIntoMap(m pcommon.Map, merge func(vals []pcommon.Value)) {
-	if len(mb.pairs) > 0 {
-		slices.SortFunc(mb.pairs, compareKeyValues)
-		groupStart := 0
-		groupEnd := 1
-		groupKey := mb.pairs[0].Key
-		for {
-			if groupEnd < len(mb.pairs) && mb.pairs[groupEnd].Key == groupKey {
-				groupEnd++
-				continue
-			}
-			groupSize := groupEnd - groupStart
-			if groupSize > 1 {
-				vals := make([]pcommon.Value, groupSize)
-				for i := range groupSize {
-					vals[i] = mb.getValue(groupStart + i)
-				}
-				// We expect the result to be placed in vals[0]
-				merge(vals)
-				mb.pairs = slices.Delete(mb.pairs, groupStart+1, groupEnd)
-				groupEnd = groupStart + 1
-			}
-			if groupEnd == len(mb.pairs) {
-				break
-			}
-			groupStart = groupEnd
-			groupEnd = groupEnd + 1
-			groupKey = mb.pairs[groupStart].Key
-		}
-	}
-	mb.UnsafeIntoMap(m)
 }
