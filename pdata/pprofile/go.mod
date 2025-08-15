b25f0a3aad361328bf4a1185af818c4d9b495d86
Bogdan Drutu <bogdandrutu@gmail.com>
2025-08-15 15:10:39 -0700

    [chore] Ensure that we are wire compatible with official google protobuf generated code
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- pdata/pprofile/go.mod
+++ pdata/pprofile/go.mod
@@ -5,8 +5,11 @@ go 1.24
 require (
 	github.com/stretchr/testify v1.10.0
 	go.opentelemetry.io/collector/pdata v1.38.0
+	go.opentelemetry.io/proto/slim/otlp/collector/profiles/v1development v0.0.1
+	go.opentelemetry.io/proto/slim/otlp/profiles/v1development v0.0.1
 	go.uber.org/goleak v1.3.0
 	google.golang.org/grpc v1.74.2
+	google.golang.org/protobuf v1.36.7
 )
 
 require (
@@ -17,12 +20,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/proto/slim/otlp v1.7.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
-	google.golang.org/protobuf v1.36.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
--- pdata/ptrace/pb_test.go
+++ pdata/ptrace/pb_test.go
@@ -9,9 +9,46 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	gootlptrace "go.opentelemetry.io/proto/slim/otlp/trace/v1"
+	goproto "google.golang.org/protobuf/proto"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
+
+func TestTracesProtoWireCompatibility(t *testing.T) {
+	// This test verifies that OTLP ProtoBufs generated using goproto lib in
+	// opentelemetry-proto repository OTLP ProtoBufs generated using gogoproto lib in
+	// this repository are wire compatible.
+
+	// Generate Traces as pdata struct.
+	td := generateTestTraces()
+
+	// Marshal its underlying ProtoBuf to wire.
+	marshaler := &ProtoMarshaler{}
+	wire1, err := marshaler.MarshalTraces(td)
+	require.NoError(t, err)
+	assert.NotNil(t, wire1)
+
+	// Unmarshal from the wire to OTLP Protobuf in goproto's representation.
+	var goprotoMessage gootlptrace.TracesData
+	err = goproto.Unmarshal(wire1, &goprotoMessage)
+	require.NoError(t, err)
+
+	// Marshal to the wire again.
+	wire2, err := goproto.Marshal(&goprotoMessage)
+	require.NoError(t, err)
+	assert.NotNil(t, wire2)
+
+	// Unmarshal from the wire into gogoproto's representation.
+	var td2 Traces
+	unmarshaler := &ProtoUnmarshaler{}
+	td2, err = unmarshaler.UnmarshalTraces(wire2)
+	require.NoError(t, err)
+
+	// Now compare that the original and final ProtoBuf messages are the same.
+	// This proves that goproto and gogoproto marshaling/unmarshaling are wire compatible.
+	assert.Equal(t, td, td2)
+}
 
 func TestProtoTracesUnmarshalerError(t *testing.T) {
 	p := &ProtoUnmarshaler{}
