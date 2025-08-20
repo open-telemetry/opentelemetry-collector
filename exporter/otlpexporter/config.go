dc0424825b82b77809a322d5873c716b97aca43e
flamingo <avo.letters@outlook.com>
2025-08-20 11:47:43 +0800

    use configgrpc to valid

--- exporter/otlpexporter/config.go
+++ exporter/otlpexporter/config.go
@@ -4,13 +4,6 @@
 package otlpexporter // import "go.opentelemetry.io/collector/exporter/otlpexporter"
 
 import (
-	"errors"
-	"fmt"
-	"net"
-	"regexp"
-	"strconv"
-	"strings"
-
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configgrpc"
 	"go.opentelemetry.io/collector/config/configretry"
@@ -34,96 +27,8 @@ type EndpointValidator interface {
 	Validate(endpoint string) error
 }
 
-type Pipeline struct {
-	validators []EndpointValidator
-}
-
-func (p *Pipeline) Add(v EndpointValidator) {
-	p.validators = append(p.validators, v)
-}
-
-func (p *Pipeline) Run(endpoint string) error {
-	for _, v := range p.validators {
-		if v.Match(endpoint) {
-			sanitized := v.Sanitize(endpoint)
-			return v.Validate(sanitized)
-		}
-	}
-	return nil
-}
-
-type HostPortValidator struct {
-	prefix string
-}
-
-func (v HostPortValidator) Match(ep string) bool {
-	return v.prefix == "" || strings.HasPrefix(ep, v.prefix)
-}
-
-func (v HostPortValidator) Sanitize(ep string) string {
-	if v.prefix == "" {
-		return ep
-	}
-	if strings.HasPrefix(ep, "dns://") {
-		r := regexp.MustCompile(`^dns:///?`)
-		return r.ReplaceAllString(ep, "")
-	}
-	return strings.TrimPrefix(ep, v.prefix)
-}
-
-func (v HostPortValidator) Validate(ep string) error {
-	if ep == "" {
-		return errors.New(`requires a non-empty "endpoint"`)
-	}
-
-	_, port, err := net.SplitHostPort(ep)
-	if err != nil {
-		return err
-	}
-
-	if _, err := strconv.Atoi(port); err != nil {
-		return fmt.Errorf(`invalid port "%s"`, port)
-	}
-	return nil
-}
-
-type UnixValidator struct{}
-
-func (v UnixValidator) Match(ep string) bool {
-	return strings.HasPrefix(ep, "unix:")
-}
-
-func (v UnixValidator) Sanitize(ep string) string {
-	return ep
-}
-
-func (v UnixValidator) Validate(ep string) error {
-	if strings.HasPrefix(ep, "unix://") {
-		path := strings.TrimPrefix(ep, "unix://")
-		if path == "" {
-			return errors.New("empty unix socket path")
-		}
-		return nil
-	} else if strings.HasPrefix(ep, "unix:@") {
-		path := strings.TrimPrefix(ep, "unix:@")
-		if path == "" {
-			return errors.New("empty abstract unix socket path")
-		}
-		return nil
-	}
-
-	return fmt.Errorf("invalid unix socket protocol: %s", ep)
-}
-
 func (c *Config) Validate() error {
-	pipeline := Pipeline{}
-	pipeline.Add(HostPortValidator{prefix: "http://"})
-	pipeline.Add(HostPortValidator{prefix: "https://"})
-	pipeline.Add(HostPortValidator{prefix: "dns://"})
-	pipeline.Add(UnixValidator{})
-	pipeline.Add(HostPortValidator{prefix: ""}) // (host:port)
-
-	return pipeline.Run(c.ClientConfig.Endpoint)
+	return c.ClientConfig.Validate()
 }
 
 var _ component.Config = (*Config)(nil)
