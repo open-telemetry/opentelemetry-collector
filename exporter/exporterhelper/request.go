51488b0aa10527a7404af82d15428d3d7cb88a70
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-09-10 13:41:16 +0200

    Do not deprecate RequestErrorHandler

--- exporter/exporterhelper/request.go
+++ exporter/exporterhelper/request.go
@@ -15,7 +15,6 @@ import (
 // until https://github.com/open-telemetry/opentelemetry-collector/issues/8122 is resolved.
 type Request = request.Request
 
-// Deprecated [v0.136.0]: will be removed in the next minor version.
 // RequestErrorHandler is an optional interface that can be implemented by Request to provide a way handle partial
 // temporary failures. For example, if some items failed to process and can be retried, this interface allows to
 // return a new Request that contains the items left to be sent. Otherwise, the original Request should be returned.
