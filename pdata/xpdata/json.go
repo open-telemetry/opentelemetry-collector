339d3a9984430ef4fac3197cbc6e15a0116a83a7
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-18 08:16:58 -0400

    Address Feedback
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- pdata/xpdata/json.go
+++ pdata/xpdata/json.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package pcommon // import "go.opentelemetry.io/collector/pdata/pcommon"
+package xpdata // import "go.opentelemetry.io/collector/pdata/xpdata"
 
 import (
 	"slices"
