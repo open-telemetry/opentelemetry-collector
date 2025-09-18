efa2cf35a3aa3158bdbc143e16306bfc819dae36
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-09-18 15:39:44 +0200

    [chore][exporterhelper] Updates documentation related to MergeSplit (#13866)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Updates documentation related to the `Request` interface that referenced
    `MaxSizeConfig`, now `MaxSize`.

--- exporter/exporterhelper/internal/request/request.go
+++ exporter/exporterhelper/internal/request/request.go
@@ -18,15 +18,15 @@ type Request interface {
 	// metric data points or log records.
 	ItemsCount() int
 	// MergeSplit is a function that merge and/or splits this request with another one into multiple requests based on the
-	// configured limit provided in MaxSizeConfig.
-	// MergeSplit does not split if all fields in MaxSizeConfig are not initialized (zero).
+	// configured limit provided in maxSize.
+	// MergeSplit does not split if maxSize is zero.
 	// All the returned requests MUST have a number of items that does not exceed the maximum number of items.
 	// Size of the last returned request MUST be less or equal than the size of any other returned request.
 	// The original request MUST not be mutated if error is returned after mutation or if the exporter is
 	// marked as not mutable. The length of the returned slice MUST not be 0.
 	// Experimental: This API is at the early stage of development and may change without backward compatibility
 	// until https://github.com/open-telemetry/opentelemetry-collector/issues/8122 is resolved.
-	MergeSplit(context.Context, int, SizerType, Request) ([]Request, error)
+	MergeSplit(ctx context.Context, maxSize int, sizerType SizerType, req Request) ([]Request, error)
 	// BytesSize returns the size of the request in bytes.
 	BytesSize() int
 }
