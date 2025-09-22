1f869ed2d7a2485ff5ce8e9666852415a9f2ce45
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2025-09-22 13:47:19 +0200

    [chore] Move missing experimental symbols to xexporterhelper (#13868)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    <!-- Issue number if applicable -->
    
    I forgot to move these symbols in #13821.
    
    #### Link to tracking issue
    
    Relates to open-telemetry/opentelemetry-collector-contrib/pull/42782,
    which is how I noticed.
    
    Follows #13821
    
    Updates #11143

--- exporter/exporterhelper/logs.go
+++ exporter/exporterhelper/logs.go
@@ -5,127 +5,21 @@ package exporterhelper // import "go.opentelemetry.io/collector/exporter/exporte
 
 import (
 	"context"
-	"errors"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/exporter/exporterhelper/internal"
-	"go.opentelemetry.io/collector/exporter/exporterhelper/internal/queue"
-	"go.opentelemetry.io/collector/exporter/exporterhelper/internal/request"
-	"go.opentelemetry.io/collector/exporter/exporterhelper/internal/sizer"
+	"go.opentelemetry.io/collector/exporter/exporterhelper/internal/queuebatch"
 	"go.opentelemetry.io/collector/pdata/plog"
-	"go.opentelemetry.io/collector/pdata/xpdata/pref"
-	pdatareq "go.opentelemetry.io/collector/pdata/xpdata/request"
-)
-
-var (
-	logsMarshaler   = &plog.ProtoMarshaler{}
-	logsUnmarshaler = &plog.ProtoUnmarshaler{}
 )
 
 // NewLogsQueueBatchSettings returns a new QueueBatchSettings to configure to WithQueueBatch when using plog.Logs.
 // Experimental: This API is at the early stage of development and may change without backward compatibility
 // until https://github.com/open-telemetry/opentelemetry-collector/issues/8122 is resolved.
+// Deprecated: [v0.136.0] Use xexporterhelper.NewLogsQueueBatchSettings instead.
 func NewLogsQueueBatchSettings() QueueBatchSettings {
-	return QueueBatchSettings{
-		ReferenceCounter: logsReferenceCounter{},
-		Encoding:         logsEncoding{},
-		ItemsSizer:       request.NewItemsSizer(),
-		BytesSizer: request.BaseSizer{
-			SizeofFunc: func(req request.Request) int64 {
-				return int64(logsMarshaler.LogsSize(req.(*logsRequest).ld))
-			},
-		},
-	}
-}
-
-var (
-	_ request.Request      = (*logsRequest)(nil)
-	_ request.ErrorHandler = (*logsRequest)(nil)
-)
-
-type logsRequest struct {
-	ld         plog.Logs
-	cachedSize int
-}
-
-func newLogsRequest(ld plog.Logs) Request {
-	return &logsRequest{
-		ld:         ld,
-		cachedSize: -1,
-	}
-}
-
-type logsEncoding struct{}
-
-var _ QueueBatchEncoding[Request] = logsEncoding{}
-
-func (logsEncoding) Unmarshal(bytes []byte) (context.Context, Request, error) {
-	if queue.PersistRequestContextOnRead() {
-		ctx, logs, err := pdatareq.UnmarshalLogs(bytes)
-		if errors.Is(err, pdatareq.ErrInvalidFormat) {
-			// fall back to unmarshaling without context
-			logs, err = logsUnmarshaler.UnmarshalLogs(bytes)
-		}
-		return ctx, newLogsRequest(logs), err
-	}
-
-	logs, err := logsUnmarshaler.UnmarshalLogs(bytes)
-	if err != nil {
-		var req Request
-		return context.Background(), req, err
-	}
-	return context.Background(), newLogsRequest(logs), nil
-}
-
-func (logsEncoding) Marshal(ctx context.Context, req Request) ([]byte, error) {
-	logs := req.(*logsRequest).ld
-	if queue.PersistRequestContextOnWrite() {
-		return pdatareq.MarshalLogs(ctx, logs)
-	}
-	return logsMarshaler.MarshalLogs(logs)
-}
-
-var _ queue.ReferenceCounter[Request] = logsReferenceCounter{}
-
-type logsReferenceCounter struct{}
-
-func (logsReferenceCounter) Ref(req Request) {
-	pref.RefLogs(req.(*logsRequest).ld)
-}
-
-func (logsReferenceCounter) Unref(req Request) {
-	pref.UnrefLogs(req.(*logsRequest).ld)
-}
-
-func (req *logsRequest) OnError(err error) Request {
-	var logError consumererror.Logs
-	if errors.As(err, &logError) {
-		// TODO: Add logic to unref the new request created here.
-		return newLogsRequest(logError.Data())
-	}
-	return req
-}
-
-func (req *logsRequest) ItemsCount() int {
-	return req.ld.LogRecordCount()
-}
-
-func (req *logsRequest) size(sizer sizer.LogsSizer) int {
-	if req.cachedSize == -1 {
-		req.cachedSize = sizer.LogsSize(req.ld)
-	}
-	return req.cachedSize
-}
-
-func (req *logsRequest) setCachedSize(size int) {
-	req.cachedSize = size
-}
-
-func (req *logsRequest) BytesSize() int {
-	return logsMarshaler.LogsSize(req.ld)
+	return queuebatch.NewLogsQueueBatchSettings()
 }
 
 // NewLogs creates an exporter.Logs that records observability logs and wraps every request with a Span.
@@ -142,7 +36,7 @@ func NewLogs(
 	if pusher == nil {
 		return nil, errNilPushLogs
 	}
-	return internal.NewLogsRequest(ctx, set, requestFromLogs(), requestConsumeFromLogs(pusher),
+	return internal.NewLogsRequest(ctx, set, queuebatch.RequestFromLogs(), queuebatch.RequestConsumeFromLogs(pusher),
 		append([]Option{internal.WithQueueBatchSettings(NewLogsQueueBatchSettings())}, options...)...)
 }
 
@@ -156,18 +50,4 @@ func NewLogsRequest(
 	options ...Option,
 ) (exporter.Logs, error) {
 	return internal.NewLogsRequest(ctx, set, converter, pusher, options...)
-}
-
-// requestConsumeFromLogs returns a RequestConsumeFunc that consumes plog.Logs.
-func requestConsumeFromLogs(pusher consumer.ConsumeLogsFunc) RequestConsumeFunc {
-	return func(ctx context.Context, request Request) error {
-		return pusher.ConsumeLogs(ctx, request.(*logsRequest).ld)
-	}
-}
-
-// requestFromLogs returns a RequestFromLogsFunc that converts plog.Logs into a Request.
-func requestFromLogs() RequestConverterFunc[plog.Logs] {
-	return func(_ context.Context, ld plog.Logs) (Request, error) {
-		return newLogsRequest(ld), nil
-	}
 }
