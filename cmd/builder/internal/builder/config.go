d76fb84110fbb04127c82e231f38433f77d294d9
otelbot <197425009+otelbot@users.noreply.github.com>
2025-09-22 14:25:44 +0000

    prepare release 1.42.0

--- cmd/builder/internal/builder/config.go
+++ cmd/builder/internal/builder/config.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	defaultBetaOtelColVersion   = "v0.135.0"
-	defaultStableOtelColVersion = "v1.41.0"
+	defaultStableOtelColVersion = "v1.42.0"
 )
 
 // errMissingGoMod indicates an empty gomod field
