e20fe3cee167d2d4a16a3e9344ccf3fb307ccf3a
braydonk <braydonk@google.com>
2025-10-03 13:51:26 +0000

    consumererror: Add partial error type
    This PR adds functionality for consumers to create a partial error type.
    This will allow consumers to properly report partial success/failure with
    failed item counts, which can subsequently be used when reporting
    sent/failed metrics.

---
+++ consumer/consumererror/partial.go
@@ -0,0 +1,44 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package consumererror // import "go.opentelemetry.io/collector/consumer/consumererror"
+
+import "errors"
+
+type partialError struct {
+	inner  error
+	failed int
+}
+
+var _ error = partialError{}
+
+func (pe partialError) Error() string {
+	return pe.inner.Error()
+}
+
+func (pe partialError) Unwrap() error {
+	return pe.inner
+}
+
+func (pe partialError) Failed() int {
+	return pe.failed
+}
+
+// NewPartial creates a new partial error. This is used by consumers
+// to report errors where only a subset of the total items failed
+// to be written, but it is not possible to tell which particular items
+// failed.
+func NewPartial(err error, failed int) error {
+	return NewPermanent(partialError{
+		inner:  err,
+		failed: failed,
+	})
+}
+
+// AsPartial checks if an error was wrapped with the NewPartial function,
+// or if it contains one such error in its Unwrap() tree.
+func AsPartial(err error) (partialError, bool) {
+	var pe partialError
+	ok := errors.As(err, &pe)
+	return pe, ok
+}
