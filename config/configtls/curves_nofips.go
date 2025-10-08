1b1bd957ebf0ca47db72c345866eea6a0da0379d
Shaunak Kashyap <ycombinator@gmail.com>
2025-10-08 16:59:58 -0700

    Add fips and nofips implementations for allowed TLS curves

---
+++ config/configtls/curves_nofips.go
@@ -0,0 +1,13 @@
+//go:build !requirefips
+
+package configtls
+
+import "crypto/tls"
+
+var tlsCurveTypes = map[string]tls.CurveID{
+	"P256":           tls.CurveP256,
+	"P384":           tls.CurveP384,
+	"P521":           tls.CurveP521,
+	"X25519":         tls.X25519,
+	"X25519MLKEM768": tls.X25519MLKEM768,
+}
