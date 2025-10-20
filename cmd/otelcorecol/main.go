10dc36146922969e649f48fa5e5423d7fe004ebd
otelbot <197425009+otelbot@users.noreply.github.com>
2025-10-20 16:16:18 +0000

    add multimod changes 0.138.0

--- cmd/otelcorecol/main.go
+++ cmd/otelcorecol/main.go
@@ -20,7 +20,7 @@ func main() {
 	info := component.BuildInfo{
 		Command:     "otelcorecol",
 		Description: "Local OpenTelemetry Collector binary, testing only.",
-		Version:     "0.137.0-dev",
+		Version:     "0.138.0-dev",
 	}
 
 	set := otelcol.CollectorSettings{
