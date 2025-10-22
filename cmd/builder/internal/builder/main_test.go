0e49276b82414818c60252a19a26ac6427435e52
Shaunak Kashyap <ycombinator@gmail.com>
2025-10-22 11:42:09 -0700

    Add GODEBUG=tlsmklem=0 if GODEBUG=fips140=only is set

--- cmd/builder/internal/builder/main_test.go
+++ cmd/builder/internal/builder/main_test.go
@@ -213,6 +213,14 @@ func TestVersioning(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
+			// X25519 curves are not supported when GODEBUG=fips140=only is set, so we
+			// detect if it is and conditionally also add the tlsmklem=0 flag to disable
+			// these curves.
+			if !strings.Contains(os.Getenv("GODEBUG"), "fips140=only") {
+				t.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tlsmklem=0")
+			}
+
 			cfg := tt.cfgBuilder()
 			require.NoError(t, cfg.Validate())
 			require.NoError(t, cfg.ParseModules())
