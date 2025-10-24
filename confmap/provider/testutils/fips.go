2742942ba6e426f4485e7d3d94c1cd76e270a38a
Shaunak Kashyap <ycombinator@gmail.com>
2025-10-24 12:28:53 -0700

    Running make goporto

--- confmap/provider/testutils/fips.go
+++ confmap/provider/testutils/fips.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package testutils
+package testutils // import "go.opentelemetry.io/collector/confmap/provider/testutils"
 
 import (
 	"os"
