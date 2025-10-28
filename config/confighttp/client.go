507664fb82dc85f7afa360746ba968aceff35d47
Jade Guiton <jade.guiton@datadoghq.com>
2025-10-28 11:54:54 +0100

    [configgrpc, confighttp] Support lists of name/value pairs for headers (#13996)
    #### Description
    
    This PR introduces:
    - A generic `MapList[T]` type, which is equivalent to `[]{ Name string,
    Value T }`, but can be unmarshalled from a `map[string]T` as well. This
    type is defined in a new `internal/maplist` package.
    - A specialized version exported as `configopaque.MapList`
    
    `configgrpc` and `confighttp` are changed to use this type instead of
    `map[string]configopaque.String` for storing header configuration, so as
    to be consistent with the SDK declarative config (see tracking issue).
    
    Note that while this is relatively elegant, it causes a number of
    breaking changes. An alternative would be to keep the current
    `map[string]configopaque.String` types, and add an Unmarshal method on
    the surrounding struct: see [this
    PoC](https://github.com/jade-guiton-dd/opentelemetry-collector/commit/fd4c11d674c80f725f221e9cf01611bf2489b4b0).
    
    #### Link to tracking issue
    Fixes #13930
    
    #### Testing
    
    I added a basic unit test in `internal/maplist`.
    
    ---------
    
    Co-authored-by: Pablo Baeyens <pbaeyens31+github@gmail.com>
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- config/confighttp/client.go
+++ config/confighttp/client.go
@@ -56,7 +56,7 @@ type ClientConfig struct {
 	// Additional headers attached to each HTTP request sent by the client.
 	// Existing header values are overwritten if collision happens.
 	// Header values are opaque since they may be sensitive.
-	Headers map[string]configopaque.String `mapstructure:"headers,omitempty"`
+	Headers configopaque.MapList `mapstructure:"headers,omitempty"`
 
 	// Auth configuration for outgoing HTTP calls.
 	Auth configoptional.Optional[configauth.Config] `mapstructure:"auth,omitempty"`
@@ -130,7 +130,6 @@ func NewDefaultClientConfig() ClientConfig {
 	defaultTransport := http.DefaultTransport.(*http.Transport)
 
 	return ClientConfig{
-		Headers:           map[string]configopaque.String{},
 		MaxIdleConns:      defaultTransport.MaxIdleConns,
 		IdleConnTimeout:   defaultTransport.IdleConnTimeout,
 		ForceAttemptHTTP2: true,
@@ -283,18 +282,18 @@ func (cc *ClientConfig) ToClient(ctx context.Context, host component.Host, setti
 // Custom RoundTripper that adds headers.
 type headerRoundTripper struct {
 	transport http.RoundTripper
-	headers   map[string]configopaque.String
+	headers   configopaque.MapList
 }
 
 // RoundTrip is a custom RoundTripper that adds headers to the request.
 func (interceptor *headerRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Set Host header if provided
-	hostHeader, found := interceptor.headers["Host"]
+	hostHeader, found := interceptor.headers.Get("Host")
 	if found && hostHeader != "" {
 		// `Host` field should be set to override default `Host` header value which is Endpoint
 		req.Host = string(hostHeader)
 	}
-	for k, v := range interceptor.headers {
+	for k, v := range interceptor.headers.Iter {
 		req.Header.Set(k, string(v))
 	}
 
