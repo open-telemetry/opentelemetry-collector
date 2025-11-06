fbeef6bf92b7c5522bfe7c8d575e15afada25c6d
Andreas Gkizas <andreas.gkizas@elastic.co>
2025-11-06 18:25:18 +0200

    mod tidy and cahngelog
    Signed-off-by: Andreas Gkizas <andreas.gkizas@elastic.co>

--- exporter/otlphttpexporter/partitioner.go
+++ exporter/otlphttpexporter/partitioner.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package otlphttpexporter
+package otlphttpexporter // import "go.opentelemetry.io/collector/exporter/otlphttpexporter"
 
 import (
 	"bytes"
