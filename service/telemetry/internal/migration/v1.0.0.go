1f983696dc7b16496fc8018297432614051d62be
alex boten <223565+codeboten@users.noreply.github.com>
2025-11-18 11:24:44 -0800

    WIP: update code to use otelconf v1.0 schema
    The go implementation of the release candidate for opentelemetry-configuration schema is still under review in otel-go-contrib. This work is too update our internal dependency to use this release candidate.
    
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

---
+++ service/telemetry/internal/migration/v1.0.0.go
@@ -0,0 +1,104 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package migration // import "go.opentelemetry.io/collector/service/telemetry/internal/migration"
+
+import (
+	"go.opentelemetry.io/contrib/otelconf"
+	"go.uber.org/zap/zapcore"
+
+	"go.opentelemetry.io/collector/config/configtelemetry"
+)
+
+type TracesConfigV100 struct {
+	// Level configures whether spans are emitted or not, the possible values are:
+	//  - "none" indicates that no tracing data should be collected;
+	//  - "basic" is the recommended and covers the basics of the service telemetry.
+	Level configtelemetry.Level `mapstructure:"level"`
+	// Propagators is a list of TextMapPropagators from the supported propagators list. Currently,
+	// tracecontext and  b3 are supported. By default, the value is set to empty list and
+	// context propagation is disabled.
+	Propagators []string `mapstructure:"propagators"`
+
+	otelconf.TracerProviderJson `mapstructure:",squash"`
+}
+
+type MetricsConfigV100 struct {
+	// Level is the level of telemetry metrics, the possible values are:
+	//  - "none" indicates that no telemetry data should be collected;
+	//  - "basic" is the recommended and covers the basics of the service telemetry.
+	//  - "normal" adds some other indicators on top of basic.
+	//  - "detailed" adds dimensions and views to the previous levels.
+	Level configtelemetry.Level `mapstructure:"level"`
+
+	otelconf.MeterProviderJson `mapstructure:",squash"`
+}
+
+type LogsConfigV100 struct {
+	// Level is the minimum enabled logging level.
+	// (default = "INFO")
+	Level zapcore.Level `mapstructure:"level"`
+
+	// Development puts the logger in development mode, which changes the
+	// behavior of DPanicLevel and takes stacktraces more liberally.
+	// (default = false)
+	Development bool `mapstructure:"development,omitempty"`
+
+	// Encoding sets the logger's encoding.
+	// Example values are "json", "console".
+	Encoding string `mapstructure:"encoding"`
+
+	// DisableCaller stops annotating logs with the calling function's file
+	// name and line number. By default, all logs are annotated.
+	// (default = false)
+	DisableCaller bool `mapstructure:"disable_caller,omitempty"`
+
+	// DisableStacktrace completely disables automatic stacktrace capturing. By
+	// default, stacktraces are captured for WarnLevel and above logs in
+	// development and ErrorLevel and above in production.
+	// (default = false)
+	DisableStacktrace bool `mapstructure:"disable_stacktrace,omitempty"`
+
+	// Sampling sets a sampling policy.
+	// Default:
+	// 		sampling:
+	//	   		enabled: true
+	//	   		tick: 10s
+	//	   		initial: 10
+	//	   		thereafter: 100
+	// Sampling can be disabled by setting 'enabled' to false
+	Sampling *LogsSamplingConfig `mapstructure:"sampling"`
+
+	// OutputPaths is a list of URLs or file paths to write logging output to.
+	// The URLs could only be with "file" schema or without schema.
+	// The URLs with "file" schema must be an absolute path.
+	// The URLs without schema are treated as local file paths.
+	// "stdout" and "stderr" are interpreted as os.Stdout and os.Stderr.
+	// see details at Open in zap/writer.go.
+	// (default = ["stderr"])
+	OutputPaths []string `mapstructure:"output_paths"`
+
+	// ErrorOutputPaths is a list of URLs or file paths to write zap internal logger errors to.
+	// The URLs could only be with "file" schema or without schema.
+	// The URLs with "file" schema must use absolute paths.
+	// The URLs without schema are treated as local file paths.
+	// "stdout" and "stderr" are interpreted as os.Stdout and os.Stderr.
+	// see details at Open in zap/writer.go.
+	//
+	// Note that this setting only affects the zap internal logger errors.
+	// (default = ["stderr"])
+	ErrorOutputPaths []string `mapstructure:"error_output_paths"`
+
+	// InitialFields is a collection of fields to add to the root logger.
+	// Example:
+	//
+	// 		initial_fields:
+	//	   		foo: "bar"
+	//
+	// By default, there is no initial field.
+	InitialFields map[string]any `mapstructure:"initial_fields,omitempty"`
+
+	// Processors allow configuration of log record processors to emit logs to
+	// any number of supported backends.
+	Processors []otelconf.LogRecordProcessor `mapstructure:"processors,omitempty"`
+}
