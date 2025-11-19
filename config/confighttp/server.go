ec678bdf2b28316b10ab34fa5f24abf624491a42
Jade Guiton <jade.guiton@datadoghq.com>
2025-11-19 19:23:15 +0100

    [confighttp, configgrpc] Replace host parameter by map of extensions

--- config/confighttp/server.go
+++ config/confighttp/server.go
@@ -168,7 +168,11 @@ func WithDecoder(key string, dec func(body io.ReadCloser) (io.ReadCloser, error)
 }
 
 // ToServer creates an http.Server from settings object.
-func (sc *ServerConfig) ToServer(ctx context.Context, host component.Host, settings component.TelemetrySettings, handler http.Handler, opts ...ToServerOption) (*http.Server, error) {
+//
+// To allow the configuration to reference middleware or authentication extensions,
+// the `extensions` argument should be the output of `host.GetExtensions()`.
+// It may also be `nil` in tests where no such extension is expected to be used.
+func (sc *ServerConfig) ToServer(ctx context.Context, extensions map[component.ID]component.Component, settings component.TelemetrySettings, handler http.Handler, opts ...ToServerOption) (*http.Server, error) {
 	serverOpts := &toServerOptions{}
 	serverOpts.Apply(opts...)
 
@@ -184,7 +188,7 @@ func (sc *ServerConfig) ToServer(ctx context.Context, host component.Host, setti
 	// forward order.  The first middleware runs after
 	// decompression, below, preceded by Auth, CORS, etc.
 	for i := len(sc.Middlewares) - 1; i >= 0; i-- {
-		wrapper, err := sc.Middlewares[i].GetHTTPServerHandler(ctx, host.GetExtensions())
+		wrapper, err := sc.Middlewares[i].GetHTTPServerHandler(ctx, extensions)
 		// If we failed to get the middleware
 		if err != nil {
 			return nil, err
@@ -210,7 +214,7 @@ func (sc *ServerConfig) ToServer(ctx context.Context, host component.Host, setti
 
 	if sc.Auth.HasValue() {
 		auth := sc.Auth.Get()
-		server, err := auth.GetServerAuthenticator(ctx, host.GetExtensions())
+		server, err := auth.GetServerAuthenticator(ctx, extensions)
 		if err != nil {
 			return nil, err
 		}
