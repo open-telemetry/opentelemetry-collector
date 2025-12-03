6eb3771e01956344979192b1681617d538c74f0b
Shaunak Kashyap <ycombinator@gmail.com>
2025-12-03 12:17:24 -0800

    Move skip to common location

--- config/configtls/tpm_test.go
+++ config/configtls/tpm_test.go
@@ -33,7 +33,6 @@ import (
 )
 
 func TestTPM_loadCertificate(t *testing.T) {
-	testutil.SkipIfFIPSOnly(t, "use of CFB is not allowed in FIPS 140-only mode")
 	tpm, err := simulator.OpenSimulator()
 	require.NoError(t, err)
 	defer tpm.Close()
@@ -191,6 +190,7 @@ func TestTPM_open(t *testing.T) {
 }
 
 func createTPMKeyCert(t *testing.T, tpm transport.TPMCloser) ([]byte, []byte) {
+	testutil.SkipIfFIPSOnly(t, "use of CFB is not allowed in FIPS 140-only mode")
 	tpmKey, err := keyfile.NewLoadableKey(tpm, tpm2.TPMAlgECC, 256, []byte(""))
 	require.NoError(t, err)
 	tpmKeySigner, err := tpmKey.Signer(tpm, []byte(""), []byte(""))
