dc599afd367c59b767332e6b572b1ea8c4b45256
sinkingpoint <colin@quirl.co.nz>
2026-01-12 14:37:01 +0000

    Shuffle listen_config files
    As per PR comments, this moves things around so that listen_config_other
    becomes listen_config_unix, and listen_config_windows becomes
    listen_config_other, with appropriate build gating.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- config/confignet/listen_config_other.go
+++ config/confignet/listen_config_other.go
@@ -1,30 +1,18 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-//go:build !windows
+//go:build !(linux || darwin)
 
 package confignet // import "go.opentelemetry.io/collector/config/confignet"
 
 import (
+	"errors"
 	"net"
-	"syscall"
-
-	"golang.org/x/sys/unix"
 )
 
 func (na *AddrConfig) getListenConfig() (net.ListenConfig, error) {
-	cfg := net.ListenConfig{}
 	if na.ReusePort {
-		cfg.Control = func(_, _ string, c syscall.RawConn) error {
-			var controlErr error
-			err := c.Control(func(fd uintptr) {
-				controlErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
-			})
-			if err != nil {
-				return err
-			}
-			return controlErr
-		}
+		return net.ListenConfig{}, errors.New("ReusePort is not supported on this platform")
 	}
 
-	return cfg, nil
+	return net.ListenConfig{}, nil
 }
