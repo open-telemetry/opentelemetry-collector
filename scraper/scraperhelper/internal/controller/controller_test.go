d3776f5b6412a3da24af36f8c45a6ef3e7f5dbe1
Donal O'Sullivan <donal.osullivan@elastic.co>
2026-01-22 12:15:15 +0000

    Add license to top of controller test file

--- scraper/scraperhelper/internal/controller/controller_test.go
+++ scraper/scraperhelper/internal/controller/controller_test.go
@@ -1,4 +1,7 @@
-package controller
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package controller // import "go.opentelemetry.io/collector/scraper/scraperhelper/internal/controller"
 
 import (
 	"testing"
