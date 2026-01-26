4ec201c4b2b695f86e392a4e9e2c95380be09032
Théo de Magalhaes <theo.demagalhaes@datadoghq.com>
2026-01-26 19:31:41 +0000

    added toStringMapRaw method

---
+++ confmap/xconfmap/confmap.go
@@ -0,0 +1,19 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package xconfmap // import "go.opentelemetry.io/collector/confmap/xconfmap"
+
+import (
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/confmap/internal"
+)
+
+// ToStringMapRaw returns the raw configuration map without sanitization.
+// This is an experimental API and may change or be removed in future versions.
+//
+// Unlike confmap.Conf.ToStringMap(), this function does not sanitize the map
+// by removing expandedValue references. This allows for configmap manipulation
+// without destroying internal types.
+func ToStringMapRaw(conf *confmap.Conf) map[string]any {
+	return internal.ToStringMapRaw(conf)
+}
