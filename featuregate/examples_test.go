18ab9a522e7ed0f3e7b3034250dcd1c86668b522
PrkBuilds <fandadep6@gmail.com>
2026-01-28 16:23:11 -0500

    Apply suggestions from code review
    Co-authored-by: Pablo Baeyens <pbaeyens31+github@gmail.com>

--- featuregate/examples_test.go
+++ featuregate/examples_test.go
@@ -13,16 +13,18 @@ import (
 func ExampleRegistry_Register() {
 	reg := featuregate.NewRegistry()
 	gate := reg.MustRegister(
-		"example.gate",
+		"featuregate.example.gate",
 		featuregate.StageAlpha,
 		featuregate.WithRegisterDescription("Example gate"),
 	)
 
+    // By default an alpha feature gate is disabled.
 	fmt.Println(gate.IsEnabled())
 	if err := reg.Set(gate.ID(), true); err != nil {
 		fmt.Println("error:", err)
 		return
 	}
+    // Alpha feature gates can be modified so the gate is now enabled.
 	fmt.Println(gate.IsEnabled())
 
 	// Output:
@@ -32,12 +34,13 @@ func ExampleRegistry_Register() {
 
 func ExampleRegistry_RegisterFlags() {
 	reg := featuregate.NewRegistry()
-	alphaGate := reg.MustRegister("alpha", featuregate.StageAlpha)
-	betaGate := reg.MustRegister("beta", featuregate.StageBeta)
+	alphaGate := reg.MustRegister("featuregate.example.alpha", featuregate.StageAlpha)
+	betaGate := reg.MustRegister("featuregate.example.beta", featuregate.StageBeta)
 
 	fs := flag.NewFlagSet("example", flag.ContinueOnError)
+    // RegisterFlags registers the `--feature-gates` flag on the FlagSet.
 	reg.RegisterFlags(fs)
-	if err := fs.Parse([]string{"--feature-gates=alpha,-beta"}); err != nil {
+	if err := fs.Parse([]string{"--feature-gates=featuregate.example.alpha,-featuregate.example.beta"}); err != nil {
 		fmt.Println("error:", err)
 		return
 	}
