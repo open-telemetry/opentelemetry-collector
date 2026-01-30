25b84d339b5b8eabef4de4afdb0e9232a86ec939
Pratik Fandade <fandadep6@gmail.com>
2026-01-30 10:34:54 -0500

    make fmt ran

--- featuregate/examples_test.go
+++ featuregate/examples_test.go
@@ -18,13 +18,13 @@ func ExampleRegistry_Register() {
 		featuregate.WithRegisterDescription("Example gate"),
 	)
 
-    // By default an alpha feature gate is disabled.
+	// By default an alpha feature gate is disabled.
 	fmt.Println(gate.IsEnabled())
 	if err := reg.Set(gate.ID(), true); err != nil {
 		fmt.Println("error:", err)
 		return
 	}
-    // Alpha feature gates can be modified so the gate is now enabled.
+	// Alpha feature gates can be modified so the gate is now enabled.
 	fmt.Println(gate.IsEnabled())
 
 	// Output:
@@ -38,7 +38,7 @@ func ExampleRegistry_RegisterFlags() {
 	betaGate := reg.MustRegister("featuregate.example.beta", featuregate.StageBeta)
 
 	fs := flag.NewFlagSet("example", flag.ContinueOnError)
-    // RegisterFlags registers the `--feature-gates` flag on the FlagSet.
+	// RegisterFlags registers the `--feature-gates` flag on the FlagSet.
 	reg.RegisterFlags(fs)
 	if err := fs.Parse([]string{"--feature-gates=featuregate.example.alpha,-featuregate.example.beta"}); err != nil {
 		fmt.Println("error:", err)
