a20fa3034f8a940f46e2fe2c610943a44690cb24
Florian Lehner <florian.lehner@elastic.co>
2026-02-04 13:16:21 +0100

    [pdata/pprofile] Declare aggregation elements as deprecated
    https://github.com/open-telemetry/opentelemetry-proto/pull/710/ removed aggregation elements from the Profiles signal.
    
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- pdata/pprofile/aggregation_temporality.go
+++ pdata/pprofile/aggregation_temporality.go
@@ -10,18 +10,28 @@ import (
 // AggregationTemporality specifies the method of aggregating metric values,
 // either DELTA (change since last report) or CUMULATIVE (total since a fixed
 // start time).
+//
+// Deprecated: Type was removed without replacement in the Profiles signal.
 type AggregationTemporality int32
 
 const (
 	// AggregationTemporalityUnspecified is the default AggregationTemporality, it MUST NOT be used.
+	//
+	// Deprecated: This is no longer supported by the Profiles signal.
 	AggregationTemporalityUnspecified = AggregationTemporality(internal.AggregationTemporality_AGGREGATION_TEMPORALITY_UNSPECIFIED)
 	// AggregationTemporalityDelta is a AggregationTemporality for a metric aggregator which reports changes since last report time.
+	//
+	// Deprecated: This is no longer supported by the Profiles signal.
 	AggregationTemporalityDelta = AggregationTemporality(internal.AggregationTemporality_AGGREGATION_TEMPORALITY_DELTA)
 	// AggregationTemporalityCumulative is a AggregationTemporality for a metric aggregator which reports changes since a fixed start time.
+	//
+	// Deprecated: This is no longer supported by the Profiles signal.
 	AggregationTemporalityCumulative = AggregationTemporality(internal.AggregationTemporality_AGGREGATION_TEMPORALITY_CUMULATIVE)
 )
 
 // String returns the string representation of the AggregationTemporality.
+//
+// Deprecated: Type was removed without replacement in the Profiles signal.
 func (at AggregationTemporality) String() string {
 	switch at {
 	case AggregationTemporalityUnspecified:
