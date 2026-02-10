23d0b6ec094f00de3c463c39d1ebd8151a89f565
Jan Korona <jakorona@cisco.com>
2026-02-10 14:03:34 +0100

    Fix build

--- cmd/mdatagen/internal/loader_test.go
+++ cmd/mdatagen/internal/loader_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/collector/cmd/mdatagen/internal/cfgen"
-
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
