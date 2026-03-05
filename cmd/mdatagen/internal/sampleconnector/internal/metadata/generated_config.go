d70543e73fac628942d05737d209b368da4f3f46
Dmitry Anoshin <anoshindx@gmail.com>
2026-03-05 14:51:24 -0800

    [chore] [cmd/mdatagen] Remove redundant generated struct AttributeConfig (#14717)
    Remove redundant `AttributeConfig` struct that is generated if
    re-aggregation is enabled but was never needed. I don't think it needs a
    changelog item given that it's not even generated by default

--- cmd/mdatagen/internal/sampleconnector/internal/metadata/generated_config.go
+++ cmd/mdatagen/internal/sampleconnector/internal/metadata/generated_config.go
@@ -54,11 +54,6 @@ func (ms *MetricConfig) Unmarshal(parser *confmap.Conf) error {
 	return nil
 }
 
-// AttributeConfig holds configuration information for a particular metric.
-type AttributeConfig struct {
-	Enabled bool `mapstructure:"enabled"`
-}
-
 // MetricsConfig provides config for sample metrics.
 type MetricsConfig struct {
 	DefaultMetric            MetricConfig `mapstructure:"default.metric"`
