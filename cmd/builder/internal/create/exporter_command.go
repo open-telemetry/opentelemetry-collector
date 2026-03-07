2ebe78a2e5d5b3fa9337251bdba13f1a14e60d8e
Yaten <yaten598@gmail.com>
2026-03-07 09:12:20 +0000

    create exporter dir & tidy go.mod files
    Signed-off-by: Yaten <yaten598@gmail.com>

--- cmd/builder/internal/create/exporter_command.go
+++ cmd/builder/internal/create/exporter_command.go
@@ -4,7 +4,11 @@
 package create // import "go.opentelemetry.io/collector/cmd/builder/internal/create"
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -19,12 +23,12 @@ func newExporterCommand() *cobra.Command {
 		Long:  `ocb create exporter initializes a new repository in the provided folder with a manifest to start building a custom Collector exporter. This command is experimental and very likely to change.`,
 		Args:  cobra.ExactArgs(1),
 		RunE: func(_ *cobra.Command, args []string) error {
-			dirName := args[0]
-			if dirName == "" {
+			path := args[0]
+			if path == "" {
 				return fmt.Errorf("exporter directory name cannot be empty")
 			}
 
-			return runCreateExporter(dirName, signals, outputPath)
+			return runCreateExporter(path+"exporter", signals, outputPath)
 		},
 	}
 
@@ -34,6 +38,34 @@ func newExporterCommand() *cobra.Command {
 	return cmd
 }
 
-func runCreateExporter(dirName string, signals []string, outputPath string) error {
+func runCreateExporter(path string, signals []string, outputPath string) error {
+	if path == "" {
+		return errors.New("argument must be a folder")
+	}
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return fmt.Errorf("failed to get absolute path: %w", err)
+	}
+	err = os.MkdirAll(path, 0o750)
+	if err != nil {
+		return fmt.Errorf("failed to create folder %s: %w", path, err)
+	}
+
+	err = runTidy(path)
+	if err != nil {
+		return fmt.Errorf("failed to run go mod tidy: %w", err)
+	}
+
+	return nil
+}
+
+func runTidy(path string) error {
+	cmd := exec.Command("go", "mod", "tidy")
+	cmd.Dir = path
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%w (%s)", err, string(output))
+	}
+
 	return nil
 }
