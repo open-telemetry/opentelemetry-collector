176b10948c2126958e2c301e3ba319618d09684c
Israel Blancas <iblancasa@gmail.com>
2026-03-18 15:30:32 +0100

    Fix ci
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- cmd/mdatagen/internal/sampleconnector/generated_component_test.go
+++ cmd/mdatagen/internal/sampleconnector/generated_component_test.go
@@ -17,14 +17,11 @@ import (
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/connector"
 	"go.opentelemetry.io/collector/connector/connectortest"
+	"go.opentelemetry.io/collector/connector/xconnector"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumertest"
-
-	"go.opentelemetry.io/collector/connector/xconnector"
 	"go.opentelemetry.io/collector/consumer/xconsumer"
-
 	"go.opentelemetry.io/collector/pipeline"
-
 	"go.opentelemetry.io/collector/pipeline/xpipeline"
 )
 
