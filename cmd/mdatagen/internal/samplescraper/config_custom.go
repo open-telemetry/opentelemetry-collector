587544ab7d7e5f0630a826866732b285034e0796
Jan Korona <jakorona@cisco.com>
2026-03-24 15:19:59 +0100

    goporto

--- cmd/mdatagen/internal/samplescraper/config_custom.go
+++ cmd/mdatagen/internal/samplescraper/config_custom.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package samplescraper
+package samplescraper // import "go.opentelemetry.io/collector/cmd/mdatagen/internal/samplescraper"
 
 import (
 	"go.opentelemetry.io/collector/config/confighttp"
