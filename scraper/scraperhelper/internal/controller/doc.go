18bc35704a932bc4f03333e4446b625c517167be
Jan Korona <jakorona@cisco.com>
2026-05-19 14:14:19 +0200

    make goporto

--- scraper/scraperhelper/internal/controller/doc.go
+++ scraper/scraperhelper/internal/controller/doc.go
@@ -4,4 +4,4 @@
 //go:generate mdatagen metadata.yaml
 
 // Package scraperhelper provides utilities for scrapers.
-package controller // import "go.opentelemetry.io/collector/scraper/scraperhelper"
+package controller // import "go.opentelemetry.io/collector/scraper/scraperhelper/internal/controller"
