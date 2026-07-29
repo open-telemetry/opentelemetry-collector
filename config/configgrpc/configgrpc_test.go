6e687d07d7b47dd245f49ac232c705adae0a58f7
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2026-07-29 12:29:58 -0400

    Add test case instead of test

--- config/configgrpc/configgrpc_test.go
+++ config/configgrpc/configgrpc_test.go
@@ -265,20 +265,6 @@ func TestAllGrpcClientSettings(t *testing.T) {
 	}
 }
 
-func TestGetGRPCDialOptionsPerRPCCredentialsError(t *testing.T) {
-	cc := ClientConfig{
-		Endpoint: "localhost:1234",
-		TLS:      configtls.ClientConfig{Insecure: true},
-		Auth:     configoptional.Some(configauth.Config{AuthenticatorID: testAuthID}),
-	}
-	extensions := map[component.ID]component.Component{
-		testAuthID: extensionauthtest.NewErr(errors.New("per-rpc credentials failure")),
-	}
-	_, err := cc.getGrpcDialOptions(context.Background(), extensions, componenttest.NewNopTelemetrySettings(), []ToClientConnOption{})
-	require.Error(t, err)
-	assert.ErrorContains(t, err, "per-rpc credentials failure")
-}
-
 func TestSanitizeEndpoint(t *testing.T) {
 	cfg := NewDefaultClientConfig()
 	cfg.Endpoint = "dns://authority/backend.example.com:4317"
@@ -604,6 +590,17 @@ func TestGRPCClientSettingsError(t *testing.T) {
 			settings: ClientConfig{
 				Endpoint: "localhost:1234",
 				Auth:     configoptional.Some(configauth.Config{AuthenticatorID: doesntExistID}),
+			},
+		},
+		{
+			err: "per-rpc credentials failure",
+			settings: ClientConfig{
+				Endpoint: "localhost:1234",
+				TLS:      configtls.ClientConfig{Insecure: true},
+				Auth:     configoptional.Some(configauth.Config{AuthenticatorID: testAuthID}),
+			},
+			extensions: map[component.ID]component.Component{
+				testAuthID: extensionauthtest.NewErr(errors.New("per-rpc credentials failure")),
 			},
 		},
 		{
