d38e62e7b12e05a65f4e7ef36bcd22f18f7b63e6
Jan Korona <jakorona@cisco.com>
2026-09-01 12:55:17 +0200

    Adjust docs.

--- config/confignet/generated_config.go
+++ config/confignet/generated_config.go
@@ -8,11 +8,11 @@ import (
 
 // AddrConfig represents a network endpoint address.
 type AddrConfig struct {
-	// Contains options for connecting to an address.
+	// DialerConfig contains options for connecting to an address.
 	DialerConfig DialerConfig `mapstructure:"dialer,omitempty"`
-	// Configures the address for this network connection. For TCP and UDP networks, the address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
+	// Endpoint configures the address for this network connection. For TCP and UDP networks, the address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
 	Endpoint string `mapstructure:"endpoint,omitempty"`
-	// Transport to use. Allowed protocols are "tcp", "tcp4" (IPv4-only), "tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4" (IPv4-only), "ip6" (IPv6-only), "unix", "unixgram", "unixpacket" and "npipe" (Windows named pipes, Windows-only).
+	// Transport defines the type of transport protocol used. Allowed protocols are "tcp", "tcp4" (IPv4-only), "tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4" (IPv4-only), "ip6" (IPv6-only), "unix", "unixgram", "unixpacket" and "npipe" (Windows named pipes, Windows-only).
 	Transport TransportType `mapstructure:"transport,omitempty"`
 	// prevent unkeyed literal initialization
 	_ struct{}
@@ -27,7 +27,7 @@ func NewDefaultAddrConfig() AddrConfig {
 
 // DialerConfig contains options for connecting to an address.
 type DialerConfig struct {
-	// The maximum amount of time a dial will wait for a connect to complete. The default is no timeout.
+	// Timeout the maximum amount of time a dial will wait for a connect to complete. The default is no timeout.
 	Timeout time.Duration `mapstructure:"timeout,omitempty"`
 	// prevent unkeyed literal initialization
 	_ struct{}
@@ -40,9 +40,9 @@ func NewDefaultDialerConfig() DialerConfig {
 
 // TCPAddrConfig represents a TCP endpoint address.
 type TCPAddrConfig struct {
-	// Contains options for connecting to an address.
+	// DialerConfig contains options for connecting to an address.
 	DialerConfig DialerConfig `mapstructure:"dialer,omitempty"`
-	// Configures the address for this network connection. The address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
+	// Endpoint configures the address for this network connection. The address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
 	Endpoint string `mapstructure:"endpoint,omitempty"`
 	// prevent unkeyed literal initialization
 	_ struct{}
