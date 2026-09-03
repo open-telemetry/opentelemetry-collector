cc4b33fc673f6bc9ccb35fa5764f2047dae328c1
Jan Korona <jakorona@cisco.com>
2026-09-03 16:34:50 +0000

    [chore][cmd/mdatagen] Improve formatting of doc comments in generated configs (#15893)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Added two readability improvements for doc comments in go config files
    generated by `mdatagen`:
    1. Allow multiline comments
    2. Add empty line between struct fields
    
    Follow up to
    [#15706](https://github.com/open-telemetry/opentelemetry-collector/pull/15706#discussion_r3916461664)
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    Regenerated configs for sample components and already migrated core
    types.
    
    <!--Authorship attestation. See AGENTS.md for details. AI agents must
    not check this box on behalf
    of the user; the human author must check it themselves before the PR is
    ready for review.-->
    #### Authorship
    
    - [x] I, a human, wrote this pull request description myself.
    
    <!--Please delete paragraphs that you did not use before submitting.-->

--- config/confignet/generated_config.go
+++ config/confignet/generated_config.go
@@ -10,10 +10,20 @@ import (
 type AddrConfig struct {
 	// DialerConfig contains options for connecting to an address.
 	DialerConfig DialerConfig `mapstructure:"dialer,omitempty"`
-	// Endpoint configures the address for this network connection. For TCP and UDP networks, the address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
+
+	// Endpoint configures the address for this network connection. For TCP and UDP networks,
+	// the address has the form "host:port". The host must be a literal IP address,
+	// or a host name that can be resolved to IP addresses. The port must
+	// be a literal port number or a service name. If the host is a literal IPv6 address
+	// it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80".
+	// The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
 	Endpoint string `mapstructure:"endpoint,omitempty"`
-	// Transport defines the type of transport protocol used. Allowed protocols are "tcp", "tcp4" (IPv4-only), "tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4" (IPv4-only), "ip6" (IPv6-only), "unix", "unixgram", "unixpacket" and "npipe" (Windows named pipes, Windows-only).
+
+	// Transport defines the type of transport protocol used. Allowed protocols are "tcp", "tcp4" (IPv4-only),
+	// "tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4" (IPv4-only),
+	// "ip6" (IPv6-only), "unix", "unixgram", "unixpacket" and "npipe" (Windows named pipes, Windows-only).
 	Transport TransportType `mapstructure:"transport,omitempty"`
+
 	// prevent unkeyed literal initialization
 	_ struct{}
 }
@@ -29,6 +39,7 @@ func NewDefaultAddrConfig() AddrConfig {
 type DialerConfig struct {
 	// Timeout the maximum amount of time a dial will wait for a connect to complete. The default is no timeout.
 	Timeout time.Duration `mapstructure:"timeout,omitempty"`
+
 	// prevent unkeyed literal initialization
 	_ struct{}
 }
@@ -42,8 +53,14 @@ func NewDefaultDialerConfig() DialerConfig {
 type TCPAddrConfig struct {
 	// DialerConfig contains options for connecting to an address.
 	DialerConfig DialerConfig `mapstructure:"dialer,omitempty"`
-	// Endpoint configures the address for this network connection. The address has the form "host:port". The host must be a literal IP address, or a host name that can be resolved to IP addresses. The port must be a literal port number or a service name. If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
+
+	// Endpoint configures the address for this network connection. The address has the form "host:port".
+	// The host must be a literal IP address, or a host name that can be resolved to IP addresses.
+	// The port must be a literal port number or a service name. If the host is a literal IPv6 address
+	// it must be enclosed in square brackets, as in "[2001:db8::1]:80" or "[fe80::1%zone]:80".
+	// The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
 	Endpoint string `mapstructure:"endpoint,omitempty"`
+
 	// prevent unkeyed literal initialization
 	_ struct{}
 }
