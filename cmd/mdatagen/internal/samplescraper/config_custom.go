44af2625fc57b0b4bf8a98888e22c7b4106630ac
Jan Korona <jakorona@cisco.com>
2026-09-04 17:01:38 +0200

    Fix custom validators in go configs generated by mdatagen

--- cmd/mdatagen/internal/samplescraper/config_custom.go
+++ cmd/mdatagen/internal/samplescraper/config_custom.go
@@ -2,7 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package samplescraper // import "go.opentelemetry.io/collector/cmd/mdatagen/internal/samplescraper"
+import "go.opentelemetry.io/collector/component"
 
 func validateJobName(_ string) error {
+	return nil
+}
+
+func validateComponentID(_ component.ID) error {
 	return nil
 }
