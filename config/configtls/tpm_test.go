ef0a5bfd46516a0191deac3d1b5cd072ac05130c
Ewuji John <johnseyi51@gmail.com>
2026-09-15 14:20:38 +0100

    fix for windows-2025

--- config/configtls/tpm_test.go
+++ config/configtls/tpm_test.go
@@ -1,8 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Don't run this test on Windows or AIX, as it requires a TPM simulator which depends on openssl headers.
-//go:build !windows && !darwin && !aix
+// Don't run this test on platforms without CGO, Windows, Darwin, or AIX,
+// as it requires a TPM simulator which depends on openssl headers.
+//go:build cgo && !windows && !darwin && !aix
 
 package configtls // import "go.opentelemetry.io/collector/config/configtls"
 
