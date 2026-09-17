b3560397cf84881a9707e5dbabaa86c6fbdf3f1f
Nandini Sharma <sharmanandini3006@gmail.com>
2026-09-17 09:27:11 +0000

    fix: handle directories in schema loader

--- internal/schemagen/loader.go
+++ internal/schemagen/loader.go
@@ -81,11 +81,18 @@ func (sl *schemaLoader) load(ref Ref) (*ConfigsMetadata, error) {
 }
 
 func (sl *schemaLoader) loadFromFile(filePath string) (*ConfigsMetadata, error) {
-	body, err := os.ReadFile(filePath) // #nosec G304
+	info, err := os.Stat(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil, ErrNotFound
 		}
+		return nil, fmt.Errorf("failed to read schema from %s: %w", filePath, err)
+	}
+	if info.IsDir() {
+		return nil, fmt.Errorf("failed to read schema from %s: is a directory", filePath)
+	}
+	body, err := os.ReadFile(filePath) // #nosec G304
+	if err != nil {
 		return nil, fmt.Errorf("failed to read schema from %s: %w", filePath, err)
 	}
 
