96110f2754fc9326038b4e2355899a1845b6a5ff
chethanm99 <chethanm1399@gmail.com>
2026-09-18 15:11:38 +0530

    fix(otelcol): resolve flaky TestComponentStatusWatcher assertions

--- otelcol/collector_test.go
+++ otelcol/collector_test.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"reflect"
 	"runtime"
 	"sync"
 	"sync/atomic"
@@ -1137,7 +1138,9 @@ func TestComponentStatusWatcher(t *testing.T) {
 			// All processors must report a status change with the same ID
 			assert.Equal(t, component.NewID(unhealthyProcessorFactory.Type()), k.ComponentID())
 			// And all must have a valid startup sequence
-			assert.Equal(t, startupStatuses(v), v)
+			assert.Eventually(t, func() bool {
+				return reflect.DeepEqual(startupStatuses(v), v)
+			}, 2*time.Second, 10*time.Millisecond)
 		}
 		// We have 3 processors with exactly the same ID in otelcol-statuswatcher.yaml
 		// We must have exactly 3 items in our map. This ensures that the "source" argument
@@ -1152,9 +1155,12 @@ func TestComponentStatusWatcher(t *testing.T) {
 
 	// Check for additional statuses after Shutdown.
 	for _, v := range changedComponents {
-		expectedStatuses := append([]componentstatus.Status{}, startupStatuses(v)...)
-		expectedStatuses = append(expectedStatuses, componentstatus.StatusStopping, componentstatus.StatusStopped)
-		assert.Equal(t, expectedStatuses, v)
+		assert.Eventually(t, func() bool {
+			expectedStatuses := append([]componentstatus.Status{}, startupStatuses(v)...)
+			expectedStatuses = append(expectedStatuses, componentstatus.StatusStopping, componentstatus.StatusStopped)
+
+			return reflect.DeepEqual(expectedStatuses, v)
+		}, 2*time.Second, 10*time.Millisecond)
 	}
 
 	assert.Equal(t, StateClosed, col.GetState())
